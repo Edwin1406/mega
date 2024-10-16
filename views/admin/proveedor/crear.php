757bf594e5c1cd346f782a50ab53daa931b7fbc9
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-16 12:36:05 -0500

    Update crear.php

--- views/admin/proveedor/crear.php
+++ views/admin/proveedor/crear.php
@@ -1,1 +1,9 @@
-<h1 class="proveedor__heading"> <?php echo $titulo ?> </h1>
+<h1 class="dashboard__heading"> <?php echo $titulo ?> </h1>
+
+<div class="dashboard__contenedor-boton">
+    <a class="dashboard__boton" href="/sitioweb/admin/ponentes" >
+        <i class="fa-solid fa-circle-arrow-left"></i>
+        Volver
+    </a>
+
+</div>
