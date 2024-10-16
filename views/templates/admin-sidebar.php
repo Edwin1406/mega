19b58446da070a20ccca7d57414314fc584b5f0f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-16 12:06:21 -0500

    .

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -1,6 +1,6 @@
 <aside class="dashboard__sidebar">
     <nav class="dashboard__menu">
-        <a href="/sitioweb/admin/index" class="dashboard__enlace <?php echo pagina_actual('/index') ? 'dashboard__enlace--actual' :'' ?>">
+        <a href="/admin/index" class="dashboard__enlace <?php echo pagina_actual('/index') ? 'dashboard__enlace--actual' :'' ?>">
             <i class="fa-solid fa-house dashboard__icono"></i>
             <span class="dashboard__menu--texto">
                 Inicio
@@ -9,7 +9,7 @@
 
        
 
-        <a href="/sitioweb/admin/eventos" class="dashboard__enlace <?php echo pagina_actual('/eventos') ? 'dashboard__enlace--actual' :'' ?>">
+        <a href="/admin/eventos" class="dashboard__enlace <?php echo pagina_actual('/eventos') ? 'dashboard__enlace--actual' :'' ?>">
             <i class="fa-duotone fa-solid fa-calendar-days dashboard__icono"></i>
             <span class="dashboard__menu--texto">
                 Eventos
