3378d7d048bbb798a2054c6d01a08c28238bc95f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-16 15:24:07 -0500

    Update footer.php

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -36,7 +36,8 @@
     
     <p class="footer__copyright">
         <span class="footer__copyright--regular">
-            -Todos los derechos reservados <?php echo date('Y-m-d H:i:s'); ?>
+            -Todos los derechos reservados <?php setlocale(LC_TIME, 'es_ES.UTF-8'); // Establece la localización en español
+echo strftime("%A %d de %B del %Y");?>
         </span>
 
     </p>
