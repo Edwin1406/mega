262105f4bcb4d36462878b5977a0b533cd676aff
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-16 10:10:49 -0500

    .

--- includes/funciones.php
+++ includes/funciones.php
@@ -10,3 +10,31 @@ function s($html) : string {
     $s = htmlspecialchars($html);
     return $s;
 }
+
+function pagina_actual($path):bool{
+    return str_contains($_SERVER['REQUEST_URI'] ?? '/', $path) ? true : false;
+
+}
+
+function isAuth() : bool {
+    // si existe una sesion iniciada
+    if(!isset($_SESSION)){
+        session_start();
+    }
+    return isset($_SESSION['nombre'])&& isset($_SESSION);
+}
+function isAdmin() : bool {
+    if(!isset($_SESSION)){
+        session_start();
+    }
+    return isset($_SESSION['admin'])&& !empty($_SESSION['admin']);
+}
+
+
+
+function aos_animation():void{
+    $efectos = ['fade-up', 'fade-down', 'fade-left', 'fade-right', 'fade-up-right', 'fade-up-left', 'fade-down-right', 'fade-down-left', 'flip-up', 'flip-down', 'flip-left', 'flip-right', 'slide-up', 'slide-down', 'slide-left', 'slide-right', 'zoom-in', 'zoom-in-up', 'zoom-in-down', 'zoom-in-left', 'zoom-in-right', 'zoom-out', 'zoom-out-up', 'zoom-out-down', 'zoom-out-left', 'zoom-out-right'];
+    $efecto= array_rand($efectos, 1);
+    echo $efectos[$efecto];
+}
+
---
+++ views/templates/header.php
@@ -0,0 +1,45 @@
+<link href="https://fonts.cdnfonts.com/css/dear-story" rel="stylesheet">
+
+
+<div class="header">
+    <div class="header__contenedor">
+        <nav class="header__navegacion">
+            <?php if (isAuth()): ?>
+                <a href="<?php echo isAdmin() ? '/sitioweb/dashboard' : '/sitioweb/finalizar-registro'?>" class="header__enlace"> Administrar</a>
+                <form method="POST" action="/sitioweb/logout" class="header__form">
+                    <input type="submit" value="Cerrar Sesión" class="header__submit">
+                </form>
+
+            <?php else: ?>
+                <a href="/sitioweb/registro" class="header__enlace">Registro</a>
+                <a href="/sitioweb/login" class="header__enlace">Iniciar Sesión</a>
+            <?php endif; ?>
+
+        </nav>
+        <div class="header__contenido">
+            <a href="">
+                <h1 class="header__logo">
+                    New Dental
+                </h1>
+            </a>
+            <p class="header__texto"> Octubre 5-6 - 2024</p>
+            <p class="header__texto header__texto--modalidad"> En linea -Presecial</p>
+            <a href="https://serviacrilico.com/" class="header__boton"> Agendar Citas</a>
+
+        </div>
+    </div>
+</div>
+
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="">
+            <h2 class="barra__logo"> Sitio Web </h2>
+        </a>
+        <nav class="navegacion">
+            <a href="/sitioweb/evento" class="navegacion__enlace <?php echo pagina_actual('/evento') ? 'navegacion__enlace--actual' : ''; ?>">Evento</a>
+            <a href="/sitioweb/paquetes" class="navegacion__enlace <?php echo pagina_actual('/paquetes') ? 'navegacion__enlace--actual' : ''; ?>">Paquetes</a>
+            <a href="/sitioweb/conferencias" class="navegacion__enlace <?php echo pagina_actual('/conferencias') ? 'navegacion__enlace--actual' : ''; ?>">workshops / Conferencias</a>
+            <a href="/sitioweb/registro" class="navegacion__enlace <?php echo pagina_actual('/registro') ? 'navegacion__enlace--actual' : ''; ?>">Comprar Pases</a>
+        </nav>
+    </div>
+</div>
