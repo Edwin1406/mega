3f3fab41c93c23b2912b730dd7b8e73cb5b969d3
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-17 13:01:06 -0500

    Update escoger.php

--- views/admin/area/escoger.php
+++ views/admin/area/escoger.php
@@ -1,8 +1,17 @@
 <h1 class="dashboard__heading"> <?php echo $titulo ?> </h1>
 
 <?php if(count($escoger)===0) {?>
-    <p class="no-proyectos"> No hay proyectos </p>
+    <p class="no-proyectos">NO HAY AREAS ASIGNADAS </p>
 
 <?php  }else {?>
-    
+    <ul class="listado-proyectos">
+        <?php foreach($escoger as $escoger) {?>
+            <li class="proyecto">
+                <a href="/admin/area/paginaArea?id=<?php echo $escoger->url ?>" class="proyecto__nombre">
+                    <?php echo $escoger->area ?>
+                </a>
+            </li>
+        <?php } ?>
+    </ul>
+
 <?php  }?>
