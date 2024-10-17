bd107c21ffd3aa22e29dfb191feaa476f4904445
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-17 14:42:26 -0500

    Update escoger.php

--- views/admin/area/escoger.php
+++ views/admin/area/escoger.php
@@ -5,17 +5,28 @@
 
 <?php  }else {?>
     <ul class="lista-areas">
-        <?php foreach($escoger as $escoger) {?>
-            <li class="areas">
-                <a  href="/admin/area/paginaArea?id=<?php echo $escoger->url ?>" >
-                    <?php  echo $escoger->area  ?>
-                    <?php debuguear($escoger->area) ?>
-                   
-                
-                </a>
-            </li>
-        <?php } ?>
-    </ul>
+    <?php foreach($escoger as $escoger) { ?>
+        <li class="areas">
+            <a href="/admin/area/paginaArea?id=<?php echo $escoger->url ?>">
+                <?php 
+                    // Asigna un ícono basado en el área
+                    $icono = '';
+                    if($escoger->area === 'produccion') {
+                        $icono = '<i class="fas fa-industry"></i>'; // ícono de producción
+                    } elseif($escoger->area === 'ventas') {
+                        $icono = '<i class="fas fa-shopping-cart"></i>'; // ícono de ventas
+                    } else {
+                        $icono = '<i class="fas fa-briefcase"></i>'; // ícono por defecto
+                    }
+
+                    // Muestra el ícono y el nombre del área
+                    echo $icono . ' ' . $escoger->area;
+                ?>
+            </a>
+        </li>
+    <?php } ?>
+</ul>
+
 
 <?php  }?>
 
