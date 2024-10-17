5788e179789f548375039de3e1742ca4d9f909d3
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-17 16:02:21 -0500

    Update index.php

--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -1,38 +1,19 @@
 <h1 class="dashboard__heading"> <?php echo $titulo ?> </h1>
-<div>
-  <canvas id="myChart"></canvas>
- 
+<div style="display: flex;">
+  <div>
+    <canvas id="myChart1"></canvas>
+  </div>
+  <div>
+    <canvas id="myChart2"></canvas>
+  </div>
 </div>
-<div>
-<canvas id="myChart2"></canvas>
-</div>
+
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-
 <script>
-  const ctx = document.getElementById('myChart');
-
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-  const ctx1 = document.getElementById('myChart2');
-
-  new Chart(ctx, {
+  // Primer gráfico
+  const ctx1 = document.getElementById('myChart1');
+  new Chart(ctx1, {
     type: 'bar',
     data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -51,7 +32,24 @@
     }
   });
 
-
-
-  
+  // Segundo gráfico
+  const ctx2 = document.getElementById('myChart2');
+  new Chart(ctx2, {
+    type: 'line', // Tipo diferente para mostrar variedad
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [15, 10, 7, 8, 5, 6],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 </script>
