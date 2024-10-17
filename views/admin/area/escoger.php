23eb77423ab2149a8f1da88e575938355334678b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-17 12:58:00 -0500

    Update escoger.php

--- views/admin/area/escoger.php
+++ views/admin/area/escoger.php
@@ -1,1 +1,8 @@
 <h1 class="dashboard__heading"> <?php echo $titulo ?> </h1>
+
+<?php if(count($escoger)===0) {?>
+    <p class="no-proyectos"> No hay proyectos </p>
+
+<?php  }else {?>
+    
+<?php  }?>
