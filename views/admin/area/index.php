3ae785f332ba68fd7e21c550f610afdf8f527353
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-17 10:24:36 -0500

    Update index.php

--- views/admin/area/index.php
+++ views/admin/area/index.php
@@ -1,9 +1,9 @@
 <h1 class="dashboard__heading"> <?php echo $titulo ?> </h1>
 
 <div class="dashboard__contenedor-boton">
-    <a class="dashboard__boton" href="/admin/proveedor/crear" >
+    <a class="dashboard__boton" href="/admin/area/crear" >
         <i class="fa-solid fa-circle-arrow-left"></i>
-        Anadir proveedor
+        Anadir area
     </a>
 
 </div>
