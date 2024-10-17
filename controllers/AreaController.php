364544762c612ed51beb4d00d38b37624f811ee2
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-17 11:35:41 -0500

    Update AreaController.php

--- controllers/AreaController.php
+++ controllers/AreaController.php
@@ -56,7 +56,26 @@ class AreaController {
 
     public static function paginaArea(Router $router)
     {
-        echo 'pagina';
+       
+        session_start();
+        isAuth();
+
+        // revisar que la persona sea el propietario del proyecto o quien la creo
+        $url = $_GET['id']; // url es id
+        if(!$url) header('Location: /admin/index');
+        // obtener el proyecto
+        $area = Area::where('url',$url);
+        if($area->propietarioId !== $_SESSION['id']){
+            header('Location: /admin/index');
+        }
+        // ----------------------------------------------------------------------------------
+        $router->render('dashboard/proyecto',[
+            'titulo' => $area->area,
+        ]);
+    
+
+
+
     }
 
 
