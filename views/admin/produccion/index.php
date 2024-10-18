0f21f74ad04eff8781d560b9e287a489a2cdaca1
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-18 14:03:14 -0500

    .

--- views/admin/produccion/index.php
+++ views/admin/produccion/index.php
@@ -1,4 +1,10 @@
 <h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
+<div class="dashboard__contenedor-boton">
+    <a class="dashboard__boton" href="/admin/produccion/index" >
+        <i class="fa-solid fa-circle-arrow-left"></i>
+        Volver
+    </a>
+</div>
 
 <?php if(count($escoger_produccion)===0) {?>
     <p class="no-areas">NO HAY AREAS ASIGNADAS </p>
