89b355dcb3eaee1c583cbebc5c571d12e6ef169a
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-18 11:08:05 -0500

    Update index.php

--- views/admin/produccion/index.php
+++ views/admin/produccion/index.php
@@ -13,21 +13,11 @@
                 $id = $produccionA->url; // Obtiene el ID del área
 
                 // Asigna una URL específica basada en el área
-                if($area === 'registro de maquinaria') {
+                if($area === 'registro de maquinaria'|| $area === 'REGISTRO DE MAQUINARIA') {
                     $url = "/admin/produccion/registro_produccion?id=".$id;
                 } elseif($area === 'COTIZADOR'|| $area === 'cotizador') {
-                    $url = "/admin/bodega/index?id=".$id;
-                } elseif($area === 'COMPRAS') {
-                    $url = "/admin/compras/index?id=".$id;
-                } elseif($area === 'VENTAS') {
-                    $url = "/admin/ventas/index?id=".$id;
-                } elseif($area === 'RECURSOS HUMANOS') {
-                    $url = "/admin/recursos-humanos/index?id=".$id;
-                } elseif($area === 'SISTEMAS') {
-                    $url = "/admin/sistemas/index?id=".$id;
-                } elseif($area === 'PRODUCTO TERMINADO') {
-                    $url = "/admin/producto-terminado/index?id=".$id;
-                }
+                    $url = "/admin/cotizador/index?id=".$id;
+                } 
 
                 echo $url; // Muestra la URL específica
             ?>">
@@ -36,18 +26,8 @@
                     $icono = '';
                     if($area === 'REGISTRO DE MAQUINARIA'|| $area === 'registro de maquinaria') {
                         $icono = '<i class="fas fa-industry"></i>'; // ícono de producción
-                    } elseif($area === 'BODEGA') {
-                        $icono = '<i class="fas fa-shopping-cart"></i>'; // ícono de BODEGA
-                    } elseif($area === 'COMPRAS') {
-                        $icono = '<i class="fas fa-shopping-basket"></i>'; // ícono de compras
-                    } elseif($area === 'VENTAS') {
-                        $icono = '<i class="fas fa-handshake"></i>'; // ícono de ventas
-                    } elseif($area === 'RECURSOS HUMANOS') {
-                        $icono = '<i class="fas fa-users"></i>'; // ícono de recursos humanos
-                    } elseif($area === 'SISTEMAS') {
-                        $icono = '<i class="fas fa-laptop-code"></i>'; // ícono de sistemas
-                    } elseif($area === 'PRODUCTO TERMINADO') {
-                        $icono = '<i class="fas fa-box-open"></i>'; // ícono de producto terminado
+                    } elseif($area === 'COTIZADOR'|| $area === 'cotizador') {
+                        $icono = '<i class="fas fa-dollar-sign"></i>'; // ícono de cotización
                     }
 
                     // Muestra el ícono y el nombre del área
