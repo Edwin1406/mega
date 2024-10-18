2ea03c27c2dc5541540cce16ea8178f5730e1f06
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-18 12:36:14 -0500

    Update index.php

--- views/admin/produccion/cotizador/index.php
+++ views/admin/produccion/cotizador/index.php
@@ -1,7 +1,7 @@
 <h1 class="dashboard__heading"> <?php echo $titulo ?> </h1>
 
 <div class="dashboard__contenedor-boton">
-    <a class="dashboard__boton" href="/admin/area" >
+    <a class="dashboard__boton" href="/admin/produccion/index" >
         <i class="fa-solid fa-circle-arrow-left"></i>
         Volver
     </a>
