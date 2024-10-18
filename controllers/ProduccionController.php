af0da3dbed396c62545b3ba4629d365bce392742
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-18 13:52:29 -0500

    Update ProduccionController.php

--- controllers/ProduccionController.php
+++ controllers/ProduccionController.php
@@ -1,7 +1,6 @@
 <?php
 namespace Controllers;
 
-use Model\Maquinas;
 use Model\Produccion;
 use MVC\Router;
 
@@ -19,14 +18,13 @@ class ProduccionController
             'escoger_produccion' => $escoger_produccion
         ]);
     }
-    
     public static function registro_produccion(Router $router)
     {
        
         session_start();
         isAuth();
         $id= $_SESSION['id'];
-        $escoge_registro = Maquinas::belongsTo('propietarioId',$id);
+        $escoge_registro = Produccion::belongsTo('propietarioId',$id);
         $router->render('admin/produccion/registro_produccion', [
             'titulo' => 'REGISTRO DE PRODUCCION',
             'escoge_registro' => $escoge_registro
