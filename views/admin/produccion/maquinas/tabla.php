262e61f9f0e6ce7362464e5ab63ca96a87d6cefe
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-21 11:47:41 -0500

    Update tabla.php

--- views/admin/produccion/maquinas/tabla.php
+++ views/admin/produccion/maquinas/tabla.php
@@ -30,8 +30,8 @@
                         <td class="table__td"><?php echo $maquina->ancho_maximo?></td>
                         <td class="table__td"><?php echo $maquina->gramaje_maximo?></td>
                         <td class="table__td"><?php echo $maquina->created_at?></td>
-                        <td class="table__td--acciones"><a class="table__accion table__accion--editar" href="/sitioweb/admin/ponentes/editar?id=<?php echo $maquina->id; ?>"><i class="fa-solid fa-user-pen"></i>Editar</a>
-                        <form method="POST" action="/sitioweb/admin/ponentes/eliminar" class="table__formulario">
+                        <td class="table__td--acciones"><a class="table__accion table__accion--editar" href="/admin/maquinas/editar?id=<?php echo $maquina->id; ?>"><i class="fa-solid fa-user-pen"></i>Editar</a>
+                        <form method="POST" action="/admin/maquinas/eliminar" class="table__formulario">
                             <input type="hidden" name="id" value="<?php echo $maquina->id; ?>">
                             <button class="table__accion table__accion--eliminar" type="submit">
                                 <i class="fa-solid fa-user-slash"></i>
