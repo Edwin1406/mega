381efaafe1da112fb907f8e1676328846d0cb06f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-21 11:41:29 -0500

    Update crear.php

--- views/admin/produccion/maquinas/crear.php
+++ views/admin/produccion/maquinas/crear.php
@@ -1,6 +1,6 @@
 <h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
 <div class="dashboard__contenedor-boton">
-    <a class="dashboard__boton" href="/sitioweb/admin/produccion/maquinas/crear">
+    <a class="dashboard__boton" href="/sitioweb/admin/produccion/maquinas/tabla">
         <i class="fa-solid fa-circle-plus"></i>
         TABLA MAQUINAS
     </a>
