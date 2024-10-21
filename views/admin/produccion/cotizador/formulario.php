1ed0e4bb0aa7129e7368507d8a9d2d069362d523
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-21 14:06:48 -0500

    Update formulario.php

--- views/admin/produccion/cotizador/formulario.php
+++ views/admin/produccion/cotizador/formulario.php
@@ -1,11 +1,4 @@
-<h1 class="dashboard__heading"> <?php echo $titulo ?> </h1>
 
-<div class="dashboard__contenedor-boton">
-    <a class="dashboard__boton" href="/admin/produccion/index" >
-        <i class="fa-solid fa-circle-arrow-left"></i>
-        Volver
-    </a>
-</div>
 
 
 <fieldset class="formulario__fieldset">
