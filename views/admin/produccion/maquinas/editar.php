4b0011ed6adf6686bd7ae95cc7ebe4ef9b567ce2
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-21 12:20:18 -0500

    Update editar.php

--- views/admin/produccion/maquinas/editar.php
+++ views/admin/produccion/maquinas/editar.php
@@ -1,7 +1,7 @@
 <h1 class="dashboard__heading"> <?php echo $titulo ?> </h1>
 
 <div class="dashboard__contenedor-boton">
-    <a class="dashboard__boton" href="/sitioweb/admin/ponentes" >
+    <a class="dashboard__boton" href="/admin/produccion/maquinas/tabla?page=1" >
         <i class="fa-solid fa-circle-arrow-left"></i>
         Volver
     </a>
