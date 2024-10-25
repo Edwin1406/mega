1fc2ecf3a60a308f3a7af695356defe81a14e98f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-25 17:04:02 -0500

    Update crear.php

--- views/admin/vendedor/cliente/crear.php
+++ views/admin/vendedor/cliente/crear.php
@@ -11,7 +11,7 @@
 
 <?php include_once __DIR__.'/../../../templates/alertas.php'  ?>
 
-    <form method="POST" action="/admin/produccion/maquinas/crear"  class="formulario" enctype="multipart/form-data">
+    <form method="POST" action="/admin/produccion/vendedor/cliente/crear"  class="formulario" enctype="multipart/form-data">
 
      
     <?php include_once __DIR__.'/formulario.php'  ?>
