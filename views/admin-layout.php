f0ab41d227d6f7a0f6d4a32ab39860b59509d54b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-28 14:10:02 -0500

    Update admin-layout.php

--- views/admin-layout.php
+++ views/admin-layout.php
@@ -10,8 +10,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- <link rel="icon" href="/public/build/img/megastock.jpg" type="image/png"> -->
-    <!-- <link rel="stylesheet" href="/public/build/css/app.css"> -->
+    <link rel="icon" href="/public/build/img/megastock.jpg" type="image/png">
+    <link rel="stylesheet" href="/public/build/css/app.css">
    
    
 </head>
@@ -35,7 +35,7 @@
          
 
 </body>
-  <!-- <script src="/public/build/js/bundle.min.js" defer></script> -->
+  <script src="/public/build/js/bundle.min.js" defer></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js"></script> -->
   
