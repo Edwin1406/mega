4840541d1b001eb768e432bd6f342b4cbb35648d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-28 13:54:56 -0500

    Update cotizador.php

--- views/admin/vendedor/cliente/cotizador.php
+++ views/admin/vendedor/cliente/cotizador.php
@@ -1,5 +1,5 @@
 <h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
 
-<?php if(empty($id)):?>
+<?php if(empty($id==1)):?>
     <?php echo 'se guardo con exito el ccliente' ?>
 <?php endif; ?>
