448a3a4d5f942ec6bafa09b3f389800265fecfae
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-28 14:43:18 -0500

    Update ApiMaquinas.php

--- controllers/ApiMaquinas.php
+++ controllers/ApiMaquinas.php
@@ -9,11 +9,6 @@ class ApiMaquinas {
 
     public static function api()
     {
-        header("Content-Security-Policy: script-src 'self'; object-src 'none';");
-        header('Content-Type: application/json');
-        
-
-
         $maquinas = Maquinas::all();
         echo json_encode($maquinas);
     }
