488a83f06330bb9411fbdc3c6b2b82fdbd52b0dd
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-28 15:19:56 -0500

    .

--- src/js/alertas.js
+++ src/js/alertas.js
@@ -1,12 +1,4 @@
 (function() {
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const respuesta = document.querySelector('aside .dashboard__sidebar');
-        if (respuesta) {
-            respuesta.remove();
-        } else {
-            console.log('Elemento no encontrado');
-        }
-    });
-    
+   alert('Hola Mundo!');
 })();
