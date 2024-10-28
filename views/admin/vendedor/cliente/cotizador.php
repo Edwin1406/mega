e8f71c4e04c71d1bcc2fdbce680768b714ff9b54
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-28 14:49:31 -0500

    Update cotizador.php

--- views/admin/vendedor/cliente/cotizador.php
+++ views/admin/vendedor/cliente/cotizador.php
@@ -2,7 +2,16 @@
 <div class="dashboard__formulario">
 
 <?php include_once __DIR__.'/../../../templates/alertas.php'  ?>
-
+    
+        <form method="POST" action="/admin/vendedor/cliente/crear"  class="formulario
+        " enctype="multipart/form-data">
+            <fieldset>
+                <legend>Información del Cliente</legend>
+                <div class="formulario
+                ">
+                    <label for="nombre">Nombre</label>
+                    <input type="text" name="nombre" id="nombre" placeholder="Nombre del Cliente" value="<?php echo $cliente->nombre ?? '' ?>">
+        </form>
 </div>
 
 
