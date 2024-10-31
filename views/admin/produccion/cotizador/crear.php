0c7b8f16bd541260ced6da2ffe2178c1c90b5451
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-10-31 11:55:23 -0500

    .

--- views/admin/produccion/cotizador/crear.php
+++ views/admin/produccion/cotizador/crear.php
@@ -22,3 +22,25 @@
     </form>
 
 </div>
+
+
+<?php $curl
+=
+curl_init();
+curl_setopt_array($curl, array(
+CURLOPT_URL => 'https://webservices.ec/api/ruc/{1716999303001}',
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_ENCODING => '',
+CURLOPT_MAXREDIRS => 10, 
+CURLOPT_TIMEOUT => 0,
+CURLOPT_FOLLOWLOCATION => true,
+CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+CURLOPT_CUSTOMREQUEST => 'GET',
+CURLOPT_HTTPHEADER => array(
+),
+'Accept: application/json',
+'Authorization: Bearer {E1AojmTPanzGXsvJW4817aByPpluAkWwU7UjDxly}'
+));
+$response
+=
+curl_exec($curl);
