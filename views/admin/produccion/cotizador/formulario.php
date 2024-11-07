db6bad32d511fcf9c90c6377fc0bcf0d2109f276
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-07 09:38:09 -0500

    Update formulario.php

--- views/admin/produccion/cotizador/formulario.php
+++ views/admin/produccion/cotizador/formulario.php
@@ -22,10 +22,13 @@
                         <?php echo s($test->id); ?>">
                         <?php echo  'Ect: ',s($test->ect) ,
                         ' Test: ', s($test->test),
-                        '-------',
+                        ' ---',
                         ' Liner Ext: ', s($test->liner_externo),
+                        ' ---',
                         ' Liner Med: ', s($test->liner_medio),
+                        ' ---',
                         ' Liner Int: ', s($test->liner_interno),
+                        ' ---',
                         ' Peso: ', s($test->peso)
                         ?>
 
