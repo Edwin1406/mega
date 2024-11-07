66190c3563202b1aa37b688b8302533e321a7ae0
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-07 10:23:12 -0500

    Update formulario.php

--- views/admin/produccion/cotizador/formulario.php
+++ views/admin/produccion/cotizador/formulario.php
@@ -45,24 +45,22 @@
         (function(){
             let test ={
             liner_id : '',
-            pedido_id : '',
-            bobinaInterna_id : '',
-            bobinaIntermedia_id : '',
-            bobinaExterna_id : '',
+            pedido_id : ''
+           
             }
 
         const  liner = document.querySelector('[name="liner_id"]')
         const pedido = document.querySelector('[name="pedido_id"]')
-        const bobinaInterna = document.querySelector('[name="bobinaInterna_id"]')
-        const bobinaIntermedia = document.querySelector('[name="bobinaIntermedia_id"]')
-        const bobinaExterna = document.querySelector('[name="bobinaExterna_id"]')
+        // const bobinaInterna = document.querySelector('[name="bobinaInterna_id"]')
+        // const bobinaIntermedia = document.querySelector('[name="bobinaIntermedia_id"]')
+        // const bobinaExterna = document.querySelector('[name="bobinaExterna_id"]')
 
         liner.addEventListener('change', busqueda)
         pedido.addEventListener('change', busqueda)
-        bobinaInterna.addEventListener('change', busqueda)
-        bobinaIntermedia.addEventListener('change', busqueda)
-        bobinaExterna.addEventListener('change', busqueda)
-        
+        // bobinaInterna.addEventListener('change', busqueda)
+        // bobinaIntermedia.addEventListener('change', busqueda)
+        // bobinaExterna.addEventListener('change', busqueda)
+
       function busqueda (e){
         test[e.target.name] = e.target.value.trim()
         console.log(test)
