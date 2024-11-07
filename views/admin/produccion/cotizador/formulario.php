9b011be9be7fd8142ba8a4f5b146b8cbcba466b1
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-07 09:37:12 -0500

    Update formulario.php

--- views/admin/produccion/cotizador/formulario.php
+++ views/admin/produccion/cotizador/formulario.php
@@ -22,6 +22,7 @@
                         <?php echo s($test->id); ?>">
                         <?php echo  'Ect: ',s($test->ect) ,
                         ' Test: ', s($test->test),
+                        '-------',
                         ' Liner Ext: ', s($test->liner_externo),
                         ' Liner Med: ', s($test->liner_medio),
                         ' Liner Int: ', s($test->liner_interno),
