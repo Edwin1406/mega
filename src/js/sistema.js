26deb4b235f13d9f79b803fe88b5f28529e44cd9
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-07 11:27:27 -0500

    .

---
+++ src/js/sistema.js
@@ -0,0 +1,43 @@
+(function(){
+    let test ={
+    liner_id : '',
+    pedido_id : '',
+    pedido2_id : '',
+    bobinaInterna_id : '',
+    bobinaIntermedia_id : '',
+    bobinaExterna_id : ''
+    }
+
+    const pedidos = document.querySelectorAll('#pedido')
+
+// if(pedidos ){
+const  liner = document.querySelector('[name="liner_id"]')
+const pedido = document.querySelector('[name="pedido_id"]')
+const pedido2 = document.querySelector('[name="pedido2_id"]')
+const bobinaInterna = document.querySelector('[name="bobinaInterna_id"]')
+const bobinaIntermedia = document.querySelector('[name="bobinaIntermedia_id"]')
+const bobinaExterna = document.querySelector('[name="bobinaExterna_id"]')
+
+liner.addEventListener('change', busqueda)
+pedido.addEventListener('change', busqueda)
+pedido2.addEventListener('change', busqueda)
+bobinaInterna.addEventListener('change', busqueda)
+bobinaIntermedia.addEventListener('change', busqueda)
+bobinaExterna.addEventListener('change', busqueda)
+
+function busqueda (e){
+
+// los pedidos no pueden ser iguales
+if ((e.target.name === 'pedido_id' || e.target.name === 'pedido2_id') && pedido.value === pedido2.value) {
+    pedido2.value = ''
+    Swal.fire("Pedido ya seleccionado", "No puede seleccionar el mismo pedido", "error");
+
+}else{
+    test[e.target.name] = e.target.value.trim()
+    console.log(test)
+}
+
+}
+
+
+})();
