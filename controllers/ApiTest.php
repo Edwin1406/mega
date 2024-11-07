1697b365e1a75d957b98624757d6d332c104100f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-07 14:47:40 -0500

    Update ApiTest.php

--- controllers/ApiTest.php
+++ controllers/ApiTest.php
@@ -9,16 +9,20 @@ class ApiTest {
 
     public static function api()
     {
-        $liner_id= $_GET['liner_id'] ?? '';
-        $liner_id =filter_var($liner_id, FILTER_VALIDATE_INT);
+        // $liner_id= $_GET['liner_id'] ?? '';
+        // $liner_id =filter_var($liner_id, FILTER_VALIDATE_INT);
 
-        if(!$liner_id){
-            echo json_encode([]);
-            return;
+        // $bobinaExterna_id= $_GET['bobinaExterna_id'] ?? '';
+        // $bobinaExterna_id =filter_var($bobinaExterna_id, FILTER_VALIDATE_INT);
+
+
+        // if(!$liner_id){
+        //     echo json_encode([]);
+        //     return;
             
-        }
+        // }
 
-        $bobinas  = Bobina::whereArray(['id' => $liner_id]);
+        $bobinas  = Bobina::all();
 
         echo json_encode($bobinas);
 
