59f028706e4176f8f5e387548575a47f0a399076
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-07 16:35:53 -0500

    Update ApiTest.php

--- controllers/ApiTest.php
+++ controllers/ApiTest.php
@@ -29,28 +29,37 @@ class ApiTest {
 
 
     public static function apibobinas()
-    {
+{
+    $bobinaInterna_id = $_GET['bobinaInterna_id'] ?? '';
+    $bobinaInterna_id = filter_var($bobinaInterna_id, FILTER_VALIDATE_INT);
 
+    $bobinaExterna_id = $_GET['bobinaExterna_id'] ?? '';
+    $bobinaExterna_id = filter_var($bobinaExterna_id, FILTER_VALIDATE_INT);
 
-        $bobinaInterna_id= $_GET['bobinaInterna_id'] ?? '';
-        $bobinaInterna_id =filter_var($bobinaInterna_id, FILTER_VALIDATE_INT);
-
-        $bobinaExterna_id= $_GET['bobinaExterna_id'] ?? '';
-        $bobinaExterna_id =filter_var($bobinaExterna_id, FILTER_VALIDATE_INT);
-
-
-        if(!$bobinaInterna_id||!$bobinaExterna_id){
-            echo json_encode([]);
-            return;
-            
-        }
-
-        $bobina  = Bobina::where('id',$bobinaInterna_id);
-        $bobinaExterna_id  = Bobina::where('id',$bobinaExterna_id);
-
-        echo json_encode($bobina);
-        echo json_encode($bobinaExterna_id);
+    if (!$bobinaInterna_id && !$bobinaExterna_id) {
+        echo json_encode([]);
+        return;
     }
+
+    $resultados = [];
+
+    if ($bobinaInterna_id) {
+        $bobinaInterna = Bobina::where('id', $bobinaInterna_id);
+        if ($bobinaInterna) {
+            $resultados[] = $bobinaInterna;
+        }
+    }
+
+    if ($bobinaExterna_id) {
+        $bobinaExterna = Bobina::where('id', $bobinaExterna_id);
+        if ($bobinaExterna) {
+            $resultados[] = $bobinaExterna;
+        }
+    }
+
+    echo json_encode($resultados);
+}
+
 
 
 
