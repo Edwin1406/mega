6f0ea96abc57688acaf0aeec54c40602b6d44064
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-07 14:29:38 -0500

    Update ApiTest.php

--- controllers/ApiTest.php
+++ controllers/ApiTest.php
@@ -16,8 +16,14 @@ class ApiTest {
 
     public static function apibobinas()
     {
-        $bobina_interna = Bobina::all();
-        echo json_encode($bobina_interna);
+        $liner_id= $_GET['liner_id'] ?? '';
+        $liner_id =filter_var($liner_id, FILTER_VALIDATE_INT);
+
+        if(!$liner_id){
+            echo json_encode([]);
+            
+        }
+
     }
 
 
