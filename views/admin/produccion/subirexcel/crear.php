8d32a1f7d3e66a6e5c7548e544d037c39492578b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-08 09:28:11 -0500

    Update crear.php

--- views/admin/produccion/subirexcel/crear.php
+++ views/admin/produccion/subirexcel/crear.php
@@ -1,9 +1,10 @@
 <h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
 
-
+<div class="dashboard__formulario">
 
 <form method="POST" action="/admin/produccion/papel/crear"  class="formulario" enctype="multipart/form-data">
     <label for="file">Subir archivo Excel:</label>
     <input type="file" name="file" id="file" accept=".xlsx, .xls">
     <input type="submit" name="submit" value="Subir">
 </form>
+</div>
