b34c7a49332dfc9e1191676db01e4b29a08ab86d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-08 09:27:28 -0500

    Update crear.php

--- views/admin/produccion/subirexcel/crear.php
+++ views/admin/produccion/subirexcel/crear.php
@@ -2,7 +2,7 @@
 
 
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<form method="POST" action="/admin/produccion/papel/crear"  class="formulario" enctype="multipart/form-data">
     <label for="file">Subir archivo Excel:</label>
     <input type="file" name="file" id="file" accept=".xlsx, .xls">
     <input type="submit" name="submit" value="Subir">
