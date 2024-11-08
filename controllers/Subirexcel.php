d8a652fce0ede10ca1d8374551e688b06d376362
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-08 09:32:10 -0500

    .

--- controllers/Subirexcel.php
+++ controllers/Subirexcel.php
@@ -6,6 +6,29 @@ use MVC\Router;
 class Subirexcel {
     public static function subirexcel(Router $router)
     {
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $archivo = $_FILES['file'];
+            $nombreArchivo = $archivo['name'];
+            $tipoArchivo = $archivo['type'];
+            $tamanoArchivo = $archivo['size'];
+            $tempArchivo = $archivo['tmp_name'];
+            $error = $archivo['error'];
+
+            if ($error === 0) {
+                $ext = pathinfo($nombreArchivo, PATHINFO_EXTENSION);
+                if ($ext === 'xlsx' || $ext === 'xls') {
+                    move_uploaded_file($tempArchivo, __DIR__ . "/../uploads/$nombreArchivo");
+                    echo 'Archivo subido correctamente';
+                } else {
+                    echo 'Solo se permiten archivos de excel';
+                }
+            } else {
+                echo 'Hubo un error al subir el archivo';
+            }
+        }
+
+
         $router->render('admin/produccion/subirexcel/crear', [
             'titulo' => 'Subir Excel'
         ]);
--- views/admin/produccion/subirexcel/crear.php
+++ views/admin/produccion/subirexcel/crear.php
@@ -2,7 +2,7 @@
 
 <div class="dashboard__formulario">
 
-<form method="POST" action="/admin/produccion/papel/crear"  class="formulario" enctype="multipart/form-data">
+<form method="POST" action="/admin/produccion/subirexcel/crear"  class="formulario" enctype="multipart/form-data">
     <label for="file">Subir archivo Excel:</label>
     <input type="file" name="file" id="file" accept=".xlsx, .xls">
     <input type="submit" name="submit" value="Subir">
