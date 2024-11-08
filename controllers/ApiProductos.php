ad2625628b88f85209d86cd32dcca479de09593d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-08 11:21:47 -0500

    Update ApiProductos.php

--- controllers/ApiProductos.php
+++ controllers/ApiProductos.php
@@ -8,11 +8,18 @@ class ApiProductos {
 
     public static function productos()
     {
-        $productos = Producto::all();
+         // Obtener todos los productos
+         $productos = Producto::all();
 
-        foreach($productos as $producto){
-            $producto->total = Producto::topProductos( $producto->id);
-        }
-        echo json_encode($productos);
+         // Iterar sobre los productos y obtener el total para cada uno
+         foreach ($productos as $producto) {
+             // Llamar a topProductos y pasar el ID correctamente
+             // Asegúrate de que el id es numérico y se pasa como parámetro adecuado
+             $producto->total = Producto::topProductos(['id_producto' => $producto->id]);
+         }
+ 
+         // Mostrar el resultado en formato JSON
+         echo json_encode($productos);
+     
     }
 }
