809cbe01d3694bb224a3e5b1f45be4fa40f3fb45
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-11 11:04:30 -0500

    Update formulario.php

--- views/admin/produccion/cotizador/formulario.php
+++ views/admin/produccion/cotizador/formulario.php
@@ -203,10 +203,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simulador de Cobertura de Bobinas</title>
+    <title>Bobinas</title>
 </head>
 <body>
-    <h2>Simulador de Cobertura de Bobinas</h2>
+    <h2>Bobinas</h2>
     <div id="resultados"></div>
 
     <script>
