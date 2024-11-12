a113d164585524d94f752b158fe99ca085c5c1fd
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-12 09:35:11 -0500

    Update formulario.php

--- views/admin/produccion/cotizador/formulario.php
+++ views/admin/produccion/cotizador/formulario.php
@@ -181,7 +181,7 @@
             id="gramaje_total"
             class="formulario__input"
             placeholder="gramaje_total del papel"
-            value="<?php echo $papel->gramaje_total ?? '' ?>">
+            value="">
     </div>
     <div class="formulario__campo">
         <label class="formulario__label" for="estado_combinacion">Estado de combinacion</label>
