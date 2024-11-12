9de3038258191af26c4e71d8d534ed6225dbe353
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-12 13:17:11 -0500

    .

--- src/js/cotizador.js
+++ src/js/cotizador.js
@@ -292,7 +292,10 @@
             console.log("Las tres bobinas ideales son:", tresBobinasIdeales);
             // Mostrar las bobinas ideales en la consola
                 const bobinaideal = document.getElementById("bobinaideal");
-                bobinaideal.innerHTML = '';
+                bobinaideal.innerHTML = tresBobinasIdeales.map(bobina => {
+                    return `<option value="${bobina.id}">${bobina.ancho} mm</option>`;
+                } );
+            
         } else {
             console.log("No se encontraron bobinas que cumplan con los requisitos.");
         }
