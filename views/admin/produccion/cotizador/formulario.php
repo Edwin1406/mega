096adf44f8765acdf61c0b6ee45317b85b0ac0b7
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-13 16:14:22 -0500

    Update formulario.php

--- views/admin/produccion/cotizador/formulario.php
+++ views/admin/produccion/cotizador/formulario.php
@@ -76,7 +76,7 @@
 
 <script>
      $(document).ready(function() {
-        $('#pedido').select2({
+        $('pedido_id').select2({
             placeholder: "-- Seleccione --",
             allowClear: false
         });
