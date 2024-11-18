134611fa1816f2f4d37b40cb062e6d7b0305b02c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-18 15:31:46 -0500

    .

--- src/js/cotizador.js
+++ src/js/cotizador.js
@@ -426,7 +426,7 @@
             // Buscar un ancho cercano al seleccionado
             const anchoEncontrado = anchosFiltrados.find(todos => 
                 todos.id !== pedidoSeleccionado.id && // Excluye el propio pedido seleccionado
-                todos.ancho >= seleccionado - 500 && todos.ancho <= seleccionado + 500
+                todos.ancho >= seleccionado - 1000 && todos.ancho <= seleccionado + 1000
             );
     
             if (anchoEncontrado) {
