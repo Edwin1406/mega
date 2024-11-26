3633aa78028e206984cfc9acacedd90e73e5392c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-26 15:56:59 -0500

    Update formulario.php

--- views/admin/produccion/cotizador/formulario.php
+++ views/admin/produccion/cotizador/formulario.php
@@ -305,7 +305,7 @@
     // Mostrar los resultados
     mostrarResultados(objetoResultados);
     console.log("Resultados finales:", objetoResultados); // Mostrar el objeto en la consola
-    console.log("resuñtados", objetoResultados.resultados);
+    console.log("resultados", objetoResultados.resultados);
 }
 
 function mostrarResultados(objetoResultados) {
