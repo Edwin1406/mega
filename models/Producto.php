b2357760b076885180148d2385825fb568a237f5
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-27 12:45:36 -0500

    Update Producto.php

--- models/Producto.php
+++ models/Producto.php
@@ -4,18 +4,40 @@ namespace Model;
 class Producto extends ActiveRecord
 {
     protected static $tabla = 'productos';
-    protected static $columnasDB = ['id', 'nombre', 'cantidad', 'fecha'];
+    protected static $columnasDB = ['id', 'almacen', 'nombre_cliente', 'ruc_cliente','fecha_pedido','vendedor','plazo_entrega','estado_pedido','codigo_producto','nombre_producto','cantidad','pvp','subtotal','total'];
 
     public $id;
-    public $nombre;
+    public $almacen;
+    public $nombre_cliente;
+    public $ruc_cliente;
+    public $fecha_pedido;
+    public $vendedor;
+    public $plazo_entrega;
+    public $estado_pedido;
+    public $codigo_producto;
+    public $nombre_producto;
     public $cantidad;
-    public $fecha;
+    public $pvp;
+    public $subtotal;
+    public $total;
+
 
     public function __construct($args = [])
     {
         $this->id = $args['id'] ?? null;
-        $this->nombre = $args['nombre'] ?? '';
-        $this->cantidad = $args['cantidad'] ?? 0;
-        $this->fecha = $args['fecha'] ?? date('Y-m-d');
+        $this->almacen = $args['almacen'] ?? '';
+        $this->nombre_cliente = $args['nombre_cliente'] ?? '';
+        $this->ruc_cliente = $args['ruc_cliente'] ?? '';
+        $this->fecha_pedido = $args['fecha_pedido'] ?? '';
+        $this->vendedor = $args['vendedor'] ?? '';
+        $this->plazo_entrega = $args['plazo_entrega'] ?? '';
+        $this->estado_pedido = $args['estado_pedido'] ?? '';
+        $this->codigo_producto = $args['codigo_producto'] ?? '';
+        $this->nombre_producto = $args['nombre_producto'] ?? '';
+        $this->cantidad = $args['cantidad'] ?? '';
+        $this->pvp = $args['pvp'] ?? '';
+        $this->subtotal = $args['subtotal'] ?? '';
+        $this->total = $args['total'] ?? '';
+        
     }
 }
