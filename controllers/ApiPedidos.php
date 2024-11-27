8bd5550214b7f4310e71fa0a9b901d167344f57c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-27 15:28:08 -0500

    .

--- controllers/ApiPedidos.php
+++ controllers/ApiPedidos.php
@@ -52,7 +52,7 @@ class ApiPedidos {
         header("Access-Control-Allow-Origin: *");  // Permite solicitudes desde cualquier origen
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Métodos permitidos
         header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Cabeceras permitidas
-        $pedidos = Producto::all('DESC');
+        $pedidos = Producto::all('ASC');
         echo json_encode($pedidos);
     }
 
