b3cf76de6987aede1713a190cd969bcbdd1a8d21
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-27 14:47:52 -0500

    Update Producto.php

--- models/Producto.php
+++ models/Producto.php
@@ -4,12 +4,13 @@ namespace Model;
 class Producto extends ActiveRecord
 {
     protected static $tabla = 'productos';
-    protected static $columnasDB = ['id', 'almacen', 'nombre_cliente', 'ruc_cliente','fecha_pedido','vendedor','plazo_entrega','estado_pedido','codigo_producto','nombre_producto','cantidad','pvp','subtotal','total'];
+    protected static $columnasDB = ['id', 'almacen', 'nombre_cliente', 'ruc_cliente','numero_pedido','fecha_pedido','vendedor','plazo_entrega','estado_pedido','codigo_producto','nombre_producto','cantidad','pvp','subtotal','total'];
 
     public $id;
     public $almacen;
     public $nombre_cliente;
     public $ruc_cliente;
+    public $numero_pedido;
     public $fecha_pedido;
     public $vendedor;
     public $plazo_entrega;
@@ -28,6 +29,7 @@ class Producto extends ActiveRecord
         $this->almacen = $args['almacen'] ?? '';
         $this->nombre_cliente = $args['nombre_cliente'] ?? '';
         $this->ruc_cliente = $args['ruc_cliente'] ?? '';
+        $this->numero_pedido = $args['numero_pedido'] ?? '';
         $this->fecha_pedido = $args['fecha_pedido'] ?? '';
         $this->vendedor = $args['vendedor'] ?? '';
         $this->plazo_entrega = $args['plazo_entrega'] ?? '';
@@ -38,6 +40,6 @@ class Producto extends ActiveRecord
         $this->pvp = $args['pvp'] ?? '';
         $this->subtotal = $args['subtotal'] ?? '';
         $this->total = $args['total'] ?? '';
-        
+
     }
 }
