27e8887ec2a11fd655ff78fb6703c8351428db3c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-11-28 13:20:39 -0500

    .

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -395,7 +395,7 @@ public static function procesarArchivoExcel($filePath)
         }, $data);
 
         // Comprobar si el nombre_producto comienza con alguno de los valores no deseados
-        if (preg_match('/^(LM|CIRELES|CHRYSAL|Z|GUANTE)/', $nombre_producto)) {
+        if (preg_match('/^(|CIRELES|CHRYSAL|Z|GUANTE)/', $nombre_producto)) {
             // Si coincide, se omite el registro
             continue;
         }
