410b164f7da940fda85a185d9b3f61b2ed904d45
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-16 14:54:16 -0500

    .

--- controllers/ApiTest.php
+++ controllers/ApiTest.php
@@ -85,6 +85,9 @@ class ApiTest {
     }
 
     public static function allbobinas(){
+        header("Access-Control-Allow-Origin: *");  // Permite solicitudes desde cualquier origen
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Métodos permitidos
+        header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Cabeceras permitidas
         $bobinas = Bobina::all();
         echo json_encode($bobinas);
     }
