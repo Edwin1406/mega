e69f4731c4d812468d30f4870b5f55edaa994a9b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-18 15:53:37 -0500

    Update cotizador.php

--- views/admin/vendedor/cliente/cotizador.php
+++ views/admin/vendedor/cliente/cotizador.php
@@ -1,45 +1,58 @@
-<h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
+<h2 class="dashboard__heading"><?php echo $titulo ?></h2>
 
 <div class="dashboard__contenedor-boton">
     <a class="dashboard__boton" href="/admin/produccion/registro_produccion">
-    <i class="fa-solid fa-circle-arrow-left"></i>
-        REGRESAR A INICIO
+        <i class="fa-solid fa-circle-arrow-left"></i> REGRESAR A INICIO
     </a>
-
 </div>
 
+<!-- Formulario de Búsqueda -->
+<form method="GET" action="" class="dashboard__formulario-busqueda">
+    <input type="text" name="codigo" placeholder="Buscar por código..." 
+           value="<?php echo htmlspecialchars($_GET['codigo'] ?? '') ?>" class="dashboard__input-busqueda">
+    <button type="submit" class="dashboard__boton-busqueda">Buscar</button>
+</form>
+
 <div class="dashboard__contenedor">
-    <?php if (!empty($visor)): ?>
+    <?php 
+    // Filtrar resultados si se proporciona un código en la búsqueda
+    $codigoBuscado = $_GET['codigo'] ?? '';
+    $resultadosFiltrados = [];
+
+    if (!empty($codigoBuscado)) {
+        foreach ($visor as $maquina) {
+            if (stripos($maquina->codigo, $codigoBuscado) !== false) {
+                $resultadosFiltrados[] = $maquina;
+            }
+        }
+    } else {
+        $resultadosFiltrados = $visor; // Mostrar todos si no hay búsqueda
+    }
+    ?>
+
+    <?php if (!empty($resultadosFiltrados)): ?>
         <table class="table">
             <thead class="table__thead">
                 <tr>
-                    <th scope="col" class="table__th">Codigo</th>
+                    <th scope="col" class="table__th">Código</th>
                     <th scope="col" class="table__th">Nombre</th>
                     <th scope="col" class="table__th">Imagen</th>
                 </tr>
             </thead>
             <tbody class="table__tbody">
-
-                <?php foreach ($visor as $maquina):?>
+                <?php foreach ($resultadosFiltrados as $maquina): ?>
                     <tr class="table__tr">
-                        <td class="table__td"><?php echo $maquina->codigo?></td>
-                        <td class="table__td"><?php echo $maquina->nombre?></td>
-                        <td class="table__td"><?php echo $maquina->imagen?></td>
+                        <td class="table__td"><?php echo htmlspecialchars($maquina->codigo) ?></td>
+                        <td class="table__td"><?php echo htmlspecialchars($maquina->nombre) ?></td>
+                        <td class="table__td">
+                            <img src="/src/visor/<?php echo htmlspecialchars($maquina->imagen) ?>" 
+                                 alt="Imagen" style="width: 100px; height: auto;">
                         </td>
-
-
                     </tr>
-                <?php endforeach;?>
-
+                <?php endforeach; ?>
             </tbody>
-
         </table>
-   
-
-
     <?php else: ?>
-        <a class="text-center"> No hay visor Aún</a>
+        <p class="text-center">No se encontraron resultados para "<?php echo htmlspecialchars($codigoBuscado) ?>".</p>
     <?php endif; ?>
 </div>
-
-<?php echo $paginacion; ?>
