94ca444c254f754f4f805c0cdcf4ef274fd03c46
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-18 15:08:37 -0500

    Update formulario.php

--- views/admin/vendedor/cliente/formulario.php
+++ views/admin/vendedor/cliente/formulario.php
@@ -1,5 +1,17 @@
 <fieldset class="formulario__fieldset">
     <legend class="formulario__legend">Información del cliente</legend>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="codigo">Codigo</label>
+        <input
+            type="text"
+            name="codigo"
+            id="codigo"
+            class="formulario__input"
+            placeholder="Codigo del cliente"
+            value="<?php echo $cliente->codigo ?? '' ?>">
+    </div>
+
     <div class="formulario__campo">
         <label class="formulario__label" for="nombre">Nombre</label>
         <input
@@ -10,82 +22,7 @@
             placeholder="Nombre del cliente"
             value="<?php echo $cliente->nombre ?? '' ?>">
     </div>
-    <div class="formulario__campo">
-        <label class="formulario__label" for="apellido">Apellido</label>
-        <input
-            type="text"
-            name="apellido"
-            id="apellido"
-            class="formulario__input"
-            placeholder="apellido del cliente"
-            value="<?php echo $cliente->apellido ?? '' ?>">
-    </div>
-
-    <div class="formulario__campo">
-        <label class="formulario__label" for="ruc">Ruc o cédula</label>
-        <input
-            type="text"
-            name="ruc"
-            id="ruc"
-            class="formulario__input"
-            placeholder="ingrese cedula o ruc"
-            value="<?php echo $cliente->ruc ?? '' ?>">
-    </div>
-
-    <div class="formulario__campo">
-        <label class="formulario__label" for="telefono">Telefono</label>
-        <input
-            type="text"
-            name="telefono"
-            id="telefono"
-            class="formulario__input"
-            placeholder="telefono del cliente"
-            value="<?php echo $cliente->telefono ?? '' ?>">
-    </div>
-    <div class="formulario__campo">
-        <label class="formulario__label" for="email">Email</label>
-        <input
-            type="text"
-            name="email"
-            id="email"
-            class="formulario__input"
-            placeholder="email del cliente"
-            value="<?php echo $cliente->email ?? '' ?>">
-    </div>
-
-    <div class="formulario__campo">
-        <label class="formulario__label" for="direccion">Direcciòn</label>
-        <input
-            type="text"
-            name="direccion"
-            id="direccion"
-            class="formulario__input"
-            placeholder="Direccion del cliente"
-            value="<?php echo $cliente->direccion ?? '' ?>">
-    </div>
-  
-    <div class="formulario__campo">
-        <label class="formulario__label" for="pais">Pais</label>
-        <input
-            type="text"
-            name="pais"
-            id="pais"
-            class="formulario__input"
-            placeholder="pais del cliente"
-            value="<?php echo $cliente->pais ?? '' ?>">
-    </div>
-    <div class="formulario__campo">
-        <label class="formulario__label" for="ciudad">Ciudad</label>
-        <input
-            type="text"
-            name="ciudad"
-            id="ciudad"
-            class="formulario__input"
-            placeholder="Ciudad del cliente"
-            value="<?php echo $cliente->gramaje_maximo ?? '' ?>">
-    </div>
-
-    <!-- subir archivos  -->
+   
 
     <div class="formulario__campo">
         <label class="formulario__label" for="imagen">Imagen</label>
