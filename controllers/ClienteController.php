61704bd77ff988be09a4f710638671b20addbbe1
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-18 15:38:42 -0500

    Update ClienteController.php

--- controllers/ClienteController.php
+++ controllers/ClienteController.php
@@ -35,38 +35,34 @@ class ClienteController
                 $nombreImagen = preg_replace('/[^a-zA-Z0-9._-]/', '_', $imagen['name']); // Limpia el nombre del archivo
                 $archivo = $imagen['tmp_name'];
                 $cliente->imagen = $nombreImagen;
-                
-                // Ruta destino: carpeta 'src/visor'
-                $destino = $_SERVER['DOCUMENT_ROOT'] . '/src/visor/';
-                
-                // Verificar si la carpeta 'visor' existe, si no, crearla
-                if (!file_exists($destino)) {
-                    mkdir($destino, 0777, true); // Crear carpeta con permisos adecuados
-                }
-                
-                // Mover el archivo a la carpeta 'src/visor'
-                if (move_uploaded_file($archivo, $destino . $nombreImagen)) {
-                    echo "Archivo subido correctamente a la carpeta 'visor'.";
-                    echo "URL de la imagen: https://megawebsistem.com/src/visor/$nombreImagen";
-                } else {
-                    echo "Error al subir el archivo a la carpeta 'visor'.";
-                }
-                
-                
-                // debuguear($cliente);
-        
+                        
                 if($codigo || $nombre) {
+                    // Ruta destino: carpeta 'src/visor'
+                    $destino = $_SERVER['DOCUMENT_ROOT'] . '/src/visor/';
+                    
+                    // Verificar si la carpeta 'visor' existe, si no, crearla
+                    if (!file_exists($destino)) {
+                        mkdir($destino, 0777, true); // Crear carpeta con permisos adecuados
+                    }
+                    
+                    // Mover el archivo a la carpeta 'src/visor'
+                    if (move_uploaded_file($archivo, $destino . $nombreImagen)) {
+                        echo "Archivo subido correctamente a la carpeta 'visor'.";
+                        echo "URL de la imagen: https://megawebsistem.com/src/visor/$nombreImagen";
+                    } else {
+                        echo "Error al subir el archivo a la carpeta 'visor'.";
+                    }
                     Cliente::setAlerta('error', 'El Cliente  y Ruc ya estan registrados');
                     $alertas = Cliente::getAlertas();
                 } else {
                     $cliente->guardar();
                     header('Location: /admin/vendedor/cliente/cotizador?id=1');
                 }
-        }
-              
-
+            }
             
-
+            
+            
+            
         }
 
          // Render a la vista
