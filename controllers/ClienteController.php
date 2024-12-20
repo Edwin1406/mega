fcaf4a9e072621d074472d7eb3695b0c86852098
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-20 09:27:51 -0500

    Update ClienteController.php

--- controllers/ClienteController.php
+++ controllers/ClienteController.php
@@ -24,7 +24,7 @@ class ClienteController
             exit;
         }
         
-        $pagina_por_registros = 10;
+        $pagina_por_registros = 100;
         $total = Cliente:: total();
         $paginacion = new Paginacion($pagina_actual, $pagina_por_registros, $total);
         if($paginacion->total_paginas() < $pagina_actual){
