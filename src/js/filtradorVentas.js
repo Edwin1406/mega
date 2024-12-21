9a191201d12dc092e3a622734b5a864898822709
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-21 12:38:30 -0500

    .

--- src/js/filtradorVentas.js
+++ src/js/filtradorVentas.js
@@ -1,31 +1,23 @@
 // Objetivo: Filtrar ventas en tiempo real
 (function() {
 
-    
-        // Verificar si el elemento con ID 'filtros_ventas' existe
-        const filtroInput = document.querySelector('#filtros_ventas');
-        if (filtroInput) {
-            // Añadir el evento 'input' solo si el elemento existe
-            filtroInput.addEventListener('input', function () {
-                const filtro = this.value.toLowerCase();
-                const filas = document.querySelectorAll('#tabla .table__tr');
-    
-                filas.forEach(fila => {
-                    const codigo = fila.cells[0].textContent.toLowerCase();
-                    const nombre = fila.cells[1].textContent.toLowerCase();
-    
-                    if (codigo.includes(filtro) || nombre.includes(filtro)) {
-                        fila.style.display = '';
-                    } else {
-                        fila.style.display = 'none';
-                    }
-                });
-            });
-        } else {
-            console.log("El elemento con ID 'filtros_ventas' no se encontró en esta página.");
-        }
+   // Filtro en tiempo real
+   document.querySelector('#filtros_ventas').addEventListener('input', function () {
+    const filtro = this.value.toLowerCase();
+    const filas = document.querySelectorAll('#tabla .table__tr');
 
-    
+    filas.forEach(fila => {
+        const codigo = fila.cells[0].textContent.toLowerCase();
+        const nombre = fila.cells[1].textContent.toLowerCase();
+
+        if (codigo.includes(filtro) || nombre.includes(filtro)) {
+            fila.style.display = '';
+        } else {
+            fila.style.display = 'none';
+        }
+    });
+});
+
 })();
 
 
