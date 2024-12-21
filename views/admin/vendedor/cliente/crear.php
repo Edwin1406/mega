011b874ccf3e43d79999631389030dddd4907762
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-21 12:48:13 -0500

    Update crear.php

--- views/admin/vendedor/cliente/crear.php
+++ views/admin/vendedor/cliente/crear.php
@@ -1,6 +1,6 @@
 <h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
 <div class="dashboard__contenedor-boton">
-    <a class="dashboard__boton" href="/admin/vendedor/cliente/cotizador">
+    <a class="dashboard__boton" href="/admin/vendedor/cliente/tabla">
     <i class="fa-regular fa-eye"></i>
         VER ARCHIVO 
     </a>
