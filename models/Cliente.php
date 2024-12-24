16a36344f3e202eef985a4f9a4be92d8850316eb
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-24 08:41:54 -0500

    Update Cliente.php

--- models/Cliente.php
+++ models/Cliente.php
@@ -7,11 +7,12 @@ use Classes\ValidarCedula;
 class Cliente extends ActiveRecord {
 
     protected static $tabla = 'visor';
-    protected static $columnasDB = ['id', 'nombre_cliente','nombre_producto','estado','pdf'];
+    protected static $columnasDB = ['id', 'nombre_cliente','nombre_producto','codigo_producto','estado','pdf'];
 
     public $id;
     public $nombre_cliente;
     public $nombre_producto;
+    public $codigo_producto;
     public $estado;
     public $pdf;
     public $pdf_actual; // Definir la propiedad explícitamente
@@ -22,6 +23,7 @@ class Cliente extends ActiveRecord {
         $this->id = $args['id'] ?? null;
         $this->nombre_cliente = $args['nombre_cliente'] ?? '';
         $this->nombre_producto = $args['nombre_producto'] ?? '';
+        $this->codigo_producto = $args['codigo_producto'] ?? '';
         $this->estado = $args['estado'] ?? 'pendiente';
     }
 
@@ -37,6 +39,10 @@ class Cliente extends ActiveRecord {
 
         if(!$this->nombre_producto) {
             self::$alertas['error'][] = 'El Campo Nombre es Obligatorio';
+        }
+
+        if(!$this->codigo_producto) {
+            self::$alertas['error'][] = 'El Campo Codigo Producto es Obligatorio';
         }
        
         return self::$alertas;
