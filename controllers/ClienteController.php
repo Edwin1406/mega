3b997a277e4f6c1218cb1a026366d11b29d2e99b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-27 09:08:35 -0500

    Update ClienteController.php

--- controllers/ClienteController.php
+++ controllers/ClienteController.php
@@ -195,7 +195,7 @@ if (!empty($_FILES['pdf']['tmp_name'])) {
 }
 
 
-public function nombreCliente (Router $router){
+public static function nombreCliente (Router $router){
     $indefinidoId = $_GET['id'];
     if(!$indefinidoId) header('Location: /');
     $clientes = Area::where('url', $indefinidoId);
