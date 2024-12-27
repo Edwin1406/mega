b0b9a35fb784fe03915bc44ea96cb716c4f37891
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-27 13:14:11 -0500

    Update tabla.php

--- views/admin/vendedor/cliente/tabla.php
+++ views/admin/vendedor/cliente/tabla.php
@@ -134,7 +134,8 @@
             const url =`${location.origin}/admin/api/nombreCliente?id=${idVisor}`
             const resultado = await fetch(url);
             const visor = await resultado.json();
-            console.log(visor);
+            // console.log(visor);
+            return visor;
           
 
           
