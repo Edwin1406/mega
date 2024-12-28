2f72231ddae9fb4c64d6383b78bdc4d848ce6be5
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-28 11:27:00 -0500

    Update tabla.php

--- views/admin/vendedor/cliente/tabla.php
+++ views/admin/vendedor/cliente/tabla.php
@@ -131,15 +131,31 @@
 
     }
 
+
+
     let visores = [];
-    
+    apiVisor();
+    async function api( idVisor) {
+        try {
+            const url =`${location.origin}/admin/api/nombreCliente?id=${idVisor}`
+            const respuesta = await fetch(url);
+            const resultado = await resultado.json();
+            visores = resultado.visor;
+            console.log(visores);
+           
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
 
     async function Apivisor( idVisor) {
         try {
             const url =`${location.origin}/admin/api/nombreCliente?id=${idVisor}`
             const resultado = await fetch(url);
             const visor = await resultado.json();
-            // 
+            
 
             const nuevoEstado = visor.estado === "ENVIADO" ? "PAUSADO" : "TERMINADO";
             visor.estado = nuevoEstado;
