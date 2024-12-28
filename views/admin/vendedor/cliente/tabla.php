5ac85cce5677c50269784d483a03639d2bf0f018
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-28 12:31:38 -0500

    Update tabla.php

--- views/admin/vendedor/cliente/tabla.php
+++ views/admin/vendedor/cliente/tabla.php
@@ -171,7 +171,9 @@
             });
             const resultado = await respuesta.json();
             if(resultado.respuesta.tipo === 'correcto'){
-                // actualizar el DOM                // colores de estado
+                // actualizar el DOM
+                document.querySelector(`[data-id="${id}"]`).textContent = estado; 
+                // colores de estado
                 document.querySelector(`[data-id="${id}"]`).style.color = estado === 'ENVIADO' ? 'green' : estado === 'PAUSADO' ? 'red' : 'orange';
 
                 
