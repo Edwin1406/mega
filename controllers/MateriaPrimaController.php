d8a9df5b0197985abb5026f9be723611e07477ba
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-30 09:58:24 -0500

    Update MateriaPrimaController.php

--- controllers/MateriaPrimaController.php
+++ controllers/MateriaPrimaController.php
@@ -11,18 +11,18 @@ class MateriaPrimaController
 {
    public static function materia(Router $router)
    {
+      $alertas = [];
+      $materiaprima = new MateriaPrima;
 
-         $materiaprima = new MateriaPrima;
       if($_SERVER['REQUEST_METHOD'] === 'POST') {
          $materiaprima->sincronizar($_POST);
          debuguear($materiaprima);
 
       }
 
-      $alertas = [];
-       $router->render('admin/produccion/materia/crear' , [
-           'titulo' => 'MEGASTOCK-MATERIA PRIMA',
-             'alertas' => $alertas
-       ]);
+      $router->render('admin/produccion/materia/crear' , [
+         'titulo' => 'MEGASTOCK-MATERIA PRIMA',
+         'alertas' => $alertas
+      ]);
    }
 }
