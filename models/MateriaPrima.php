a3175f5bf514e28085759ca8f75b28c11c8864b6
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2024-12-30 09:54:01 -0500

    Create MateriaPrima.php

---
+++ models/MateriaPrima.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Model;
+
+class MateriaPrima extends ActiveRecord
+{
+    protected static $tabla = 'materias_prima';
+    protected static $columnasDB = ['id', 'tipo', 'ancho','barcode','peso','created_at'];
+
+    public $id;
+    public $tipo;
+    public $ancho;
+    public $barcode;
+    public $peso;
+    public $created_at;
+
+    public function __construct($args = [])
+    {
+        date_default_timezone_set('America/Guayaquil');
+
+        $this->id = $args['id'] ?? null;
+        $this->tipo = $args['tipo'] ?? '';
+        $this->ancho = $args['ancho'] ?? '';
+        $this->barcode = $args['barcode'] ?? '';
+        $this->peso = $args['peso'] ?? '';
+        $this->created_at = $args['created_at'] ?? date('Y-m-d H:i:s');
+    }
+
+
+    public function validar() {
+
+        if(!$this->tipo) {
+            self::$alertas['error'][] = 'El Campo Numero de Cuchillas es Obligatorio';
+        }
+
+        if(!$this->ancho) {
+            self::$alertas['error'][] = 'El Campo Ancho Maximo es Obligatorio';
+        }
+
+        if(!$this->barcode) {
+            self::$alertas['error'][] = 'El Campo Gramaje Maximo es Obligatorio';
+        }
+
+        if(!$this->peso) {
+            self::$alertas['error'][] = 'El Campo Gramaje Maximo es Obligatorio';
+        }
+
+        return self::$alertas;
+
+    }
+
+
+
+}
+
+
+
+?>
