d23877d3aded231ae6e9666bea1f84a1467d73b0
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-03 11:46:43 -0500

    .

--- src/js/filtradorVentas.js
+++ src/js/filtradorVentas.js
@@ -1,22 +1,28 @@
-filtroVentas.addEventListener('input', function () {
-    const filtro = this.value.toLowerCase();
-    const filas = document.querySelectorAll('#tabla .table__tr');
+document.addEventListener('DOMContentLoaded', function () {
+    const filtroVentas = document.querySelector('#filtros_ventas');
+    if (filtroVentas) {
+        filtroVentas.addEventListener('input', function () {
+            const filtro = this.value.toLowerCase();
+            const filas = document.querySelectorAll('#tabla .table__tr');
 
-    filas.forEach(fila => {
-        const nombreCliente = fila.cells[0].textContent.toLowerCase();
-        const nombreProducto = fila.cells[1].textContent.toLowerCase();
-        const codigoProducto = fila.cells[2].textContent.toLowerCase();
-        const estado = fila.cells[3].textContent.toLowerCase();
+            filas.forEach(fila => {
+                const nombreCliente = fila.cells[0].textContent.toLowerCase();
+                const nombreProducto = fila.cells[1].textContent.toLowerCase();
+                const codigoProducto = fila.cells[2].textContent.toLowerCase();
+                const estado = fila.cells[3].textContent.toLowerCase();
 
-        if (
-            nombreCliente.includes(filtro) || 
-            nombreProducto.includes(filtro) || 
-            codigoProducto.includes(filtro) || 
-            estado.includes(filtro)
-        ) {
-            fila.style.display = '';
-        } else {
-            fila.style.display = 'none';
-        }
-    });
+                // Condición para mostrar u ocultar la fila
+                if (
+                    nombreCliente.includes(filtro) || 
+                    nombreProducto.includes(filtro) || 
+                    codigoProducto.includes(filtro) || 
+                    estado.includes(filtro)
+                ) {
+                    fila.style.display = ''; // Mostrar fila
+                } else {
+                    fila.style.display = 'none'; // Ocultar fila
+                }
+            });
+        });
+    }
 });
