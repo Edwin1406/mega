d5380a1e6278f74a2fefdb328a3a444638b221b7
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-03 12:09:19 -0500

    .

--- src/js/filtradorVentas.js
+++ src/js/filtradorVentas.js
@@ -1,27 +1,31 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     // Objetivo: Filtrar ventas en tiempo real
-//     (function() {
-//         // Filtro en tiempo real
-//         const filtroVentas = document.querySelector('#filtros_ventas');
-//         if (filtroVentas) {
-//             filtroVentas.addEventListener('input', function () {
-//                 const filtro = this.value.toLowerCase();
-//                 const filas = document.querySelectorAll('#tabla .table__tr');
+document.addEventListener('DOMContentLoaded', function () {
+    const filtroVentas = document.querySelector('#filtros_ventas');
+    if (filtroVentas) {
+        filtroVentas.addEventListener('input', function () {
+            const filtro = this.value.toLowerCase();
+            const filas = document.querySelectorAll('#tabla .table__tr');
 
-//                 filas.forEach(fila => {
-//                     const codigo = fila.cells[0].textContent.toLowerCase();
-//                     const nombre = fila.cells[1].textContent.toLowerCase();
+            filas.forEach(fila => {
+                const id = fila.cells[0].textContent.toLowerCase();
+                const nombreCliente = fila.cells[1].textContent.toLowerCase();
+                const nombreProducto = fila.cells[2].textContent.toLowerCase();
+                const codigoProducto = fila.cells[3].textContent.toLowerCase();
+                const estado = fila.cells[4].textContent.toLowerCase();
 
-//                     console.log(codigo, nombre);
+                console.log(id, nombreCliente, nombreProducto, codigoProducto, estado);
 
-//                     if (codigo.includes(filtro) || nombre.includes(filtro)) {
-//                         fila.style.display = '';
-//                     } else {
-//                         fila.style.display = 'none';
-//                     }
-//                 });
-//             });
-//         }
-
-//     })();
-// });
+                if (
+                    id.includes(filtro) || 
+                    nombreCliente.includes(filtro) || 
+                    nombreProducto.includes(filtro) || 
+                    codigoProducto.includes(filtro) || 
+                    estado.includes(filtro)
+                ) {
+                    fila.style.display = ''; // Mostrar fila
+                } else {
+                    fila.style.display = 'none'; // Ocultar fila
+                }
+            });
+        });
+    }
+});
