5152db8e0de082c33b00dd6dacd3b789b0901e9b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-03 14:33:59 -0500

    Update Pdf.php

--- classes/Pdf.php
+++ classes/Pdf.php
@@ -113,8 +113,8 @@ class Pdf extends TCPDF
         $pageWidth = $this->GetPageWidth();
         $pageHeight = $this->GetPageHeight();
     
-        $etiquetaWidth = 100; // Ancho de la etiqueta
-        $etiquetaHeight = 120; // Alto de la etiqueta
+        $etiquetaWidth = 120; // Ancho de la etiqueta
+        $etiquetaHeight = 140; // Alto de la etiqueta
         $x = ($pageWidth - $etiquetaWidth) / 2;
         $y = ($pageHeight - $etiquetaHeight) / 2;
     
