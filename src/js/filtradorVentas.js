8e17711ffdf0a30bdd18f90638b0b20e70a94f09
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-03 12:02:14 -0500

    Update filtradorVentas.js

---
+++ src/js/filtradorVentas.js
@@ -0,0 +1,27 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Objetivo: Filtrar ventas en tiempo real
+//     (function() {
+//         // Filtro en tiempo real
+//         const filtroVentas = document.querySelector('#filtros_ventas');
+//         if (filtroVentas) {
+//             filtroVentas.addEventListener('input', function () {
+//                 const filtro = this.value.toLowerCase();
+//                 const filas = document.querySelectorAll('#tabla .table__tr');
+
+//                 filas.forEach(fila => {
+//                     const codigo = fila.cells[0].textContent.toLowerCase();
+//                     const nombre = fila.cells[1].textContent.toLowerCase();
+
+//                     console.log(codigo, nombre);
+
+//                     if (codigo.includes(filtro) || nombre.includes(filtro)) {
+//                         fila.style.display = '';
+//                     } else {
+//                         fila.style.display = 'none';
+//                     }
+//                 });
+//             });
+//         }
+
+//     })();
+// });
