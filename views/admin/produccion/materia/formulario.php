d774fe8c9d624803c9fa870ab54f7d0b469a14eb
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-03 09:58:07 -0500

    Update formulario.php

--- views/admin/produccion/materia/formulario.php
+++ views/admin/produccion/materia/formulario.php
@@ -3,6 +3,17 @@
 <fieldset class="formulario__fieldset">
     <legend class="formulario__legend">Información Materia Prima</legend>
     <div class="formulario__campo">
+        <l class="formulario__label" for="nombre_rollo">Nombre del Material</label>
+        <input
+            type="text"
+            name="nombre_rollo"
+            id="nombre_rollo"
+            class="formulario__input"
+            placeholder="Nombre del rollo"
+            value="<?php echo $papel->nombre_rollo ?? '' ?>">
+    </div>
+
+    <div class="formulario__campo">
         <label class="formulario__label" for="tipo">Tipo</label>
         <input
             type="text"
@@ -32,6 +43,51 @@
             placeholder="peso del papel"
             value="<?php echo $papel->peso ?? '' ?>">
     </div>
-    
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="gramaje">Gramaje</label>
+        <input
+            type="text"
+            name="gramaje"
+            id="gramaje"
+            class="formulario__input"
+            placeholder="gramaje del papel"
+            value="<?php echo $papel->gramaje ?? '' ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="CED">CED</label>
+        <input
+            type="text"
+            name="CED"
+            id="CED"
+            class="formulario__input"
+            placeholder="CED del papel"
+            value="<?php echo $papel->CED ?? '' ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="proveedor">proveedor</label>
+        <input
+            type="text"
+            name="proveedor"
+            id="proveedor"
+            class="formulario__input"
+            placeholder="proveedor del papel"
+            value="<?php echo $papel->proveedor ?? '' ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="precio">precio</label>
+        <input
+            type="text"
+            name="precio"
+            id="precio"
+            class="formulario__input"
+            placeholder="precio del papel"
+            value="<?php echo $papel->precio ?? '' ?>">
+    </div>
+
+
 </fieldset>
 
