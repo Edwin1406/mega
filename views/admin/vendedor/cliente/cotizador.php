ba8f6aa91087b45d6457be72ab5c401135529fd3
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-07 15:04:37 -0500

    Update cotizador.php

--- views/admin/vendedor/cliente/cotizador.php
+++ views/admin/vendedor/cliente/cotizador.php
@@ -42,6 +42,8 @@
         <option value="10">10</option>
         <option value="25">25</option>
         <option value="50">50</option>
+        <option value="100">100</option>
+
         <option value="all">Todos</option>
     </select>
 </form>
