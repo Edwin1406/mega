e760251910975642b2ec8d6a41b86b6bc6848e65
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-07 13:39:09 -0500

    .

--- controllers/ClienteController.php
+++ controllers/ClienteController.php
@@ -356,15 +356,6 @@ public static function editar(Router $router)
     }
 
 
-    public static function apivisor(Router $router)
-    {
-        header("Access-Control-Allow-Origin: *");  // Permite solicitudes desde cualquier origen
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Métodos permitidos
-    header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Cabeceras permitidas
-        $clientes = Cliente::all();
-        header('Content-Type: application/json');
-        echo json_encode($clientes);
-    }
 
 }
 
