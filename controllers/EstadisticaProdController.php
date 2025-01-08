8f186c9fe7044a1a9802d36b06f5724d8ed35b7b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-08 16:10:37 -0500

    d

--- controllers/EstadisticaProdController.php
+++ controllers/EstadisticaProdController.php
@@ -2,12 +2,20 @@
 
 namespace Controllers;
 
+use Model\EstadisticaProd;
 use MVC\Router;
 
 class EstadisticaProdController {
     public static function crear(Router $router)
     {
         $alertas = [];
+        $estadistica = new EstadisticaProd;
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $estadistica->sincronizar($_POST);
+            $alertas = $estadistica->validar();
+            
+        }
+
         $router->render('admin/produccion/estadistica/crear', [
             'titulo' => 'Estadísticas de Producción',
             'alertas' => $alertas
--- models/EstadisticaProd.php
+++ models/EstadisticaProd.php
@@ -77,6 +77,91 @@ class EstadisticaProd extends ActiveRecord {
     }
 
 
+  public function validar() {
+
+        if(!$this->import) {
+            self::$alertas['error'][] = 'El Campo Import es Obligatorio';
+        }
+
+        if(!$this->proyecto) {
+            self::$alertas['error'][] = 'El Campo Proyecto es Obligatorio';
+        }
+
+        if(!$this->pedido_interno) {
+            self::$alertas['error'][] = 'El Campo Pedido Interno es Obligatorio';
+        }
+
+        if(!$this->fecha_solicitud) {
+            self::$alertas['error'][] = 'El Campo Fecha Solicitud es Obligatorio';
+        }
+
+        if(!$this->trader) {
+            self::$alertas['error'][] = 'El Campo Trader es Obligatorio';
+        }
+
+        if(!$this->marca) {
+            self::$alertas['error'][] = 'El Campo Marca es Obligatorio';
+        }
+
+        if(!$this->producto) {
+            self::$alertas['error'][] = 'El Campo Producto es Obligatorio';
+        }
+
+        if(!$this->gms) {
+            self::$alertas['error'][] = 'El Campo GMS es Obligatorio';
+        }
+
+        if(!$this->ancho) {
+            self::$alertas['error'][] = 'El Campo Ancho es Obligatorio';
+        }
+
+        if(!$this->cantidad) {
+            self::$alertas['error'][] = 'El Campo Cantidad es Obligatorio';
+        }
+
+        if(!$this->precio) {
+            self::$alertas['error'][] = 'El Campo Precio es Obligatorio';
+        }
+
+        if(!$this->total_item) {
+            self::$alertas['error'][] = 'El Campo Total Item es Obligatorio';
+        }
+
+        if(!$this->fecha_produccion) {
+            self::$alertas['error'][] = 'El Campo Fecha Producción es Obligatorio';
+        }
+
+        if(!$this->ets) {
+            self::$alertas['error'][] = 'El Campo ETS es Obligatorio';
+        }
+
+        if(!$this->eta) {
+            self::$alertas['error'][] = 'El Campo ETA es Obligatorio';
+
+        }
+
+        if(!$this->arribo_planta) {
+            self::$alertas['error'][] = 'El Campo Arribo Planta es Obligatorio';
+        }
+
+        if(!$this->transito) {
+            self::$alertas['error'][] = 'El Campo Transito es Obligatorio';
+        }
+
+        if(!$this->fecha_en_planta) {
+            self::$alertas['error'][] = 'El Campo Fecha en Planta es Obligatorio';
+        }
+
+        if(!$this->observaciones) {
+            self::$alertas['error'][] = 'El Campo Observaciones es Obligatorio';
+        }
+        
+        return self::$alertas;
+
+}
+     
+
+
 
 
 
