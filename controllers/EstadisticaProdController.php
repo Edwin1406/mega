546edfd947567580f48730417573860d86b19ef1
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-08 15:49:17 -0500

    Update EstadisticaProdController.php

--- controllers/EstadisticaProdController.php
+++ controllers/EstadisticaProdController.php
@@ -7,7 +7,9 @@ use MVC\Router;
 class EstadisticaProdController {
     public static function crear(Router $router)
     {
+        $alertas = [];
         $router->render('admin/produccion/estadistica/crear', [
             'titulo' => 'Estadísticas de Producción',
+            'alertas' => $alertas
         ]);      
 }}
