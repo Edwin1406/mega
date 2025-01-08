6da13932197b589b49aabb72394d833f8ba5b3aa
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-08 11:34:23 -0500

    Update crear.php

--- views/admin/produccion/materia/crear.php
+++ views/admin/produccion/materia/crear.php
@@ -1,5 +1,12 @@
 <h1 class="dashboard__heading"> <?php echo $titulo ?> </h1>
+
 <div class="dashboard__contenedor-boton-izquierdo">
+    <a class="dashboard__boton" href="/admin/produccion/materia/graficas">
+        <i class="fa-solid fa-arrow-right"></i>
+        Ver Graficas
+    </a>
+</div>
+<div class="dashboard__contenedor-boton">
     <a class="dashboard__boton" href="/admin/produccion/materia/tabla">
         <i class="fa-solid fa-arrow-right"></i>
         Ver a Materia Prima
