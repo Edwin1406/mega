8a9b444c83e37b49587fc5e8711051cd14809bc9
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-15 09:24:07 -0500

    .

--- controllers/ComercialController.php
+++ controllers/ComercialController.php
@@ -7,13 +7,40 @@ use MVC\Router;
 
 
 class ComercialController {
+
+    
+
     public static function index(Router $router)
     {
+       
+        session_start();
+        isAuth();
+        $id= $_SESSION['id'];
+        $escoger_produccion = Produccion::belongsTo('propietarioId',$id);
         $router->render('admin/comercial/index', [
-            'titulo' => 'Área Comercial'
+            'titulo' => 'Área Comercial',
+            'escoger_produccion' => $escoger_produccion
         ]);
     }
+
+    public static function registro_comercial(Router $router)
+    {   
+        session_start();
+        isAuth();
+        $id= $_SESSION['id'];
+        $escoge_registro = UrlRegistro::belongsTo('propietarioId',$id);
+        $router->render('admin/comercial/registro_comercial', [
+            'titulo' => 'REGISTRO DE PRODUCCION',
+            'escoge_registro' => $escoge_registro
+        ]);
+    }
+
 }
+
+
+
+
+
 
 
 
