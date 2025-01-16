5678855fc779528b0a280585b4fa53022f95f274
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-16 12:52:03 -0500

    d

--- views/admin/comercial/tabla.php
+++ views/admin/comercial/tabla.php
@@ -55,56 +55,56 @@
 
 <div class="dashboard__contenedor">
     <?php if (!empty($comercial)): ?>
-        <table class="table" id="tabla">
-            <thead class="table__thead">
+        <table class="tables" id="tabla">
+            <thead class="tables__thead">
                 <tr>
-                    <th scope="col" class="table__th">Nro.</th>
-                    <th scope="col" class="table__th">Import</th>
-                    <th scope="col" class="table__th">Proyecto</th>
-                    <th scope="col" class="table__th">Pedido Interno</th>
-                    <th scope="col" class="table__th">Fecha Solicitud</th>
-                    <th scope="col" class="table__th">Puerto Destino</th>
-                    <th scope="col" class="table__th">Trader</th>
-                    <th scope="col" class="table__th">Marca</th>
-                    <th scope="col" class="table__th">Linea</th>
-                    <th scope="col" class="table__th">Producto</th>
-                    <th scope="col" class="table__th">GMS</th>
-                    <th scope="col" class="table__th">Ancho</th>
-                    <th scope="col" class="table__th">Cantidad</th>
-                    <th scope="col" class="table__th">Precio</th>
-                    <th scope="col" class="table__th">Total Item</th>
-                    <th scope="col" class="table__th">Fecha Producción</th>
-                    <th scope="col" class="table__th">Arribo Planta</th>
-                    <th scope="col" class="table__th">Transito</th>
-                    <th scope="col" class="table__th">Fecha en Planta</th>
-                    <th scope="col" class="table__th">Estado</th>
-                    <th scope="col" class="table__th">Acciones</th>
+                    <th scope="col" class="tables__th">Nro.</th>
+                    <th scope="col" class="tables__th">Import</th>
+                    <th scope="col" class="tables__th">Proyecto</th>
+                    <th scope="col" class="tables__th">Pedido Interno</th>
+                    <th scope="col" class="tables__th">Fecha Solicitud</th>
+                    <th scope="col" class="tables__th">Puerto Destino</th>
+                    <th scope="col" class="tables__th">Trader</th>
+                    <th scope="col" class="tables__th">Marca</th>
+                    <th scope="col" class="tables__th">Linea</th>
+                    <th scope="col" class="tables__th">Producto</th>
+                    <th scope="col" class="tables__th">GMS</th>
+                    <th scope="col" class="tables__th">Ancho</th>
+                    <th scope="col" class="tables__th">Cantidad</th>
+                    <th scope="col" class="tables__th">Precio</th>
+                    <th scope="col" class="tables__th">Total Item</th>
+                    <th scope="col" class="tables__th">Fecha Producción</th>
+                    <th scope="col" class="tables__th">Arribo Planta</th>
+                    <th scope="col" class="tables__th">Transito</th>
+                    <th scope="col" class="tables__th">Fecha en Planta</th>
+                    <th scope="col" class="tables__th">Estado</th>
+                    <th scope="col" class="tables__th">Acciones</th>
                 </tr>
             </thead>
-            <tbody class="table__tbody">
+            <tbody class="tables__tbody">
                 <?php foreach ($comercial as $comerciales):?>
-                    <tr class="table__tr">
-                        <td class="table__td"><?php echo $comerciales->id?></td>
-                        <td class="table__td"><?php echo $comerciales->import?></td>
-                        <td class="table__td"><?php echo $comerciales->proyecto?></td>
-                        <td class="table__td"><?php echo $comerciales->pedido_interno?></td>
-                        <td class="table__td"><?php echo $comerciales->fecha_solicitud?></td>
-                        <td class="table__td"><?php echo $comerciales->puerto_destino?></td>
-                        <td class="table__td"><?php echo $comerciales->trader?></td>
-                        <td class="table__td"><?php echo $comerciales->marca?></td>
-                        <td class="table__td"><?php echo $comerciales->linea?></td>
-                        <td class="table__td"><?php echo $comerciales->producto?></td>
-                        <td class="table__td"><?php echo $comerciales->gms?></td>
-                        <td class="table__td"><?php echo $comerciales->ancho?></td>
-                        <td class="table__td"><?php echo $comerciales->cantidad?></td>
-                        <td class="table__td"><?php echo $comerciales->precio?></td>
-                        <td class="table__td"><?php echo $comerciales->total_item?></td>
-                        <td class="table__td"><?php echo $comerciales->fecha_produccion?></td>
-                        <td class="table__td"><?php echo $comerciales->arribo_planta?></td>
-                        <td class="table__td"><?php echo $comerciales->transito?></td>
-                        <td class="table__td"><?php echo $comerciales->fecha_en_planta?></td>
-                        <td class="table__td"><?php echo $comerciales->estado?></td>
-                        <td  class="table__td--acciones"><a class="table__accion table__accion--editar" href="/admin/comercial/editar?id=<?php echo $comerciales->id; ?>"><i class="fa-solid fa-user-pen"></i>Editar</a>
+                    <tr class="tables__tr">
+                        <td class="tables__td"><?php echo $comerciales->id?></td>
+                        <td class="tables__td"><?php echo $comerciales->import?></td>
+                        <td class="tables__td"><?php echo $comerciales->proyecto?></td>
+                        <td class="tables__td"><?php echo $comerciales->pedido_interno?></td>
+                        <td class="tables__td"><?php echo $comerciales->fecha_solicitud?></td>
+                        <td class="tables__td"><?php echo $comerciales->puerto_destino?></td>
+                        <td class="tables__td"><?php echo $comerciales->trader?></td>
+                        <td class="tables__td"><?php echo $comerciales->marca?></td>
+                        <td class="tables__td"><?php echo $comerciales->linea?></td>
+                        <td class="tables__td"><?php echo $comerciales->producto?></td>
+                        <td class="tables__td"><?php echo $comerciales->gms?></td>
+                        <td class="tables__td"><?php echo $comerciales->ancho?></td>
+                        <td class="tables__td"><?php echo $comerciales->cantidad?></td>
+                        <td class="tables__td"><?php echo $comerciales->precio?></td>
+                        <td class="tables__td"><?php echo $comerciales->total_item?></td>
+                        <td class="tables__td"><?php echo $comerciales->fecha_produccion?></td>
+                        <td class="tables__td"><?php echo $comerciales->arribo_planta?></td>
+                        <td class="tables__td"><?php echo $comerciales->transito?></td>
+                        <td class="tables__td"><?php echo $comerciales->fecha_en_planta?></td>
+                        <td class="tables__td"><?php echo $comerciales->estado?></td>
+                        <td  class="tables__td--acciones"><a class="tables__accion tables__accion--editar" href="/admin/comercial/editar?id=<?php echo $comerciales->id; ?>"><i class="fa-solid fa-user-pen"></i>Editar</a>
 
                     </tr>
                 <?php endforeach;?>
