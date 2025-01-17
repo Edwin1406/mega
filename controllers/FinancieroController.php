cd6f1ed5b4a9a47546a8e4515f5cfdd2c900af51
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-17 12:51:21 -0500

    Update FinancieroController.php

--- controllers/FinancieroController.php
+++ controllers/FinancieroController.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Comercial;
 
 
 
@@ -10,6 +11,20 @@ class FinancieroController {
 
     public static function tabla(Router $router)
     {
+
+        
+        $id = $_GET['id'] ?? null;
+        if (!$id){
+            header('Location: /');
+        }
+        
+
+        $pagina_actual = $_GET['page'] ?? 1;
+        $pagina_actual = filter_var($pagina_actual, FILTER_VALIDATE_INT);
+
+        if (!$pagina_actual|| $pagina_actual < 1) {
+            header('Location: /admin/comercial/tabla?page=1');
+        }
 
         $router->render('admin/financiero/tabla', [
             'titulo' => 'TABLA DE FINANCIERO',
