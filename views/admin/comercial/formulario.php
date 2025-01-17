ab26b6a1f5ea7fbf213c9e1b2aef90735f4c92e1
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-17 13:12:03 -0500

    Update formulario.php

--- views/admin/comercial/formulario.php
+++ views/admin/comercial/formulario.php
@@ -163,6 +163,17 @@
             value="<?php echo $comercial->arribo_planta ?? '' ?>">
     </div>
 
+    <div class="formulario__campo">
+        <label class="formulario__label" for="transito">Transito</label>
+        <input
+            type="number"
+            name="transito"
+            id="transito"
+            class="formulario__input"
+            placeholder="Días de Transito"
+            value="<?php echo $comercial->transito ?? '' ?>">
+    </div>
+
 
     <div class="formulario__campo">
         <label class="formulario__label" for="observaciones">Observaciones</label>
