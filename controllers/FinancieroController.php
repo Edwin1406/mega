70fadfac616234dde01f84c30c0fc3e362bdae07
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-17 12:53:42 -0500

    Update FinancieroController.php

--- controllers/FinancieroController.php
+++ controllers/FinancieroController.php
@@ -4,6 +4,7 @@ namespace Controllers;
 
 use MVC\Router;
 use Model\Comercial;
+use Classes\Paginacion;
 
 
 
@@ -26,8 +27,33 @@ class FinancieroController {
             header('Location: /admin/comercial/tabla?page=1');
         }
 
+        
+         // Obtener el número de registros por página
+         $registros_por_pagina = $_GET['per_page'] ?? 10;
+         if ($registros_por_pagina === 'all') {
+             $total = Comercial::total();
+             $registros_por_pagina = $total; // Mostrar todos los registros
+         } else {
+             $registros_por_pagina = filter_var($registros_por_pagina, FILTER_VALIDATE_INT) ?: 10;
+         }
+
+         $total = Comercial::total();
+        $paginacion = new Paginacion($pagina_actual, $registros_por_pagina, $total);
+
+        if ($paginacion->total_paginas() < $pagina_actual) {
+            header('Location: /admin/financiero/tabla?page=1');
+            exit;
+        }
+    
+        $financiero = Comercial::paginar($registros_por_pagina, $paginacion->offset());
+    
+
+
+
         $router->render('admin/financiero/tabla', [
             'titulo' => 'TABLA DE FINANCIERO',
+            'financiero' => $financiero,
+            'paginacion' => $paginacion->paginacion()
         ]);
     }
 }
