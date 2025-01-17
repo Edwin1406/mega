a3ac3499bfcf9cda3d906c6d1322ee0039f35a8e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-17 13:55:24 -0500

    Update cotizador.php

--- views/admin/vendedor/cliente/cotizador.php
+++ views/admin/vendedor/cliente/cotizador.php
@@ -40,10 +40,10 @@
     <label for="per_page">Registros por página:</label>
     <select name="per_page" id="per_page" onchange="this.form.submit()">
         <option value="10" <?php echo ($_GET['per_page'] ?? '10') == '10' ? 'selected' : ''; ?>>10</option>
-        <!-- <option value="10">10</option> -->
-        <option value="25">25</option>
-        <option value="50">50</option>
-        <option value="all">Todos</option>
+        <option value="25" <?php echo ($_GET['per_page'] ?? '') == '25' ? 'selected' : ''; ?>>25</option>
+        <option value="50" <?php echo ($_GET['per_page'] ?? '') == '50' ? 'selected' : ''; ?>>50</option>
+        <option value="all" <?php echo ($_GET['per_page'] ?? '') == 'all' ? 'selected' : ''; ?>>Todos</option>
+     
     </select>
 </form>
 
