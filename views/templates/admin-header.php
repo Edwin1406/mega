45610f751ad15810298231ff59908bdd05aec101
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-20 09:27:56 -0500

    Update admin-header.php

--- views/templates/admin-header.php
+++ views/templates/admin-header.php
@@ -7,26 +7,7 @@
             </h2>
         </a>
         <nav class="dashboard__nav"> 
-
-            <?php
-session_start();
-$nombre = $_SESSION['nombre'] ?? '';
-
-// Determina el ícono según el nombre
-$icono = '';
-if ($nombre === 'ventas') {
-    $icono = '<i class="fa fa-shopping-cart" aria-hidden="true"></i>'; // Ícono para ventas
-} elseif ($nombre === 'produccion') {
-    $icono = '<i class="fa fa-industry" aria-hidden="true"></i>'; // Ícono para producción
-} else {
-    $icono = '<i class="fa fa-question-circle" aria-hidden="true"></i>'; // Ícono por defecto
-}
-?>
-<p class="dashboard__submit--name">
-    <?php echo $icono; ?>
-    <?php echo htmlspecialchars($nombre); ?>
-</p>
-
+            <p class="dashboard__submit--name" class="fa fa-user" ><?php session_start(); echo $_SESSION['nombre'] ?> </p> 
 
                 <form method="POST" action="/logout" class="dashboard__form">
              
