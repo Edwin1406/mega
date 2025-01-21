6165cb6ca2a5ec38512c311a460f1a0ea797e8d0
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-21 11:40:37 -0500

    Update excel.php

--- views/admin/produccion/materia/excel.php
+++ views/admin/produccion/materia/excel.php
@@ -6,7 +6,7 @@
 
 <?php include_once __DIR__.'/../../../templates/alertas.php'  ?>
 
-    <form method="POST" action="/admin/produccion/materia/crear"  class="formulario" enctype="multipart/form-data">
+    <form method="POST" action="/admin/produccion/materia/excel"  class="formulario" enctype="multipart/form-data">
 
      
     <?php include_once __DIR__.'/subirExcel.php'  ?>
