1c2d6f0d2976d4382affdf23a7ac9d0aca1b3adc
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-22 09:18:19 -0500

    Update periodico.php

--- views/admin/produccion/materia/periodico.php
+++ views/admin/produccion/materia/periodico.php
@@ -1,1 +1,20 @@
 <h1 class="dashboard__heading"> <?php echo $titulo ?> </h1>
+
+<ul class="lista-areas-produccion">
+    <li class="areas-produccion">
+        <a href="#">
+            <i class="fas fa-industry">  </i> TOTAL REGISTROS :
+            <?php if($totalRegistros > 0) : ?>
+                <span class="areas-produccion__numero"> <?php echo $totalRegistros ?> </span>
+            <?php endif; ?> 
+        </a>
+    </li>
+    <li class="areas-produccion">
+        <a href="#">
+            <i class="fas fa-scroll"></i> TOTAL EXISTENCIA :
+            <?php if($totalExistencia > 0) : ?>
+                <span class="areas-produccion__numero"> <?php echo $totalExistencia ?> KG </span>
+            <?php endif; ?> 
+        </a>
+    </li>
+</ul>
