3d1bd6cae9cb745caac7dd244aed4700ba202ead
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-23 10:13:37 -0500

    Update corrugador.php

--- views/admin/produccion/materia/corrugador.php
+++ views/admin/produccion/materia/corrugador.php
@@ -57,7 +57,6 @@
     fetch('https://megawebsistem.com/admin/api/apicorrugador')
         .then(response => response.json())
         .then(data => {
-            originalData = data; // Guardar datos originales
             initializeFilters(data); // Inicializar filtros dinámicos
             renderChart(data); // Renderizar gráfico inicial
         });
