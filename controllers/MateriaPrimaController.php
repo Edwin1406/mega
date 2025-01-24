b8641581b3e13c7577da9dce81449a1f3e05c501
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-24 14:33:50 -0500

    Update MateriaPrimaController.php

--- controllers/MateriaPrimaController.php
+++ controllers/MateriaPrimaController.php
@@ -414,7 +414,7 @@ class MateriaPrimaController
 
 
     public static function apicajablanco (){
-        $corrugador = MateriaPrimaV::allc('DESC', 'MICRO');
+        $corrugador = MateriaPrimaV::allc('DESC', 'CAJA');
         $jsoncorrugador = json_encode($corrugador);
         $data = json_decode($jsoncorrugador, true);
     
