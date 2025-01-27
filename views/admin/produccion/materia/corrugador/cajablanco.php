0c1540ae44d89c840eb6e66c226243c5a2787e30
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-27 14:36:30 -0500

    Update cajablanco.php

--- views/admin/produccion/materia/corrugador/cajablanco.php
+++ views/admin/produccion/materia/corrugador/cajablanco.php
@@ -14,13 +14,8 @@
 
 
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Gráfico con Filtros y Tabla</title>
-  <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
+  <!-- <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script> -->
   <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
