03425dbcd36a5590cca95eca9b6e28a17ae9a402
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-27 09:42:25 -0500

    Update cajablanco.php

--- views/admin/produccion/materia/corrugador/cajablanco.php
+++ views/admin/produccion/materia/corrugador/cajablanco.php
@@ -300,9 +300,19 @@
       data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-          label: '# of Votes',
+          label: '',
           data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: ['red', 'blue', 'yellow', 'green', 'purple', 'orange'],
+          backgroundColor: [
+          '#ea580c',
+          '#84cc16',
+          '#22d3ee',
+          '#a855f7',
+          '#ef4444',
+          '#14b8a6',
+          '#db2777',
+          '#e11d48',
+          '#7e22ce'
+          ],
           borderWidth: 1
         }]
       },
