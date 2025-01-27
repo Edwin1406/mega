ea8183585300287d716924668586574ef0cc78f3
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-27 14:38:28 -0500

    Update cajablanco.php

--- views/admin/produccion/materia/corrugador/cajablanco.php
+++ views/admin/produccion/materia/corrugador/cajablanco.php
@@ -16,8 +16,8 @@
 
 <!-- <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script> -->
 
-  <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-  <!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css"> -->
+  <!-- <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script> -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
   <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
 <!-- <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script> -->
 
