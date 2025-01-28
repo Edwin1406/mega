8fa726c07c8d05e69fd8015a08e68103bb405191
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-28 11:43:24 -0500

    Create subirExcel.php

---
+++ views/admin/comercial/subirExcel.php
@@ -0,0 +1,15 @@
+<fieldset class="formulario__fieldset">
+
+
+    <legend class="formulario__legend">Subir Excel</legend>
+    <div class="formulario__campo">
+        <label class="formulario__label" for="excel">Excel</label>
+        <input
+            type="file"
+            name="file"
+            id="file"
+            class="formulario__input"
+            placeholder="Excel"
+            accept=".xls,.xlsx">
+    </div>
+</fieldset>
