5eb8542f0bed2c0b45c8a902b888ca954f73313e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-01-29 15:21:35 -0500

    Update MateriaPrimaController.php

--- controllers/MateriaPrimaController.php
+++ controllers/MateriaPrimaController.php
@@ -546,7 +546,7 @@ class MateriaPrimaController
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
         
-        $corru = MateriaPrimaV::all('DESC');
+        $corru = MateriaPrimaV::allc('DESC', 'CAJA');
         echo json_encode($corru);
 
     } 
