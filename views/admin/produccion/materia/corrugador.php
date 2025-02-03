272a78bc2b8a233435b04c2510df6b0d07e5ed90
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-03 12:48:48 -0500

    Update corrugador.php

--- views/admin/produccion/materia/corrugador.php
+++ views/admin/produccion/materia/corrugador.php
@@ -115,17 +115,20 @@
 </style>
 
 <div class="display">
-    <table id="dataTable">
-        <thead>
-            <tr>
-                <th>Ancho</th>
-                <th>Gramaje</th>
-                <th>Línea</th>
-                <th>Existencia</th>
-            </tr>
-        </thead>
-        <tbody></tbody>
-    </table>
+    <div>
+        <h2 class="titulo_existencia">Existencia (Corrugador)</h2>
+        <table id="dataTable">
+            <thead>
+                <tr>
+                    <th>Ancho</th>
+                    <th>Gramaje</th>
+                    <th>Línea</th>
+                    <th>Existencia</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
     
     <div>
 
