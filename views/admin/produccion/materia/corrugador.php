c388bc890ca8dd08686ed631c5c3a12759fd4d66
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-07 10:01:36 -0500

    Update corrugador.php

--- views/admin/produccion/materia/corrugador.php
+++ views/admin/produccion/materia/corrugador.php
@@ -153,7 +153,12 @@
         border-radius: 1rem;
         box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
     } */
-
+    .columna_filtros, .columna_tabla {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start; /* Cambiado de space-between */
+    flex: 1;
+}
 
     #filters {
         display: flex;
