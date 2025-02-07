ca1ba9b0088498e5ce1f57e7fde96b2d4b1321c4
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-07 10:11:48 -0500

    Update corrugador.php

--- views/admin/produccion/materia/corrugador.php
+++ views/admin/produccion/materia/corrugador.php
@@ -214,7 +214,7 @@
 
 #totalExistencia {
     position: absolute; /* O usa 'fixed' si quieres que esté siempre visible */
-    top: 20px;        /* Ajusta esta posición vertical */
+    top: 550px;        /* Ajusta esta posición vertical */
     left: 20px;       /* Ajusta esta posición horizontal */
     background-color: rgba(0, 0, 0, 0.7); /* Fondo semitransparente */
     color: white;
