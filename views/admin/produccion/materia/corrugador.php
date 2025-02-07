7a7c9e06a706a8846b701be1c867d8ed5666ca45
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-07 11:42:16 -0500

    .

--- views/admin/produccion/materia/corrugador.php
+++ views/admin/produccion/materia/corrugador.php
@@ -34,8 +34,8 @@
 </style>
 
 <div class="container">
+    <div class="item"><a href="/admin/produccion/materia/corrugador">INICIO</a></div>
   <div class="item"><a href="/admin/produccion/materia/corrugador/cajacraft">KRAFT</a></div>
-  <div class="item"><a href="/admin/produccion/materia/corrugador">INICIO</a></div>
   <div class="item"><a href="https://www.google.com/?hl=es">CLICK</a></div>
   <div class="item"><a href="https://www.google.com/?hl=es">CLICK</a></div>
   <div class="item"><a href="https://www.google.com/?hl=es">CLICK</a></div>
--- views/admin/produccion/materia/crear.php
+++ views/admin/produccion/materia/crear.php
@@ -167,7 +167,7 @@
         .chart {
             width: 100%;
             height: 50px;
-            background: linear-gradient(to top, #ddd, #8a5ba3);
+            /* background: linear-gradient(to top, #ddd, #8a5ba3); */
             border-radius: 5px;
             margin-top: 10px;
         }
