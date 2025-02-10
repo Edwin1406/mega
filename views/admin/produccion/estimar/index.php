b331394774ed689e24dfdfbd4a741da654eb44ee
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-10 16:21:38 -0500

    Update index.php

--- views/admin/produccion/estimar/index.php
+++ views/admin/produccion/estimar/index.php
@@ -61,10 +61,10 @@
 
 <div class="container">
     <div class="item"><a href="/admin/produccion/estimar/index?id=20255"> <i class="fas fa-home"></i> INICIO</a></div>
-    <div class="item"><a href="/admin/produccion/"> <i class="fas fa-industry"></i> COTIZADOR MICRO</a></div>
-    <div class="item"><a href="/admin/produccion/"> <i class="fas fa-scroll"></i> COTIZADOR CAJAS</a></div>
-    <div class="item"><a href="/admin/produccion/">  <i class="fas fa-newspaper"></i> COTIZADOR SEPARADORES</a></div>
-    <div class="item"><a href="/admin/produccion/">  <i class="fa-solid fa-calculator"></i> COSTOS GENERALES</a></div>
-    <div class="item"><a href="/admin/produccion/">  <i class="fa-solid fa-money-bill-1"></i> COSTOS PAPEL</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/micro"> <i class="fas fa-industry"></i> COTIZADOR MICRO</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/cajas"> <i class="fas fa-scroll"></i> COTIZADOR CAJAS</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/separadores">  <i class="fas fa-newspaper"></i> COTIZADOR SEPARADORES</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/costos_generales">  <i class="fa-solid fa-calculator"></i> COSTOS GENERALES</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/costos_papel">  <i class="fa-solid fa-money-bill-1"></i> COSTOS PAPEL</a></div>
 </div>
 
