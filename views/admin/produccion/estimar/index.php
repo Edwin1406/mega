2d3bbaf8212b848feb2f17583b005b64fa8135e9
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-10 16:12:04 -0500

    Update index.php

--- views/admin/produccion/estimar/index.php
+++ views/admin/produccion/estimar/index.php
@@ -25,7 +25,6 @@
     .item:hover {
       background-color: #d5f5e3;
       scale: 1.1;
-      /* text-align: center; */
       color: #24292d;
     }
 
@@ -60,10 +59,10 @@
 
 <div class="container">
     <div class="item"><a href="/admin/produccion/estimar/index?id=20255"> <i class="fas fa-home"></i> INICIO</a></div>
-    <div class="item"><a href="/admin/produccion/materia/corrugador/cajacraft"> <i class="fas fa-industry"></i> COTIZADOR MICRO</a></div>
-    <div class="item"><a href="/admin/produccion/materia/microcorrugador/cajablanco"> <i class="fas fa-scroll"></i> COTIZADOR CAJAS</a></div>
-    <div class="item"><a href="/admin/produccion/materia/periodico/cajamedium">  <i class="fas fa-newspaper"></i> COTIZADOR SEPARADORES</a></div>
-    <div class="item"><a href="/admin/produccion/materia/periodico/cajamedium">  <i class="fas fa-newspaper"></i> COSTOS GENERALES</a></div>
-    <div class="item"><a href="/admin/produccion/materia/periodico/cajamedium">  <i class="fas fa-newspaper"></i> COSTOS PAPEL</a></div>
+    <div class="item"><a href="/admin/produccion/"> <i class="fas fa-industry"></i> COTIZADOR MICRO</a></div>
+    <div class="item"><a href="/admin/produccion/"> <i class="fas fa-scroll"></i> COTIZADOR CAJAS</a></div>
+    <div class="item"><a href="/admin/produccion/">  <i class="fas fa-newspaper"></i> COTIZADOR SEPARADORES</a></div>
+    <div class="item"><a href="/admin/produccion/">  <i class="fa-solid fa-calculator"></i> COSTOS GENERALES</a></div>
+    <div class="item"><a href="/admin/produccion/">  <i class="fa-solid fa-money-bill-1"></i> COSTOS PAPEL</a></div>
 </div>
 
