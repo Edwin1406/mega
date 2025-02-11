6e64d5536fb8d45ead65719104338f9483d7e3c4
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-11 08:52:52 -0500

    Update Router.php

--- Router.php
+++ Router.php
@@ -19,34 +19,29 @@ class Router
 
     public function comprobarRutas()
     {
-
-          
         $url_actual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];   
-
-        
-
+    
         if ($method === 'GET') {
             $fn = $this->getRoutes[$url_actual] ?? null;
         } else {
             $fn = $this->postRoutes[$url_actual] ?? null;
         }
-
-        if ( $fn ) {
+    
+        if ($fn) {
             call_user_func($fn, $this);
         } else {
-             // ruta error 404
-            http_response_code(404);  // Enviar código de respuesta 404
-            header('Location: admin/error/404');  // Redirigir a la página de error
-            exit();  // Detener la ejecución para asegurar la redirección
+            // Evitar redirección infinita si ya estamos en la página de error
+            if ($url_actual !== '/admin/error/404') {
+                http_response_code(404);  // Enviar código de respuesta 404
+                header('Location: /admin/error/404');  // Redirigir a la página de error
+                exit();  // Detener la ejecución
+            } else {
+                echo 'Página no encontrada';  // Muestra un mensaje simple si ya estamos en el error
+            }
         }
-
-
-
-
-
-        
     }
+    
 
     public function render(string $view, array $datos = []): void
     {
