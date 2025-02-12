a8f7e0b31532b5049a25025029e85bd2d86bdc9d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-12 14:33:53 -0500

    c

--- views/admin/produccion/cotizador/trimar.php
+++ views/admin/produccion/cotizador/trimar.php
@@ -3,27 +3,67 @@
         <table class="tables" id="tabla_mejor_combinacion">
             <thead class="tables__thead">
                 <tr>
-                    <th scope="col" class="tables__th">ID Pedido 1</th>
-                    <th scope="col" class="tables__th">Ancho Pedido 1</th>
-                    <th scope="col" class="tables__th">ID Pedido 2</th>
-                    <th scope="col" class="tables__th">Ancho Pedido 2</th>
-                    <th scope="col" class="tables__th">ID Bobina</th>
-                    <th scope="col" class="tables__th">Ancho Bobina</th>
-                    <th scope="col" class="tables__th">Espacio Sobrante</th>
+                    <th scope="col" class="tables__th">ID</th>
+                    <th scope="col" class="tables__th">Pedidos</th>
+                    <th scope="col" class="tables__th">Ancho</th>
                 </tr>
             </thead>
             <tbody class="tables__tbody">
                 <tr class="tables__tr">
-                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_1']->id; ?></td>
+                    <td class="tables__td">1</td>
+                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_1']->nombre_pedido; ?></td>
                     <td class="tables__td"><?php echo $mejor_combinacion['pedido_1']->ancho; ?></td>
-                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_2']->id; ?></td>
+                </tr>
+                <tr class="tables__tr">
+                    <td class="tables__td">2</td>
+                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_2']->nombre_pedido; ?></td>
                     <td class="tables__td"><?php echo $mejor_combinacion['pedido_2']->ancho; ?></td>
-                    <td class="tables__td"><?php echo $mejor_combinacion['bobina']['id']; ?></td>
-                    <td class="tables__td"><?php echo $mejor_combinacion['bobina']['ancho']; ?></td>
-                    <td class="tables__td"><?php echo $mejor_suma; ?></td>
                 </tr>
             </tbody>
         </table>
+
+        <!-- Tarjeta con la información de la bobina -->
+        <div class="card">
+            <h3>Bobina Seleccionada</h3>
+            <p><strong>ID:</strong> <?php echo $mejor_combinacion['bobina']['id']; ?></p>
+            <p><strong>Ancho:</strong> <?php echo $mejor_combinacion['bobina']['ancho']; ?></p>
+        </div>
+
+    <?php else: ?>
+        <a class="text-center"> No hay combinaciones óptimas encontradas</a>
+    <?php endif; ?>
+</div>
+<div class="dashboard__contenedor">
+    <?php if (!empty($mejor_combinacion)): ?>
+        <table class="tables" id="tabla_mejor_combinacion">
+            <thead class="tables__thead">
+                <tr>
+                    <th scope="col" class="tables__th">ID</th>
+                    <th scope="col" class="tables__th">Pedidos</th>
+                    <th scope="col" class="tables__th">Ancho</th>
+                </tr>
+            </thead>
+            <tbody class="tables__tbody">
+                <tr class="tables__tr">
+                    <td class="tables__td">1</td>
+                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_1']->nombre_pedido; ?></td>
+                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_1']->ancho; ?></td>
+                </tr>
+                <tr class="tables__tr">
+                    <td class="tables__td">2</td>
+                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_2']->nombre_pedido; ?></td>
+                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_2']->ancho; ?></td>
+                </tr>
+            </tbody>
+        </table>
+
+        <!-- Tarjeta con la información de la bobina -->
+        <div class="card">
+            <h3>Bobina Seleccionada</h3>
+            <p><strong>ID:</strong> <?php echo $mejor_combinacion['bobina']['id']; ?></p>
+            <p><strong>Ancho:</strong> <?php echo $mejor_combinacion['bobina']['ancho']; ?></p>
+        </div>
+
     <?php else: ?>
         <a class="text-center"> No hay combinaciones óptimas encontradas</a>
     <?php endif; ?>
