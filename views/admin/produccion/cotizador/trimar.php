809c7f06bf9e8d588efda20197786eb76e5aebaf
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-12 14:37:11 -0500

    Update trimar.php

--- views/admin/produccion/cotizador/trimar.php
+++ views/admin/produccion/cotizador/trimar.php
@@ -1,5 +1,5 @@
 <div class="dashboard__contenedor">
-    <?php if (!empty($mejor_combinacion)): ?>
+    <?php if (!empty($mejor_combinacion) && is_array($mejor_combinacion)): ?>
         <table class="tables" id="tabla_mejor_combinacion">
             <thead class="tables__thead">
                 <tr>
@@ -11,13 +11,13 @@
             <tbody class="tables__tbody">
                 <tr class="tables__tr">
                     <td class="tables__td">1</td>
-                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_1']->nombre_pedido; ?></td>
-                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_1']->ancho; ?></td>
+                    <td class="tables__td"><?php echo htmlspecialchars($mejor_combinacion['pedido_1']->nombre_pedido); ?></td>
+                    <td class="tables__td"><?php echo htmlspecialchars($mejor_combinacion['pedido_1']->ancho); ?></td>
                 </tr>
                 <tr class="tables__tr">
                     <td class="tables__td">2</td>
-                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_2']->nombre_pedido; ?></td>
-                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_2']->ancho; ?></td>
+                    <td class="tables__td"><?php echo htmlspecialchars($mejor_combinacion['pedido_2']->nombre_pedido); ?></td>
+                    <td class="tables__td"><?php echo htmlspecialchars($mejor_combinacion['pedido_2']->ancho); ?></td>
                 </tr>
             </tbody>
         </table>
@@ -25,43 +25,8 @@
         <!-- Tarjeta con la información de la bobina -->
         <div class="card">
             <h3>Bobina Seleccionada</h3>
-            <p><strong>ID:</strong> <?php echo $mejor_combinacion['bobina']['id']; ?></p>
-            <p><strong>Ancho:</strong> <?php echo $mejor_combinacion['bobina']['ancho']; ?></p>
-        </div>
-
-    <?php else: ?>
-        <a class="text-center"> No hay combinaciones óptimas encontradas</a>
-    <?php endif; ?>
-</div>
-<div class="dashboard__contenedor">
-    <?php if (!empty($mejor_combinacion)): ?>
-        <table class="tables" id="tabla_mejor_combinacion">
-            <thead class="tables__thead">
-                <tr>
-                    <th scope="col" class="tables__th">ID</th>
-                    <th scope="col" class="tables__th">Pedidos</th>
-                    <th scope="col" class="tables__th">Ancho</th>
-                </tr>
-            </thead>
-            <tbody class="tables__tbody">
-                <tr class="tables__tr">
-                    <td class="tables__td">1</td>
-                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_1']->nombre_pedido; ?></td>
-                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_1']->ancho; ?></td>
-                </tr>
-                <tr class="tables__tr">
-                    <td class="tables__td">2</td>
-                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_2']->nombre_pedido; ?></td>
-                    <td class="tables__td"><?php echo $mejor_combinacion['pedido_2']->ancho; ?></td>
-                </tr>
-            </tbody>
-        </table>
-
-        <!-- Tarjeta con la información de la bobina -->
-        <div class="card">
-            <h3>Bobina Seleccionada</h3>
-            <p><strong>ID:</strong> <?php echo $mejor_combinacion['bobina']['id']; ?></p>
-            <p><strong>Ancho:</strong> <?php echo $mejor_combinacion['bobina']['ancho']; ?></p>
+            <p><strong>ID:</strong> <?php echo htmlspecialchars($mejor_combinacion['bobina']['id']); ?></p>
+            <p><strong>Ancho:</strong> <?php echo htmlspecialchars($mejor_combinacion['bobina']['ancho']); ?></p>
         </div>
 
     <?php else: ?>
