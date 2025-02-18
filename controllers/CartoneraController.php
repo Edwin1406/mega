bb7a680a5a8672c8f758411a6e1605987b77c7d6
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-18 16:49:04 -0500

    Update CartoneraController.php

--- controllers/CartoneraController.php
+++ controllers/CartoneraController.php
@@ -9,7 +9,7 @@ class CartoneraController
 {
     public static function cartonera(Router $router)
     {
-        $router->render('admin/produccion/cotizador/cartonera', [
+        $router->render('admin/produccion/corrugador/cartonera/index', [
             'titulo' => 'Cotizador Cartonera'
         ]);
     }
