9dee964c1f90f91b639a5e1fc971099edad026d3
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-19 11:44:32 -0500

    Update pedidoseleccionados.php

--- views/admin/produccion/materia/corrugador/cartonera/pedidoseleccionados.php
+++ views/admin/produccion/materia/corrugador/cartonera/pedidoseleccionados.php
@@ -17,7 +17,11 @@ document.querySelector(".borrar").addEventListener("click", () => {
 
 document.addEventListener("DOMContentLoaded",()=>{
     const pedidos = JSON.parse(localStorage.getItem("pedidosFiltrados"))||[];
-    console.log(pedidos);
+    pedidos.foreach(pedido=>{
+        const{ id, nombre_pedido, cantidad, largo, ancho, alto, flauta, test, fecha_ingreso, fecha_entrega} = pedido;
+        console.log(id, nombre_pedido, cantidad, largo, ancho, alto, flauta, test, fecha_ingreso, fecha_entrega);
+
+    })
 })
 
 </script>
