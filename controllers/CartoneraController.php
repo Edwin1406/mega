90ad253970bc52448a779b1ec500396e147948d1
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-19 09:43:05 -0500

    Update CartoneraController.php

--- controllers/CartoneraController.php
+++ controllers/CartoneraController.php
@@ -2,8 +2,9 @@
 
 namespace Controllers;
 
-use Model\Pedido;
 use MVC\Router;
+use Model\Pedido;
+use Classes\Paginacion;
 
 
 
@@ -17,12 +18,48 @@ class CartoneraController {
         isAuth();
         $id= $_SESSION['id'];
         $alertas = [];
-        $pedidosTrimar = Pedido::all('ASC');
+        // $pedidosTrimar = Pedido::all('ASC');
+
+        
+        $pagina_actual = $_GET['page'] ?? 1;
+        $pagina_actual = filter_var($pagina_actual, FILTER_VALIDATE_INT);
+    
+        if (!$pagina_actual || $pagina_actual < 1) {
+            header('Location: /admin/produccion/materia/corrugador/cartonera/index?page=1');
+            exit;
+        }
+
+        $registros_por_pagina = $_GET['per_page'] ?? 10;
+        if ($registros_por_pagina === 'all') {
+            $total = Pedido::total();
+            $registros_por_pagina = $total; // Mostrar todos los registros
+        } else {
+            $registros_por_pagina = filter_var($registros_por_pagina, FILTER_VALIDATE_INT) ?: 10;
+        }
+
+        $total = Pedido::total();
+        $paginacion = new Paginacion($pagina_actual, $registros_por_pagina, $total);
+    
+        if ($paginacion->total_paginas() < $pagina_actual) {
+            header('Location: /admin/produccion/materia/corrugador/cartonera/index?page=1');
+            exit;
+        }
+    
+
+        $pedidosTrimar = Pedido::paginar($registros_por_pagina, $paginacion->offset());
+    
+        $alertas = Pedido::getAlertas();
+
+
+
+
         // debuguear($pedidosTrimar);
         $router->render('admin/produccion/materia/corrugador/cartonera/index', [
             'titulo' => 'CARTOGAR',
+
             'alertas' => $alertas,
             'pedidosTrimar' => $pedidosTrimar,
+            'paginacion' => $paginacion->paginacion(),
         ]);
     }
 }
