23727c18a0e50bf313362402398d456c0af25c05
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-19 11:03:02 -0500

    Update pedidoseleccionados.php

--- views/admin/produccion/materia/corrugador/cartonera/pedidoseleccionados.php
+++ views/admin/produccion/materia/corrugador/cartonera/pedidoseleccionados.php
@@ -1,1 +1,11 @@
 <h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
+<script>
+
+const pedidosFiltrados = localStorage.getItem("pedidosFiltrados");
+pedidosFiltrados = JSON.parse(pedidosFiltrados);
+
+// ver 
+console.log(pedidosFiltrados);
+
+
+</script>
