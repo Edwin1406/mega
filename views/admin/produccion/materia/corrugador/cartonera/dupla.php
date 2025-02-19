11c4630775d899f865058e60b91502ecd3b49c6d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-19 16:10:48 -0500

    Update dupla.php

--- views/admin/produccion/materia/corrugador/cartonera/dupla.php
+++ views/admin/produccion/materia/corrugador/cartonera/dupla.php
@@ -17,7 +17,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     let pedidos = JSON.parse(localStorage.getItem("pedidosFiltrados")) || [];
     console.log(pedidos);
+    consumirapi();
 });
+
+
+
+
+
+async function consumirapi(params) {
+    const url = "https://megawebsistem.com/admin/api/apipapel";
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+}
 
 
 
