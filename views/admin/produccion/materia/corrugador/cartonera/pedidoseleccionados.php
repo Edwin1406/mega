5de158ed7441195bddefe91e6ebf005f11bb70c3
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-19 11:22:58 -0500

    Update pedidoseleccionados.php

--- views/admin/produccion/materia/corrugador/cartonera/pedidoseleccionados.php
+++ views/admin/produccion/materia/corrugador/cartonera/pedidoseleccionados.php
@@ -1,5 +1,16 @@
 <h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
+
+<button class="borrar">
+    clear
+</button>
+
 <script>
+
+
+document.querySelector(".borrar").addEventListener("click", () => {
+    localStorage.removeItem("pedidosFiltrados");
+    alert("Los datos filtrados se han eliminado de Local Storage.");
+});
 
 const pedidosFil = localStorage.getItem("pedidosFiltrados");
 
