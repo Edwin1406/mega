09443ea51353e8e59b7cb68650090b45798f1ca4
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-19 14:57:04 -0500

    Update dupla.php

--- views/admin/produccion/materia/corrugador/cartonera/dupla.php
+++ views/admin/produccion/materia/corrugador/cartonera/dupla.php
@@ -13,5 +13,13 @@
 
 
 
+<script>
+document.addEventListener("DOMContentLoaded", () => {
+    let pedidos = JSON.parse(localStorage.getItem("pedidosFiltrados")) || [];
+    console.log(pedidos);
+});
 
+
+
+</script>
 
