da6b38756862a210786889ad7f8519320b247656
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-20 12:20:22 -0500

    Update Material.php

--- models/Material.php
+++ models/Material.php
@@ -11,7 +11,6 @@ class Material extends ActiveRecord {
     public $flauta;
 
 
-
     public static function obtenerMaterialesConPapeles() {
         $query = "SELECT 
                     m.id_material AS material_id,
@@ -22,37 +21,40 @@ class Material extends ActiveRecord {
                     p.descripcion AS descripcion_papel,
                     p.peso
                   FROM material m
-                  LEFT JOIN papel p ON m.id_material = p.material_id
+                  LEFT JOIN papel_material pm ON m.id_material = pm.id_material
+                  LEFT JOIN papel p ON pm.id_papel = p.id_papel
                   ORDER BY m.id_material, p.id_papel";
-
+    
         $resultado = self::arrayasociativo($query);
-
+    
         // Agrupar los materiales y sus papeles en un array estructurado
         $materiales = [];
         foreach ($resultado as $fila) {
-            $nombreMaterial = $fila['nombre_material'];
-            $flauta = $fila['flauta'];
-
+            $materialId = $fila['material_id'];
+    
             // Si el material no está en la lista, lo agregamos
-            if (!isset($materiales[$nombreMaterial])) {
-                $materiales[$nombreMaterial] = [
-                    'id' => $fila['material_id'],
-                    'material' => $nombreMaterial,
-                    'flauta' => $flauta,
+            if (!isset($materiales[$materialId])) {
+                $materiales[$materialId] = [
+                    'id' => $materialId,
+                    'material' => $fila['nombre_material'],
+                    'flauta' => $fila['flauta'],
                     'papeles' => []
                 ];
             }
-
-            // Agregar los papeles al material correspondiente
-            $materiales[$nombreMaterial]['papeles'][] = [
-                'codigo' => $fila['codigo_papel'],
-                'peso' => $fila['peso'],
-                'descripcion' => $fila['descripcion_papel']
-            ];
+    
+            // Verificamos si el papel tiene valores antes de agregarlo (evita entradas nulas)
+            if (!is_null($fila['papel_id'])) {
+                $materiales[$materialId]['papeles'][] = [
+                    'codigo' => $fila['codigo_papel'],
+                    'peso' => $fila['peso'],
+                    'descripcion' => $fila['descripcion_papel']
+                ];
+            }
         }
-
-        return array_values($materiales); // Convertir a array indexado para el JSON
+    
+        return array_values($materiales); // Convertir a array indexado para JSON limpio
     }
+    
 
     
     // public static function obtenerMaterialesConPapeles() {
