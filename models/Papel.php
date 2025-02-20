2cb64a11705f4e187b49531964ba0ebc4710e9b4
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-20 12:37:28 -0500

    Update Papel.php

--- models/Papel.php
+++ models/Papel.php
@@ -4,7 +4,7 @@ namespace Model;
 
 class Papel extends ActiveRecord {
     protected static $tabla = 'papel';
-    protected static $columnasDB = ['id_papel', 'codigo', 'descripcion', 'peso', 'material_id'];
+    protected static $columnasDB = ['id_papel', 'codigo', 'descripcion', 'peso', 'gramaje'];
 
     public $id_papel;
     public $codigo;
