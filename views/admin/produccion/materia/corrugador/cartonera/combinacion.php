5472da0107bbfee0f2afa0a5fd6365d36ce8ab0c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-20 09:25:34 -0500

    Update combinacion.php

--- views/admin/produccion/materia/corrugador/cartonera/combinacion.php
+++ views/admin/produccion/materia/corrugador/cartonera/combinacion.php
@@ -1,1 +1,15 @@
 <h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
+
+
+
+
+
+
+<script>
+
+document.addEventListener("DOMContentLoaded", () => {
+    let pedidos = JSON.parse(localStorage.getItem("pedidosFiltrados")) || [];
+    console.log(pedidos);
+});
+
+</script>
