fb08d00e4dcd115378028a229a6f0572c56a2b4d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-27 13:03:36 -0500

    Update combinacion.php

--- views/admin/produccion/materia/corrugador/cartonera/combinacion.php
+++ views/admin/produccion/materia/corrugador/cartonera/combinacion.php
@@ -1,29 +1,4 @@
 
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <h1>Combinación</h1>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <form action="" method="post">
-                <div class="form-group
-                    <label for="bobina-1880">Bobina 1880</label>
-                    <input type="number" class="form-control" id="bobina-1880" name="bobina-1880" value="0">
-                </div>
-                <div class="form-group
-                    <label for="bobina-1100">Bobina 1100</label>
-                    <input type="number" class="form-control" id="bobina-1100" name="bobina-1100" value="0">
-                </div>
-                <div class="form-group
-                    <label for="total">Total</label>
-                    <input type="number" class="form-control" id="total" name="total" value="0" readonly>   
-                </div>
-                <button type="submit" class="btn btn-primary">Guardar</button>
-            </form>
-        </div>
-
 
 <script>
 
@@ -31,17 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let pedidos = JSON.parse(localStorage.getItem("pedidosFiltrados")) || [];
 
     const bobinas = [1880, 1100];
+    const refile = 30;
 
     for(const bobina of bobinas) {
-        const input = document.getElementById(`bobina-${bobina}`);
-        input.addEventListener("input", () => {
-            const total = bobinas.reduce((acc, bobina) => {
-                const input = document.getElementById(`bobina-${bobina}`);
-                return acc + (input.value ? parseInt(input.value) : 0);
-            }, 0);
-            document.getElementById("total").value = total;
-        });
-
+        const disponible =bobina-refile;
+        console.log(disponible);
     }
 
 
