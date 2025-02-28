35056951d284fccb1f704c89a414345bd5c12502
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-28 17:11:50 -0500

    .

--- views/admin/produccion/materia/corrugador/cartonera/combinacion.php
+++ views/admin/produccion/materia/corrugador/cartonera/combinacion.php
@@ -7,14 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const bobinas = [1880, 1100];
     const refile = 30;
-    const Cavidades= [1, 2,3,4];
+    const Cavidades= 1
     for(const bobina of bobinas) {
         const disponible =bobina-refile;
         // pedidos 
        
         for(const pedido of pedidos) {
-            const {id, cantidad, fecha, nombre_pedido, ancho, largo, corte, cavidades} = pedido;
-            console.log(cavidades);
+         
+            
 
     }
 
