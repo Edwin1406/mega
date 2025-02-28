ea1cc29b40690446f7283ca11120915392b8b3a0
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-28 10:10:38 -0500

    .

--- views/admin/produccion/materia/corrugador/cartonera/combinacion.php
+++ views/admin/produccion/materia/corrugador/cartonera/combinacion.php
@@ -10,46 +10,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     for(const bobina of bobinas) {
         const disponible =bobina-refile;
+       
         console.log(disponible);
-
-        for(let i=0; i< pedidos.length; i++) {
-            let mejorCombinacionLocal = null;
-            const pedidoActual = pedidos[i];
-            for(let j=i+1;j< pedidos.length; j++ ){
-               const otroPedido =pedidos[j];
-            //    suma de anchos
-            const suma = parseFloat(pedidoActual.ancho) + parseFloat(otroPedido.ancho);
-            if(suma <= disponible){
-                let pedido1 = pedidoActual;
-                let pedido2 = otroPedido;
-                if (pedido2.cantidad < pedido1.cantidad) {
-                     [pedido1, pedido2] = [pedido2, pedido1];
-                }
-                const sobrante = disponible - suma;
-                const metrosLineales = (pedido1.cantidad * pedido1.largo) / 1000;
-                const corte2 = metrosLineales / (pedido2.largo / 1000);
-                const corte1 = metrosLineales / (pedido1.largo / 1000);
-                if (corte2 < 0) continue;
-
-                        if (!mejorCombinacionLocal || sobrante < mejorCombinacionLocal.sobrante) {
-                            mejorCombinacionLocal = {
-                                bobina,
-                                pedidos: [pedido1, pedido2],
-                                suma,
-                                sobrante,
-                                cavidad: "1<br>1",
-                                corteP1: pedido1.cantidad,
-                                corteP2: Math.max(0, parseInt(corte2)),
-                                metrosLineales,
-                            };
-                        }
-                    console.log(mejorCombinacionLocal);
-
-            }
-
-            }
-
-        }
 
     }
 
