5a8be8ab096be535943258897acb6ba6bc3835af
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-02-28 10:16:52 -0500

    Update combinacion.php

--- views/admin/produccion/materia/corrugador/cartonera/combinacion.php
+++ views/admin/produccion/materia/corrugador/cartonera/combinacion.php
@@ -13,33 +13,8 @@ document.addEventListener("DOMContentLoaded", () => {
         // pedidos 
        
         for(const pedido of pedidos) {
-            const {id, cantidad, cliente, fecha, nombre_pedido, ancho, largo, corte, cavidades} = pedido;
-            const total = cantidad * cavidades;
-            const totalBobinas = total / disponible;
-            const totalBobinasRedondeado = Math.ceil(totalBobinas);
-            const totalBobinasReales = totalBobinasRedondeado * disponible;
-            const totalBobinasRealesCavidades = totalBobinasReales / cavidades;
-            const totalBobinasRealesCavidadesRedondeado = Math.ceil(totalBobinasRealesCavidades);
-            const totalBobinasRealesCavidadesRedondeado2 = Math.ceil(totalBobinasRealesCavidades/2);
-
-            console.log(`
-                Pedido: ${id}
-                Producto: ${nombre_pedido}
-                Ancho: ${ancho}
-                Largo: ${largo}
-                Corte: ${corte}
-                Cantidad: ${cantidad}
-                Cavidades: ${cavidades}
-                Total: ${total}
-                Bobina: ${bobina}
-                Disponible: ${disponible}
-                Total Bobinas: ${totalBobinas}
-                Total Bobinas Redondeado: ${totalBobinasRedondeado}
-                Total Bobinas Reales: ${totalBobinasReales}
-                Total Bobinas Reales Cavidades: ${totalBobinasRealesCavidades}
-                Total Bobinas Reales Cavidades Redondeado: ${totalBobinasRealesCavidadesRedondeado}
-                Total Bobinas Reales Cavidades Redondeado2: ${totalBobinasRealesCavidadesRedondeado2}
-            `);
+            const {id, cantidad, fecha, nombre_pedido, ancho, largo, corte, cavidades} = pedido;
+            console.log(cavidades);
 
     }
 
