b75fdfafc9f102dac19c390d1213b32153aa21c0
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-12 14:41:31 -0500

    Update Productos_inventario.php

--- models/Productos_inventario.php
+++ models/Productos_inventario.php
@@ -4,11 +4,12 @@ namespace Model;
 class Productos_inventario extends ActiveRecord
 {
     protected static $tabla = 'productos_inventario';
-    protected static $columnasDB = ['id_producto', 'nombre_producto','id_categoria','stock_actual','costo_unitario'];
+    protected static $columnasDB = ['id_producto', 'nombre_producto','id_categoria','id_area','stock_actual','costo_unitario'];
 
     public $id_producto;
     public $nombre_producto;
     public $id_categoria;
+    public $id_area;
     public $stock_actual;
     public $costo_unitario;
 
@@ -17,6 +18,7 @@ class Productos_inventario extends ActiveRecord
         $this->id_producto = $args['id_producto'] ?? '';
         $this->nombre_producto = $args['nombre_producto'] ?? '';
         $this->id_categoria = $args['id_categoria'] ?? '';
+        $this->id_area = $args['id_area'] ?? '';
         $this->stock_actual = $args['stock_actual'] ?? '';
         $this->costo_unitario = $args['costo_unitario'] ?? '';
 
