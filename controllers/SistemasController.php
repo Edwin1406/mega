869458d9b8ea7080f231b366fb3ffcf907020e45
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-12 11:32:07 -0500

    .

--- controllers/SistemasController.php
+++ controllers/SistemasController.php
@@ -16,6 +16,37 @@ class SistemasController {
 
     public static function crear(Router $router)
     {
+
+
+        $comercial = new Comercial;
+
+        $escoger_produccion = Area::belongsTo('propietarioId',$id);
+
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $comercial->sincronizar($_POST);
+            $comercial->total_item = $comercial->cantidad * $comercial->precio;
+            $comercial->transito = $comercial->fecha_produccion - $comercial->arribo_planta;
+            $comercial->calcularTransito();
+
+            // debuguear($comercial);
+            $alertas = $comercial->validar();
+
+            // debuguear($comercial);
+
+           if (empty($alertas)) {
+                $comercial->guardar();
+                $alertas = $comercial->getAlertas();
+                header('Location: /admin/comercial/tabla?id='.$id);
+            }
+
+
+        }
+
+
+
+
+
         
         $alertas = [];
         $router->render('admin/sistemas/productos/crear', [
---
+++ models/Productos_inventario.php
@@ -0,0 +1,26 @@
+<?php 
+namespace Model;
+
+class Productos_inventario extends ActiveRecord
+{
+    protected static $tabla = 'productos_inventario';
+    protected static $columnasDB = ['id_producto', 'nombre_producto','id_categoria','stock_actual','costo_unitario'];
+
+    public $id_producto;
+    public $nombre_producto;
+    public $id_categoria;
+    public $stock_actual;
+    public $costo_unitario;
+
+    public function __construct($args = [])
+    {
+        $this->id_producto = $args['id'] ?? null;
+        $this->id_producto = $args['id_producto'] ?? '';
+        $this->nombre_producto = $args['nombre_producto'] ?? '';
+        $this->id_categoria = $args['id_categoria'] ?? '';
+        $this->stock_actual = $args['stock_actual'] ?? '';
+        $this->costo_unitario = $args['costo_unitario'] ?? '';
+
+
+    }
+}
