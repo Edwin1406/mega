11259e04a4545ec93a177b5f526e2307c48308f6
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-13 10:18:12 -0500

    Update SistemasController.php

--- controllers/SistemasController.php
+++ controllers/SistemasController.php
@@ -62,7 +62,7 @@ class SistemasController {
 public static function movimientos(Router $router) {
     $alertas = [];
     // $productos_inventario = Productos_inventario::all('DESC');
-    $productos_inventario = Productos_inventario::allSis('producto', 'ASC');
+    $productos_inventario = Productos_inventario::allSis('producto');
     $area_inventario = Area_inventario::allSis('area', 'ASC');
     $categoria_inventario = Categoria_inventario::allSis('categoria', 'ASC');
 
