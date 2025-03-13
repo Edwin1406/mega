5071e3a3d5e3a0e895816a174d1a9c1e8e04e758
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-13 10:09:54 -0500

    Update SistemasController.php

--- controllers/SistemasController.php
+++ controllers/SistemasController.php
@@ -72,20 +72,20 @@ public static function movimientos(Router $router) {
         // $id_producto = $_POST['id'];
         $sincronizar->sincronizar($_POST);
         debuguear($sincronizar);
-        $sincronizar->guardar();
+        // $sincronizar->guardar();
 
-        $id_area = $_POST['id_area'];
-        $tipo_movimiento = $_POST['tipo_movimiento'];
-        $cantidad = $_POST['cantidad'];
+        // $id_area = $_POST['id_area'];
+        // $tipo_movimiento = $_POST['tipo_movimiento'];
+        // $cantidad = $_POST['cantidad'];
 
-        $movimientos_invetario = new Movimientos_inventario([
-                    'id' => '',
-                    'id_producto' => $id_producto,
-                    'id_area' => $id_area,
-                    'tipo_movimiento' => $tipo_movimiento,
-                    'cantidad' => $cantidad,
-                    'fecha_movimiento' => date('Y-m-d H:i:s')
-        ]);
+        // $movimientos_invetario = new Movimientos_inventario([
+        //             'id' => '',
+        //             'id_producto' => $id_producto,
+        //             'id_area' => $id_area,
+        //             'tipo_movimiento' => $tipo_movimiento,
+        //             'cantidad' => $cantidad,
+        //             'fecha_movimiento' => date('Y-m-d H:i:s')
+        // ]);
         
        
        
