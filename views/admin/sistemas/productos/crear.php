0289cae0060d9b1446d96ce73c507fac02bfd832
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-14 12:57:03 -0500

    Update crear.php

--- views/admin/sistemas/productos/crear.php
+++ views/admin/sistemas/productos/crear.php
@@ -72,7 +72,7 @@
 
 <?php include_once __DIR__.'/../../../templates/alertas.php'  ?>
 
-    <form method="POST" action="/admin/sistemas/productos/crear "  class="formulario" enctype="multipart/form-data">
+    <form  class="formulario" enctype="multipart/form-data">
     <?php include_once __DIR__.'/formulario.php'  ?>
 
         <input class="formulario__submit formulario__submit--registrar" type="submit" value="Registrar producto">
