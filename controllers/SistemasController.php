d0b4907112df7e238a41aba5499687d4817be4f2
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-14 14:56:05 -0500

    Update SistemasController.php

--- controllers/SistemasController.php
+++ controllers/SistemasController.php
@@ -191,27 +191,17 @@ public static function solicitud(Router $router)
     $productos_inventario = Productos_inventario::allSis('producto','DESC');
     $area_inventario = Area_inventario::allSis('area', 'ASC');
     $categoria_inventario = Categoria_inventario::allSis('categoria', 'ASC');
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Leer los datos recibidos
-        $jsonData = file_get_contents('php://input');
-        $decodedData = json_decode($jsonData, true);
-    
-        // Verificar si los datos son válidos
-        if ($decodedData) {
-            // Procesar los productos
-            $solicitud_inventario = new Solicitud($decodedData['productos']);
-            $resultado = $solicitud_inventario->guardar();
-    
-            // Enviar respuesta en formato JSON
-            header('Content-Type: application/json');
-            echo json_encode($resultado);
-        } else {
-            // Si los datos no son válidos, enviar un error
-            header('Content-Type: application/json');
-            echo json_encode(['error' => 'Datos inválidos o faltantes']);
-        }
+        
+        $solicitud_inventario = new Solicitud($_POST);
+
+        // debuguear($solicitud_inventario);
+        $resultado =$solicitud_inventario->guardar();
+        
+        echo json_encode($resultado);
     }
-    
+
 
 
     $router->render('admin/sistemas/solicitudes/solicitud', [
