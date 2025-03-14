5fefd522ceb8e7ab127f0fb3129cf3832aa09c73
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-14 10:04:39 -0500

    .

---
+++ views/admin/sistemas/solicitudes/solicitud.php
@@ -0,0 +1,13 @@
+<div class="dashboard__formulario">
+
+<?php include_once __DIR__.'/../../../templates/alertas.php'  ?>
+
+    <form method="POST" action="/admin/sistemas/solicitudes/solicitud "  class="formulario" enctype="multipart/form-data">
+    <?php include_once __DIR__.'/formulario.php'  ?>
+
+        <input class="formulario__submit formulario__submit--registrar" type="submit" value="Registrar producto">
+
+        
+    </form>
+
+</div>
