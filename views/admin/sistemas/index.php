72c8c83a84cffdeda807e3f48ec5e730f1832390
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-17 12:12:35 -0500

    Update index.php

--- views/admin/sistemas/index.php
+++ views/admin/sistemas/index.php
@@ -52,14 +52,13 @@
       width: 20%;
     }
 
-    @media (mim-width: 768px) {
-      .containers {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-
-      }
-        
+    @media (min-width: 768px) {
+        .item:nth-child(1), .item:nth-child(2), .item:nth-child(3), .item:nth-child(4), .item:nth-child(5) {
+           display: flex;
+              justify-content: center;
+                align-items: center;
+                flex-direction: column;
+        }
     }
     
 
