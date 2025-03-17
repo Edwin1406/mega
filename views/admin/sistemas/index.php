df3218dd8c531f3e0c6904c3a5272f62d6d58f29
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-17 10:47:09 -0500

    Update index.php

--- views/admin/sistemas/index.php
+++ views/admin/sistemas/index.php
@@ -89,6 +89,9 @@
 
     <div style="width: 80%; margin: auto;">
         <canvas id="myChart"></canvas>
+
+        <canvas id="totalgeneral"></canvas>
+
     </div>
 
 <script>
@@ -248,6 +251,55 @@ async function sumadevaloresdeapi(){
 
     console.log("Total general:", TotalGeneral);
 
+
+    const ctx = document.getElementById('totalgeneral').getContext('2d');
+        
+        const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: 'My First Dataset',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(201, 203, 207, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                    'rgb(153, 102, 255)',
+                    'rgb(201, 203, 207)'
+                ],
+                borderWidth: 1
+            }]
+        };
+
+        const config = {
+            type: 'bar',
+            data: data,
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        };
+        
+        new Chart(ctx, config);
+
+
+
+
 }
 
 sumadevaloresdeapi();
@@ -256,3 +308,8 @@ sumadevaloresdeapi();
 
 
 </script>
+
+
+
+
+
