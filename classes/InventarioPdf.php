e07e9d4e0b1ad0c8a54d5207cab4cc8a9d82c10c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-19 16:33:42 -0500

    Update InventarioPdf.php

--- classes/InventarioPdf.php
+++ classes/InventarioPdf.php
@@ -49,7 +49,7 @@ class InventarioPdf extends TCPDF
         $this->SetFont('helvetica', 'B', 14); // Aumentar tamaño de fuente
     
         // Centrar la tabla
-        $totalAnchoTabla = 100 + 40 + 30 + 20 + 100; // Ancho total de la tabla
+        $totalAnchoTabla = 105 + 40 + 30 + 20 + 100; // Ancho total de la tabla
         $margenIzquierdo = ($this->getPageWidth() - $totalAnchoTabla) / 2; // Calcular el margen izquierdo para centrar
         $this->SetX($margenIzquierdo);
         
