f6b1c0ea4156c102c47cb2526100fd289bb83ba7
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-19 10:48:36 -0500

    Update index.php

--- views/admin/sistemas/index.php
+++ views/admin/sistemas/index.php
@@ -301,7 +301,7 @@ async function sumadevaloresdeapi(){
         console.log("Fecha procesada:", itemDate, "Mes:", itemDate.getMonth());
 
         const monthIndex = itemDate.getMonth();
-        monthlyTotals[monthIndex] += Number(item.costo_nuevo); 
+        monthlyTotals[monthIndex] += Number(item.costo_nuevo*cantidad); 
     });
 
     console.log("Total acumulado por mes:", monthlyTotals);
