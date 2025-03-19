0d57d87dfea379eb45716f0df61fb9a6c2a2e676
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-19 15:59:54 -0500

    Update index.php

--- views/admin/sistemas/index.php
+++ views/admin/sistemas/index.php
@@ -329,7 +329,7 @@ async function sumadevaloresdeapi(){
         if (parseInt(monthIndex) === currentMonth) { // Compara el índice del mes con el mes actual
             totalgeneralpormes += total;
             // Actualiza el contenido del span con el total correspondiente al mes actual
-            document.querySelector('.areas-produccion__numero.totales').textContent = totalgeneralpormes + ' USD';
+            document.querySelector('.areas-produccion__numero.totales').textContent = totalgeneralpormes + ' $';
         }
     }
 
