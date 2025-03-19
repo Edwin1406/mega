92cd7e16747b0a438964652027af8793cf6a6867
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-19 15:10:45 -0500

    Update admin-sidebar.php

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -1,11 +1,13 @@
 <aside class="dashboard__sidebar">
     <nav class="dashboard__menu">
+    <?php if (isset($_SESSION['email'])): ?>
         <a href="/admin/index" class="dashboard__enlace <?php echo pagina_actual_admin('/index') ? 'dashboard__enlace--actual' :'' ?>">
             <i class="fa-solid fa-house dashboard__icono"></i>
             <span class="dashboard__menu--texto">
                 Inicio
             </span>
         </a>
+        <?php endif; ?>
 
        
         <!-- <?php 
@@ -32,13 +34,15 @@
         <?php endif; ?>
 
 
-
+        <?php if (isset($_SESSION['email'])): ?>
         <a href="/admin/area/escoger" class="dashboard__enlace <?php echo pagina_actual_admin('/escoger') ? 'dashboard__enlace--actual' :'' ?>">
             <i class="fa-duotone fa-solid fa-calendar-days dashboard__icono"></i>
             <span class="dashboard__menu--texto">
              Escoger Areas 
             </span>
         </a>
+        <?php endif; ?>
+
 
        
        
