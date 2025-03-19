cd71da55cea05a0b434ee9c4617bcc2f8337031b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-18 20:22:56 -0500

    Update InventarioPdf.php

--- classes/InventarioPdf.php
+++ classes/InventarioPdf.php
@@ -76,7 +76,7 @@ class InventarioPdf extends TCPDF
     
         // Total final
         $this->SetFont('helvetica', 'B', 12);
-        $this->Cell(180, 8, 'TOTAL:', 1, 0, 'R');
+        $this->Cell(200, 8, 'TOTAL:', 1, 0, 'R');
         $this->Cell(30, 8, '$' . number_format($total, 2), 1, 1, 'C');
     
         // Nota al pie
