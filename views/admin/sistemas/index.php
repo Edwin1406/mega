5aaa0876fddcf8c13176e4e3889a53ad54fb0c5e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-20 11:04:05 -0500

    Update index.php

--- views/admin/sistemas/index.php
+++ views/admin/sistemas/index.php
@@ -112,7 +112,7 @@
     .grafico1{
         width: 48%;
         /* background-image: linear-gradient(120deg,rgb(229, 235, 225) 0%,rgb(204, 212, 189) 100%); */
-        background-image: white;
+        background-image: rgb(255, 255, 255);
         border-radius: 10px;
         padding: 1rem;
     }
