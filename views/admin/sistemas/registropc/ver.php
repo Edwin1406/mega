681c20214301a0b051506358c654ec62f0d318cb
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-26 16:07:56 -0500

    Update ver.php

--- views/admin/sistemas/registropc/ver.php
+++ views/admin/sistemas/registropc/ver.php
@@ -6,21 +6,5 @@
 
 
 <div class="contenedor">
-    <div class="container">
-        <div class="item">
-            <a href="<?php echo ROUTE_URL?>/admin/sistemas/registropc/ver">VER</a>
-        </div>
-        <div class="item">
-            <a href="<?php echo ROUTE_URL?>/admin/sistemas/registropc/agregar">AGREGAR</a>
-        </div>
-        <div class="item">
-            <a href="<?php echo ROUTE_URL?>/admin/sistemas/registropc/editar">EDITAR</a>
-        </div>
-        <div class="item">
-            <a href="<?php echo ROUTE_URL?>/admin/sistemas/registropc/eliminar">ELIMINAR</a>
-        </div>
-        <div class="item">
-            <a href="<?php echo ROUTE_URL?>/admin/sistemas/registropc/version">VERSION</a>
-        </div>
-    </div>
+        
 </div>
