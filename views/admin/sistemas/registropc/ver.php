170f92ed0e2dbb2f55ccb261539f1caff9982b71
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-26 17:10:04 -0500

    Update ver.php

--- views/admin/sistemas/registropc/ver.php
+++ views/admin/sistemas/registropc/ver.php
@@ -136,70 +136,11 @@
         </div>
     </div>
     <div class="caracteristicas">
-        <div class="caracteristicas__item">
-            <p> ID DEL EQUIPO: </p>
-            <p> <?php echo $computadora->numero_interno ?> </p>
-        
-        </div>
-        <div class="caracteristicas__item">
-            <p> AREA: </p>
-            <p> <?php echo $computadora->area ?> </p>
-        </div>
-        
-        <div class="caracteristicas__item">
-            <p> USUARIO ASIGNADO: </p>
-            <p> <?php echo $computadora->usuario_asignado ?> </p>
-        </div>
-        
-        <div class="caracteristicas__item">
-            <p> FECHA COMPRA: </p>
-            <p> <?php echo $computadora->fecha_compra ?> </p>
-        </div>
-        
-        <div class="caracteristicas__item">
-            <p> MARCA O MODELO: </p>
-            <p> <?php echo $computadora->marca_modelo ?> </p>
-        </div>
-        
-        <div class="caracteristicas__item">
-            <p> PROCESADOR: </p>
-            <p> <?php echo $computadora->procesador ?> </p>
-        </div>
-        
-        <div class="caracteristicas__item">
-            <p> RAM: </p>
-            <p> <?php echo $computadora->ram ?> </p>
-        </div>
-        
-        <div class="caracteristicas__item">
-            <p> DISCO DURO: </p>
-            <p> <?php echo $computadora->disco_duro ?> </p>
-        </div>
-        
-        <div class="caracteristicas__item">
-            <p> SISTEMA OPERATIVO: </p>
-            <p> <?php echo $computadora->sistema_operativo ?> </p>
-        </div>
-        
-        <div class="caracteristicas__item">
-            <p> ESTADO ACTUAL: </p>
-            <p> <?php echo $computadora->estado_actual ?> </p>
-        </div>
-        
-        <div class="caracteristicas__item">
-            <p> DIRECCION IP: </p>
-            <p> <?php echo $computadora->direccion_ip ?> </p>
-        </div>
-        
-        <div class="caracteristicas__item">
-            <p> CONTRASEÑA: </p>
-            <p> <?php echo $computadora->contrasena ?> </p>
-        </div>
-        
-        <div class="caracteristicas__item">
-            <p> FECHA DE REGISTRO: </p>
-            <p> <?php echo $computadora->created_at ?> </p>
-        </div>
+            <!-- formualrio  -->
+            <form method="POST" class="formulario" action="/admin/sistemas/registropc/eliminar">
+                <input type="hidden" name="id" value="<?php echo $computadora->id; ?>">
+                <input type="submit" value="Eliminar" class="formulario__submit">
+            </form>
     </div>
 
     
