f616edff597d18b9fb35139abd4aa47d44dad5bb
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-26 16:54:53 -0500

    Update ver.php

--- views/admin/sistemas/registropc/ver.php
+++ views/admin/sistemas/registropc/ver.php
@@ -58,7 +58,7 @@
 
 
 <div class="contenido">
-    <p><b>Usuario</b><?php echo $computadora->usuario_asignado ?></p>
+    <p><b>Usuario Asignado:</b><?php echo $computadora->usuario_asignado ?></p>
     <p><b>Area:</b><?php echo $computadora->area ?></p>
 </div>
 
