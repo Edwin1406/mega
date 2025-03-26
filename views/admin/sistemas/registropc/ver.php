541aff655cc4201bcd3197cad50228ef3031fd6f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-26 17:00:05 -0500

    Update ver.php

--- views/admin/sistemas/registropc/ver.php
+++ views/admin/sistemas/registropc/ver.php
@@ -136,3 +136,66 @@
         </div>
     </div>
 </div>
+
+
+
+
+<h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
+
+<div class="dashboard__contenedor-boton">
+    <a class="dashboard__boton" href="/admin/produccion/registro_produccion">
+    <i class="fa-solid fa-circle-arrow-left"></i>
+        REGRESAR A INICIO
+    </a>
+
+</div>
+
+
+<div class="dashboard__contenedor">
+    <?php if (!empty($bobinas)): ?>
+        <table class="table">
+            <thead class="table__thead">
+                <tr>
+                    <th scope="col" class="table__th">Tipo de Papel</th>
+                    <th scope="col" class="table__th">Gramaje</th>
+                    <th scope="col" class="table__th">Ancho</th>
+                    <th scope="col" class="table__th">Fecha y Hora </th>
+                    <th scope="col" class="table__th"></th>
+                </tr>
+            </thead>
+            <tbody class="table__tbody">
+
+                <?php foreach ($bobinas as $bobina):?>
+                    <tr class="table__tr">
+                        <td class="table__td"><?php echo $bobina->tipo_papel?></td>
+                        <td class="table__td"><?php echo $bobina->gramaje?></td>
+                        <td class="table__td"><?php echo $bobina->ancho?></td>
+                        <td class="table__td"><?php echo $bobina->created_at?></td>
+                        <td class="table__td--acciones"><a class="table__accion table__accion--editar" href="/admin/produccion/papel/editar?id=<?php echo $bobina->id; ?>"><i class="fa-solid fa-user-pen"></i>Editar</a>
+                        <form method="POST" action="/admin/produccion/papel/eliminar" class="table__formulario">
+                            <input type="hidden" name="id" value="<?php echo $bobina->id; ?>">
+                            <button class="table__accion table__accion--eliminar" type="submit">
+                                <i class="fa-solid fa-user-slash"></i>
+                                    Eliminar
+                            </button>
+                        </form>
+                        </td>
+
+
+                    </tr>
+                <?php endforeach;?>
+
+            </tbody>
+
+        </table>
+   
+
+
+    <?php else: ?>
+        <a class="text-center"> No hay Papel Aún</a>
+    <?php endif; ?>
+</div>
+
+
+<?php echo $paginacion; ?>
+
