bc5d9d0f68a263944c16ba25ba4e2891a45975ed
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-26 12:20:45 -0500

    Update formulario.php

--- views/admin/sistemas/registropc/formulario.php
+++ views/admin/sistemas/registropc/formulario.php
@@ -1,7 +1,7 @@
 <fieldset class="formulario__fieldset">
     <legend class="formulario__legend">Información de Registro de computadora</legend>
     <div class="formulario__campo">
-        <label class="formulario__label" for="nombre">Nombre</label>
+        <label class="formulario__label" for="nombre">ID DEL EQUIPO</label>
         <input
             type="text"
             name="nombre"
@@ -12,7 +12,7 @@
     </div>
 
     <div class="formulario__campo">
-        <label class="formulario__label" for="num_cuchillas">numero cuchillas</label>
+        <label class="formulario__label" for="num_cuchillas">AREA</label>
         <input
             type="text"
             name="num_cuchillas"
@@ -23,7 +23,7 @@
     </div>
 
     <div class="formulario__campo">
-        <label class="formulario__label" for="ancho_maximo">Ancho maximo</label>
+        <label class="formulario__label" for="ancho_maximo">USUARIO ASIGNADO</label>
         <input
             type="text"
             name="ancho_maximo"
@@ -34,7 +34,7 @@
     </div>
 
     <div class="formulario__campo">
-        <label class="formulario__label" for="gramaje_maximo">gramaje_maximo</label>
+        <label class="formulario__label" for="gramaje_maximo">FECHA COMPRA </label>
         <input
             type="text"
             name="gramaje_maximo"
@@ -44,4 +44,68 @@
             value="<?php echo $maquina->gramaje_maximo ?? '' ?>">
     </div>
 
+    <div class="formulario__campo">
+        <label class="formulario__label" for="gramaje_maximo">MARCA O MODELO </label>
+        <input
+            type="text"
+            name="gramaje_maximo"
+            id="gramaje_maximo"
+            class="formulario__input"
+            placeholder="gramaje_maximo de la maquina"
+            value="<?php echo $maquina->gramaje_maximo ?? '' ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="gramaje_maximo">PROCESADOR </label>
+        <input
+            type="text"
+            name="gramaje_maximo"
+            id="gramaje_maximo"
+            class="formulario__input"
+            placeholder="gramaje_maximo de la maquina"
+            value="<?php echo $maquina->gramaje_maximo ?? '' ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="gramaje_maximo">RAM </label>
+        <input
+            type="text"
+            name="gramaje_maximo"
+            id="gramaje_maximo"
+            class="formulario__input"
+            placeholder="gramaje_maximo de la maquina"
+            value="<?php echo $maquina->gramaje_maximo ?? '' ?>">
+    </div>
+
+   <div class="formulario__campo">
+        <label class="formulario__label" for="gramaje_maximo">DISCO </label>
+        <input
+            type="text"
+            name="gramaje_maximo"
+            id="gramaje_maximo"
+            class="formulario__input"
+            placeholder="gramaje_maximo de la maquina"
+            value="<?php echo $maquina->gramaje_maximo ?? '' ?>">
+    </div>
+   <div class="formulario__campo">
+        <label class="formulario__label" for="gramaje_maximo">SISTEMA OPERATIVO  </label>
+        <input
+            type="text"
+            name="gramaje_maximo"
+            id="gramaje_maximo"
+            class="formulario__input"
+            placeholder="gramaje_maximo de la maquina"
+            value="<?php echo $maquina->gramaje_maximo ?? '' ?>">
+    </div>
+
+    <div class="formulario__campo">
+    <label class="formulario__label" for="estado_actual">ESTADO ACTUAL  </label>
+    <select name="estado_actual" id="estado_actual" class="formulario__input">
+        <option value="Bueno">Bueno</option>
+        <option value="Regular">Regular</option>
+        <option value="Mal estado">Mal Estado</option>
+    </select>
+    </div>
+
+ 
 </fieldset>
