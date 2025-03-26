38ac0aa767b5a514de3e6339299ac1c96fc161ce
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-03-26 16:09:58 -0500

    Update ver.php

--- views/admin/sistemas/registropc/ver.php
+++ views/admin/sistemas/registropc/ver.php
@@ -6,5 +6,19 @@
 
 
 <div class="contenedor">
+    <div class="caracteristicas">
+        <h1> informacion de la computadora </h1>
+        <div class="caracteristicas__item">
+            <p> ID DEL EQUIPO: </p>
+            <p> <?php echo $computadora->numero_interno ?> </p>
         
+        </div>
+        <div class="caracteristicas__item">
+            <p> AREA: </p>
+            <p> <?php echo $computadora->area ?> </p>
+
+        
+
+
+    </div>
 </div>
