ad5fcefd12a3b7716dd2b59e03a616ecc05e5d4a
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-04-02 10:44:26 -0500

    Update vistaTicket.php

--- views/admin/sistemas/ticket/vistaTicket.php
+++ views/admin/sistemas/ticket/vistaTicket.php
@@ -1,1 +1,22 @@
 <h1 class="dashboard__heading"> <?php echo $titulo ?> </h1>
+
+
+
+
+
+<!-- visualizar el ticket generado  -->
+<div class="dashboard__ticket">
+    <div class="dashboard__ticket--info">
+        <h2 class="dashboard__ticket--heading">Ticket #<?php echo $ticket->id ?></h2>
+        <p class="dashboard__ticket--fecha">Fecha de creación: <?php echo $ticket->fecha_creacion ?></p>
+        <p class="dashboard__ticket--estado">Estado: <?php echo $ticket->estado ?></p>
+        <p class="dashboard__ticket--prioridad">Prioridad: <?php echo $ticket->prioridad ?></p>
+        <p class="dashboard__ticket--categoria">Categoría: <?php echo $ticket->categoria ?></p>
+        <p class="dashboard__ticket--calificacion">Calificación: <?php echo $ticket->calificacion ?></p>
+    </div>
+
+    <div class="dashboard__ticket--descripcion">
+        <h3>Descripción del problema:</h3>
+        <p><?php echo $ticket->descripcion ?></p>
+    </div>
+</div>
