5f96b0e7df2dc6a8b3ba7bce4034ac7547663a82
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-04-02 10:51:45 -0500

    Update vistaTicket.php

--- views/admin/sistemas/ticket/vistaTicket.php
+++ views/admin/sistemas/ticket/vistaTicket.php
@@ -47,6 +47,12 @@
         text-align: center;
     }
 
+    h3{
+        font-size: 1.5rem;
+        font-weight: bold;
+        margin-bottom: 0.5rem;
+    }
+
 
 
 </style>
@@ -55,11 +61,11 @@
 <div class="dashboard__ticket">
     <div class="dashboard__ticket--info">
         <h2 class="dashboard__ticket--heading">Ticket #<?php echo $ticket->id ?></h2>
-        <p class="dashboard__ticket--fecha">Fecha de creación: <?php echo $ticket->fecha_creacion ?></p>
-        <p class="dashboard__ticket--estado">Estado: <?php echo $ticket->estado ?></p>
-        <p class="dashboard__ticket--prioridad">Prioridad: <?php echo $ticket->prioridad ?></p>
-        <p class="dashboard__ticket--categoria">Categoría: <?php echo $ticket->categoria ?></p>
-        <p class="dashboard__ticket--calificacion">Calificación: <?php echo $ticket->calificacion ?></p>
+        <p class="dashboard__ticket--fecha"><b>Fecha de creación:</b> <?php echo $ticket->fecha_creacion ?></p>
+        <p class="dashboard__ticket--estado"><b>Estado:</b><?php echo $ticket->estado ?></p>
+        <p class="dashboard__ticket--prioridad"><b>Prioridad: </b><?php echo $ticket->prioridad ?></p>
+        <p class="dashboard__ticket--categoria"><b>Categoría: </b><?php echo $ticket->categoria ?></p>
+        <p class="dashboard__ticket--calificacion"><b>Calificación:</b> <?php echo $ticket->calificacion ?></p>
     </div>
 
     <div class="dashboard__ticket--descripcion">
