27be692af17bfa35d3e29359e1c8829709cd1cb5
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-04-02 09:28:21 -0500

    Update SistemasController.php

--- controllers/SistemasController.php
+++ controllers/SistemasController.php
@@ -603,12 +603,11 @@ public static function crearTicket(Router $router){
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $ticket->sincronizar($_POST);
         // guardar el ticket
-        $ticket->estado = 'abierto';
         $ticket->fecha_creacion = date('Y-m-d H:i:s');
         $ticket->computadora_id = $_POST['computadora_id'];
         $ticket->descripcion = $_POST['descripcion'];
         $ticket->prioridad = $_POST['prioridad'];
-        $ticket->estado = $_POST['estado'];
+        $ticket->estado ='abierto';
         $ticket->categoria = $_POST['categoria'];
         // debuguear($ticket);
         $alertas = $ticket->validar();
@@ -619,7 +618,7 @@ public static function crearTicket(Router $router){
             $alertas = $ticket->getAlertas();
             header('Location: /admin/sistemas/ticket/tabla');
         }
-        
+
 
 
         debuguear($ticket);
