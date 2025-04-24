3daab3c76e30f129e1d73aafcd7ccadc89c4748c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-04-23 23:49:24 -0500

    Update corrugadorVentana.php

--- views/admin/produccion/materia/corrugadorVentana.php
+++ views/admin/produccion/materia/corrugadorVentana.php
@@ -668,12 +668,7 @@ table.dataTable {
   <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
 
   <style>
-    body {
-      margin: 0;
-      padding: 0;
-      background-color: #f5f5f5;
-      font-family: Arial, sans-serif;
-    }
+ 
 
     h2 {
       text-align: center;
