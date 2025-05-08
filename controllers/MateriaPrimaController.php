f54d64d53c4249b2bcefdccbe32646015a51ad0e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-08 11:04:47 -0500

    Update MateriaPrimaController.php

--- controllers/MateriaPrimaController.php
+++ controllers/MateriaPrimaController.php
@@ -252,7 +252,8 @@ class MateriaPrimaController
                     echo 'Archivo subido correctamente';
 
                     // Llamar al método de Producto para procesar el archivo
-                    if (MateriaPrimaV::procesarArchivoExcelMateria($rutaDestino)) {
+                    // if (MateriaPrimaV::procesarArchivoExcelMateria($rutaDestino)) {
+                    if (Pedido::procesarArchivoExcelpedidos($rutaDestino)) {
                         header('Location: /admin/produccion/materia/crear');
                     } else {
                         echo 'Hubo un error al procesar el archivo Excel';
