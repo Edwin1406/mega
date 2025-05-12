308bce47c8fb2997a9d93ce0c08de09a31891bed
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-12 14:20:49 -0500

    Update MateriaPrimaController.php

--- controllers/MateriaPrimaController.php
+++ controllers/MateriaPrimaController.php
@@ -527,7 +527,7 @@ class MateriaPrimaController
     // Obtén los datos desde la consulta base
     $corrugador = Comercial::allcIMPORT('ASC', 'CAJAS-KRAFT');
 
-    // debuguear($corrugador);
+    debuguear($corrugador);
     
     // Procesa los datos para agrupar por gramaje y ancho
     $agregados = [];
