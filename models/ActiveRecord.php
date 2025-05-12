f6ff6f7da1b8d49a417c10cb3b56bb4c2076ccdb
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-12 14:18:49 -0500

    k

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -449,7 +449,7 @@ public static function allcIMPORT($orden = 'DESC', $lineas = null) {
     // Convierte la cantidad a float para cada resultado
     foreach ($resultado as $item) {
         // decimal_format($item->cantidad, 3);
-        $item->cantidad = number_format((float)$item->cantidad, 1, '.', '');
+        $item->cantidad = $item->cantidad; // Convierte la cantidad a float
     }
 
     return $resultado;
