4e94d2942d6d3624fc9c8fd1716e8e95d9fa2435
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-13 16:17:23 -0500

    Update microcorrugador.php

--- views/admin/produccion/materia/microcorrugador.php
+++ views/admin/produccion/materia/microcorrugador.php
@@ -101,7 +101,7 @@
       position: fixed;
       top: 0; left: 0;
       width: 100%; height: 100%;
-      background-color: rgba(0, 0, 0, 0.5);
+      background-color: rgba(163, 110, 110, 0.5);
       justify-content: center;
       align-items: center;
       z-index: 1000;
