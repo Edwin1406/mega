392b1e84a448034dbbb14e39f9ceb413519f7cdf
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-14 08:58:34 -0500

    Update microcorrugador.php

--- views/admin/produccion/materia/microcorrugador.php
+++ views/admin/produccion/materia/microcorrugador.php
@@ -678,12 +678,9 @@ cargarDatos();
       const mesesConDatos = Array(12).fill(false);
       const totalesPorMes = Array(12).fill(0);
 
-
-           let lineaOriginal = item.linea ? item.linea.toUpperCase().trim() : '';
-            // if (!/^MICRO|^MEDIUM/.test(lineaOriginal)) return;
-            if (!/^MICRO/.test(lineaOriginal)) return;
-
-      data.forEach(item => {
+     data
+  .filter(item => item.linea !== "CAJAS-KRAFT" && item.linea !== "MEDIUM" && item.linea !== "CAJAS-BLANCO")
+  .forEach(item => {
 
         const fecha = new Date(item.fecha_corte);
         const mes = fecha.getMonth();
