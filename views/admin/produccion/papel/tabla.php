2220a8c809ddcf8ffa88c52c00fa363161320bb3
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-14 10:45:37 -0500

    Update tabla.php

--- views/admin/produccion/papel/tabla.php
+++ views/admin/produccion/papel/tabla.php
@@ -1,7 +1,7 @@
 <h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
 
 <div class="dashboard__contenedor-boton">
-    <a class="dashboard__boton" href="/admin/produccion/registro_produccion">
+    <a class="dashboard__boton" href="/admin/produccion/papel/crear">
     <i class="fa-solid fa-circle-arrow-left"></i>
         REGRESAR A INICIO
     </a>
