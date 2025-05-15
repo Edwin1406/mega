e5ba98f56011adba07e654b4343dfc837d972307
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-15 08:56:35 -0500

    Update tabla.php

--- views/admin/produccion/papel/tabla.php
+++ views/admin/produccion/papel/tabla.php
@@ -7,14 +7,18 @@
     </a>
 
 </div>
-
-
 <div class="dashboard__contenedor">
     <?php if (!empty($bobinas)): ?>
+
+        <?php 
+            function boldIfGreaterThanZero($value) {
+                return $value > 0 ? '<strong>' . $value . '</strong>' : $value;
+            }
+        ?>
+
         <table class="table">
             <thead class="table__thead">
                 <tr>
-                    <!-- <th scope="col" class="table__th">Tipo de maquina</th> -->
                     <th scope="col" class="table__th">Tipo de clasificacion</th>
                     <th scope="col" class="table__th">Single Face</th>
                     <th scope="col" class="table__th">Empalme</th>
@@ -35,54 +39,46 @@
                     <th scope="col" class="table__th">Extra trim</th>
                     <th scope="col" class="table__th">Consumo</th>
                     <th scope="col" class="table__th">Fecha</th>
-
-                    
                     <th scope="col" class="table__th">Acciones</th>
                 </tr>
             </thead>
             <tbody class="table__tbody">
 
-                <?php foreach ($bobinas as $bobina):?>
+                <?php foreach ($bobinas as $bobina): ?>
                     <tr class="table__tr">
-                        <!-- <td class="table__td"><?php echo $bobina->tipo_maquina?></td> -->
-                        <td class="table__td"><?php echo $bobina->tipo_clasificacion?></td>
-                        <td class="table__td"><?php echo $bobina->SINGLEFACE?></td>
-                        <td class="table__td"><?php echo $bobina->EMPALME?></td>
-                        <td class="table__td"><?php echo $bobina->RECUB?></td>
-                        <td class="table__td"><?php echo $bobina->MECANICO?></td>
-                        <td class="table__td"><?php echo $bobina->GALLET?></td>
-                        <td class="table__td"><?php echo $bobina->HUMEDO?></td>
-                        <td class="table__td"><?php echo $bobina->COMBADO?></td>
-                        <td class="table__td"><?php echo $bobina->DESPE?></td>
-                        <td class="table__td"><?php echo $bobina->ERROM?></td>
-                        <td class="table__td"><?php echo $bobina->DESHOJE?></td>
-                        <td class="table__td"><?php echo $bobina->CAMBIO_PEDIDO?></td>
-                        <td class="table__td"><?php echo $bobina->FILOS_ROTOS?></td>
-                        <td class="table__td"><?php echo $bobina->OTROS?></td>
-                        <td class="table__td"><?php echo $bobina->PEDIDOS_CORTOS?></td>
-                        <td class="table__td"><?php echo $bobina->DIFER_ANCHO?></td>
-                        <td class="table__td"><?php echo $bobina->CAMBIO_GRAMAJE?></td>
-                        <td class="table__td"><?php echo $bobina->EXTRA_TRIM?></td>
-                        <td class="table__td"><?php echo $bobina->CONSUMO?></td>
-                        <td class="table__td"><?php echo $bobina->created_at?></td>
-                     
-                        <td class="table__td--acciones"><a class="table__accion table__accion--editar" href="/admin/produccion/papel/editar?id=<?php echo $bobina->id; ?>"><i class="fa-solid fa-user-pen"></i>Ver</a>
-                        <form method="POST" action="/admin/produccion/papel/eliminar" class="table__formulario">
-                            <input type="hidden" name="id" value="<?php echo $bobina->id; ?>">
-                            <!-- <button class="table__accion table__accion--eliminar" type="submit">
-                                <i class="fa-solid fa-user-slash"></i>
-                                    Eliminar
-                            </button> -->
-                        </form>
+                        <td class="table__td"><?php echo $bobina->tipo_clasificacion ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->SINGLEFACE) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->EMPALME) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->RECUB) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->MECANICO) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->GALLET) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->HUMEDO) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->COMBADO) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->DESPE) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->ERROM) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->DESHOJE) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->CAMBIO_PEDIDO) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->FILOS_ROTOS) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->OTROS) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->PEDIDOS_CORTOS) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->DIFER_ANCHO) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->CAMBIO_GRAMAJE) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->EXTRA_TRIM) ?></td>
+                        <td class="table__td"><?php echo boldIfGreaterThanZero($bobina->CONSUMO) ?></td>
+                        <td class="table__td"><?php echo $bobina->created_at ?></td>
+                        <td class="table__td--acciones">
+                            <a class="table__accion table__accion--editar" href="/admin/produccion/papel/editar?id=<?php echo $bobina->id; ?>">
+                                <i class="fa-solid fa-user-pen"></i>Ver
+                            </a>
+                            <form method="POST" action="/admin/produccion/papel/eliminar" class="table__formulario">
+                                <input type="hidden" name="id" value="<?php echo $bobina->id; ?>">
+                            </form>
                         </td>
                     </tr>
-                <?php endforeach;?>
+                <?php endforeach; ?>
 
             </tbody>
-
         </table>
-   
-
 
     <?php else: ?>
         <a class="text-center"> No hay Papel Aún</a>
