bb05d847ff426e4ccfb39343abb4c01e1eea7f2f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-16 11:07:19 -0500

    Update tabla.php

--- views/admin/produccion/papel/tabla.php
+++ views/admin/produccion/papel/tabla.php
@@ -89,7 +89,7 @@
 
             </tbody>
            <tfoot>
-  <tr style="background-color: #f5f5f5; font-weight: bold;  text-align: center;">
+  <tr style="background-color:rgb(113, 178, 204); font-weight: bold;  text-align: center;">
     <th>Totales:</th>
     <?php 
       $columnas = [
