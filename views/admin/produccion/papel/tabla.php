2758c1fc0f9d49492597a46de0f92b8dc8ee8867
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-16 08:54:52 -0500

    Update tabla.php

--- views/admin/produccion/papel/tabla.php
+++ views/admin/produccion/papel/tabla.php
@@ -68,7 +68,7 @@
                         <td class="table__td"><?php echo letranegrita($bobina->EXTRA_TRIM) ?></td>
                         <td class="table__td"><?php echo letranegrita($bobina->CONSUMO) ?></td>
                         <td class="table__td"><?php echo letranegrita($bobina->TOTAL) ?></td>
-                        <td class="table__td"><?php echo letranegrita($bobina->full) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->PORCENTAJE) ?></td>
                         <td class="table__td"><?php echo $bobina->created_at ?></td>
                         <td class="table__td--acciones">
                             <a class="table__accion table__accion--editar" href="/admin/produccion/papel/editar?id=<?php echo $bobina->id; ?>">
