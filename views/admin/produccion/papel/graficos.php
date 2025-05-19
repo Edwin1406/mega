7816fcf714519490e22037e4b929d5d0e74b6a11
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-19 11:08:57 -0500

    Update graficos.php

--- views/admin/produccion/papel/graficos.php
+++ views/admin/produccion/papel/graficos.php
@@ -1,9 +1,14 @@
-  <!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css"> -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
   <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-<!-- <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script> -->
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
  <style>
- 
+    body {
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      background-color: #f4f6f8;
+      padding: 20px;
+    }
+
     h2 {
       text-align: center;
       color: #333;
