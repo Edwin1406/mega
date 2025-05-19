d9ba56ef969eb3c6eb24e524bc68b26b8adc3e5d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-19 10:58:23 -0500

    Update graficos.php

--- views/admin/produccion/papel/graficos.php
+++ views/admin/produccion/papel/graficos.php
@@ -1,4 +1,7 @@
-
+  <!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css"> -->
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> -->
  <style>
  
     h2 {
