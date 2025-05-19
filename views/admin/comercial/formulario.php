d7c45651e415252706c7b2f31323bdcb5c47338e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-19 16:08:03 -0500

    Update formulario.php

--- views/admin/comercial/formulario.php
+++ views/admin/comercial/formulario.php
@@ -1,3 +1,133 @@
+<fieldset class="formulario__fieldset">
+    <legend class="formulario__legend">RELCAMOS Y QUEJAS </legend>
+    <div class="formulario__campo">
+        <label class="formulario__label" for="responsable_reporte">responsable</label>
+        <input
+            type="text"
+            name="responsable_reporte"
+            id="responsable_reporte"
+            class="formulario__input"
+            placeholder="responsable_reporte"
+            value="<?php echo $comercial->responsable_reporte ?? '' ?>">
+    </div>
+
+    <!-- option de clientes  -->
+    <!-- <div class="formulario__campo">
+        <label class="formulario__label" for="cliente">cliente</label>
+        <select name="cliente" id="cliente" class="formulario__input">
+            <option value="" disabled selected>-- Seleccione --</option>
+            <?php foreach ($clientes as $cliente) : ?>
+                <option <?php echo $cliente->cliente === $cliente->id ? 'selected' : ''; ?> value="<?php echo $cliente->id; ?>"><?php echo $cliente->cliente; ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+     -->
+
+    <form method="POST">
+        <select name="cliente" onchange="this.form.submit()" class="formulario__input">
+            <option value="">-- Seleccione --</option>
+            <?php foreach ($clientes as $cliente): ?>
+                <option value="<?= $cliente->cliente ?>"
+                    <?= $cliente->cliente === $clienteSeleccionado ? 'selected' : '' ?>>
+                    <?= $cliente->cliente ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+
+
+        <div class="formulario__campo">
+            <select name="factura" class="formulario__input" onchange="this.form.submit()">
+                <option value="">-- Seleccione factura --</option>
+                <?php foreach ($facturas as $factura): ?>
+                    <option value="<?= $factura ?>"
+                        <?= $factura === $facturaSeleccionada ? 'selected' : '' ?>>
+                        <?= $factura ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+
+        </div>
+
+
+        <div class="formulario__campo">
+
+            <select name="descripcion" onchange="this.form.submit()" class="formulario__input" >
+                <option value="">-- Seleccione descripción --</option>
+                <?php foreach ($descripciones as $desc): ?>
+                    <option value="<?= htmlspecialchars($desc) ?>"
+                        <?= $desc === $descripcionSeleccionada ? 'selected' : '' ?>>
+                        <?= $desc ?>
+                <?php endforeach; ?>
+            </select>
+
+        </div>
+
+        <div class="formulario__campo">
+
+            <input
+                type="text"
+                name="fecha_factura"
+                id="fecha_factura"
+                class="formulario__input"
+                placeholder="Nombre de la fecha_factura"
+                value="<?= htmlspecialchars($fecha_factura ?? '') ?>"
+                readonly>
+        </div>
+
+
+
+
+
+        <div class="formulario__campo">
+            <label class="formulario__label" for="per_reporta_reclamo">per_reporta_reclamo</label>
+            <input
+                type="text"
+                name="per_reporta_reclamo"
+                id="per_reporta_reclamo"
+                class="formulario__input"
+                placeholder="Nombre del per_reporta_reclamo"
+                value="<?php echo $comercial->per_reporta_reclamo ?? '' ?>">
+        </div>
+
+
+
+        <div class="formulario__campo">
+            <label class="formulario__label" for="descripcion_producto">descripcion_producto</label>
+            <input
+                type="text"
+                name="descripcion_producto"
+                id="descripcion_producto"
+                class="formulario__input"
+                placeholder="Nombre de la descripcion_producto"
+                value="<?php echo $comercial->descripcion_producto ?? '' ?>">
+        </div>
+
+
+        <div class="formulario__campo">
+            <label class="formulario__label" for="motivo_reclamo">motivo_reclamo</label>
+            <input
+                type="text"
+                name="motivo_reclamo"
+                id="motivo_reclamo"
+                class="formulario__input"
+                placeholder="motivo_reclamo"
+                value="<?php echo $comercial->motivo_reclamo ?? '' ?>">
+        </div>
+
+        <div class="formulario__campo">
+            <label class="formulario__label" for="accion_solicitada">accion_solicitada</label>
+            <input
+                type="text"
+                name="accion_solicitada"
+                id="accion_solicitada"
+                class="formulario__input"
+                placeholder="accion_solicitada)"
+                value="<?php echo $comercial->accion_solicitada ?? '' ?>">
+        </div>
+
+</fieldset>
+
+
 
 
 
@@ -64,19 +194,18 @@
 
     </div>
 
+    <div class="formulario__campo">
+        <label class="formulario__label" for="fecha_factura">Fecha Factura</label>
 
-        <div class="formulario__campo">
-
-            <input
-                type="text"
-                name="fecha_factura"
-                id="fecha_factura"
-                class="formulario__input"
-                placeholder="Nombre de la fecha_factura"
-                value="<?= htmlspecialchars($fecha_factura ?? '') ?>"
-                readonly>
-        </div>
-
+        <input
+            type="text"
+            name="fecha_factura"
+            id="fecha_factura"
+            class="formulario__input"
+            placeholder="Nombre de la fecha_factura"
+            value="<?= htmlspecialchars($fecha_factura ?? '') ?>"
+            readonly>
+    </div>
 
 
 
