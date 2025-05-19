50d6e9e63ec73c87128b9e8d8afa6b38877e7f18
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-19 14:03:35 -0500

    Create Datareclamos.php

---
+++ models/Datareclamos.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Model;
+
+class Datareclamos extends ActiveRecord {
+    protected static $tabla = '';
+    protected static $columnasDB = ['id','numero','emision','cliente','codigo','descripcion','cantidad','pvp_total','costo','pvp_unid','costo_unid','margen'];
+
+
+    public $id;
+    public $numero;
+    public $emision;
+    public $cliente;
+    public $codigo;
+    public $descripcion;
+    public $cantidad;
+    public $pvp_total;
+    public $costo;
+    public $pvp_unid;
+    public $costo_unid;
+    public $margen;
+
+
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? null;
+        $this->numero = $args['numero'] ?? '';
+        $this->emision = $args['emision'] ?? '';
+        $this->cliente = $args['cliente'] ?? '';
+        $this->codigo = $args['codigo'] ?? '';
+        $this->descripcion = $args['descripcion'] ?? '';
+        $this->cantidad = $args['cantidad'] ?? '';
+        $this->pvp_total = $args['pvp_total'] ?? '';
+        $this->costo = $args['costo'] ?? '';
+        $this->pvp_unid = $args['pvp_unid'] ?? '';
+        $this->costo_unid = $args['costo_unid'] ?? '';
+        $this->margen = $args['margen'] ?? '';
+
+    }
+
+    
+
+
+
+    
+}
+
+
+
+
+
