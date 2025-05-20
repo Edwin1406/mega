12d1261af2ff3d952288f2cb396fa6621da47006
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-20 08:10:21 -0500

    Update pdfQuejas.php

--- classes/pdfQuejas.php
+++ classes/pdfQuejas.php
@@ -159,7 +159,7 @@ class pdfQuejas extends TCPDF
         $this->Cell(55, 8, "Genera Descuento:", 0, 0);
         $this->Cell(20, 8, "SI", 0, 0);
         $this->Rect($this->GetX() - 14, $this->GetY() + 2, 6, 6);
-        $this->Cell(2, 8, "%", 0, 0);
+        $this->Cell(0, 8, "%", 0, 0);
         $this->Cell(20, 8, $this->queja->descuento ?? "", 'B', 0);
         $this->Cell(25, 8, "Autorizado por:", 0, 0);
         $this->Cell(40, 8, "", 'B', 1);
