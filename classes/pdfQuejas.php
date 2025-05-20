2a8e211aac91c6e8fb951a43d11765ba8eaaa6c4
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-19 20:42:57 -0500

    Update pdfQuejas.php

--- classes/pdfQuejas.php
+++ classes/pdfQuejas.php
@@ -3,6 +3,50 @@ namespace Classes;
 use TCPDF;
 class pdfQuejas extends TCPDF 
 {
+   
+
+    
+    public function generarPdf($materias)
+    {
+       
+    
+        
+    }
+
+    
+
+
+    
+
+    public function verPdf($materias)
+    {
+        $this->generarPdf($materias);
+        $this->Output('reporte.pdf', 'I');
+    }
+
+    public function guardarPdf($materias)
+    {
+        $this->generarPdf($materias);
+        $this->Output('reporte.pdf', 'F');
+    }
+
+    public function enviarPdf($materias)
+    {
+        $this->generarPdf($materias);
+        $this->Output('reporte.pdf', 'E');
+    }
+
+    public function imprimirPdf($materias)
+    {
+        $this->generarPdf($materias);
+        $this->Output('reporte.pdf', 'I');
+    }
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
 
 }
 
