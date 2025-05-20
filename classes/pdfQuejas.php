f4e67e08f5ec0a36a31a23714b8bd8425ce7f1a7
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-20 08:56:37 -0500

    Update pdfQuejas.php

--- classes/pdfQuejas.php
+++ classes/pdfQuejas.php
@@ -225,19 +225,19 @@ $this->Cell(30, 8, $this->queja->ancho ?? "", 1, 1);
 $this->Ln(-32); // Regresa arriba
 $this->SetX(65); // Mueve a la segunda columna
 $this->Cell(20, 8, "ECT", 0, 0);
-$this->Cell(20, 8, $this->queja->ect ?? "", 1, 1);
+$this->Cell(20, 8, $this->queja->ect ?? "", 1, 1); //ancho del caudro
 $this->SetX(65);
 $this->Cell(20, 8, "FCT", 0, 0);
-$this->Cell(30, 8, $this->queja->fct ?? "", 1, 1);
+$this->Cell(20, 8, $this->queja->fct ?? "", 1, 1);
 $this->SetX(65);
 $this->Cell(20, 8, "PAT", 0, 0);
-$this->Cell(30, 8, $this->queja->pat1 ?? "", 1, 1);
+$this->Cell(20, 8, $this->queja->pat1 ?? "", 1, 1);
 $this->SetX(65);
 $this->Cell(20, 8, "PAT", 0, 0);
-$this->Cell(30, 8, $this->queja->pat2 ?? "", 1, 1);
+$this->Cell(20, 8, $this->queja->pat2 ?? "", 1, 1);
 $this->SetX(65);
 $this->Cell(20, 8, "PESO", 0, 0);
-$this->Cell(30, 8, $this->queja->peso ?? "", 1, 1);
+$this->Cell(20, 8, $this->queja->peso ?? "", 1, 1);
 
 // Sub-etiquetas: columna derecha
 $this->Ln(-40); // Regresa arriba
