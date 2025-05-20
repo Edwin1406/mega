a671d2a2dde734ebd6ffa58306726df9ba2a96b6
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-20 09:42:53 -0500

    Update pdfQuejas.php

--- classes/pdfQuejas.php
+++ classes/pdfQuejas.php
@@ -257,15 +257,26 @@ $this->Cell(20, 8, $this->queja->tinta4 ?? "", 1, 1);
 
 // Tercera fila
 $this->Ln(-32); // Regresa arriba
+
+// Fila 1
 $this->SetX(155);
 $this->Cell(12, 8, "GCMI", 0, 0);
-$this->Cell(20, 8, $this->queja->tinta5 ?? "", 1, 0);
+$this->Cell(20, 8, $this->queja->tinta5 ?? "", 1, 1);
+
+// Fila 2
+$this->SetX(155);
 $this->Cell(12, 8, "GCMI", 0, 0);
 $this->Cell(20, 8, $this->queja->tinta6 ?? "", 1, 1);
+
+// Fila 3
+$this->SetX(155);
 $this->Cell(12, 8, "GCMI", 0, 0);
-$this->Cell(20, 8, $this->queja->tinta7 ?? "", 1, 0);
+$this->Cell(20, 8, $this->queja->tinta7 ?? "", 1, 1);
 
-
+// Fila 4 - nueva fila que agregas
+$this->SetX(155);
+$this->Cell(12, 8, "GCMI", 0, 0);
+$this->Cell(20, 8, $this->queja->tinta8 ?? "", 1, 1);
 
 
         $this->Ln(50);
