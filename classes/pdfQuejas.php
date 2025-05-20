c7d67b44e8de90f5bbf847d1e234164a01404b45
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-20 09:05:23 -0500

    Update pdfQuejas.php

--- classes/pdfQuejas.php
+++ classes/pdfQuejas.php
@@ -241,7 +241,7 @@ $this->Cell(20, 8, $this->queja->peso ?? "", 1, 1);
 
 // Sub-etiquetas: columna derecha
 $this->Ln(-40); // Regresa arriba
-$this->SetX(100); // Mueve a la tercera columna
+$this->SetX(90); // Mueve a la tercera columna
 $this->Cell(20, 8, "GCMI", 0, 0);
 $this->Cell(30, 8, $this->queja->tinta1 ?? "", 1, 1);
 $this->SetX(130);
