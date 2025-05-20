48a8818073272e84308de136d89563741c324357
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-19 20:36:26 -0500

    Update pdfQuejas.php

--- classes/pdfQuejas.php
+++ classes/pdfQuejas.php
@@ -1,13 +1,9 @@
 <?php
 namespace Classes;
-
 use TCPDF;
 class pdfQuejas extends TCPDF 
 {
-
-
-
-public function Header()
+    public function Header()
     {
         $this->SetFont('helvetica', 'B', 12); // Cambia Arial por helvetica
         $this->Cell(0, 10, 'Formato de Quejas y Reclamos', 0, 0, 'C');
@@ -60,40 +56,6 @@ public function Header()
         
     }
 
-    
-
-
-    
-
-    public function descargarPdf($materias)
-    {
-        $this->generarPdf($materias);
-        $this->Output('reporte.pdf', 'D');
-    }
-
-    public function verPdf($materias)
-    {
-        $this->generarPdf($materias);
-        $this->Output('reporte.pdf', 'I');
-    }
-
-    public function guardarPdf($materias)
-    {
-        $this->generarPdf($materias);
-        $this->Output('reporte.pdf', 'F');
-    }
-
-    public function enviarPdf($materias)
-    {
-        $this->generarPdf($materias);
-        $this->Output('reporte.pdf', 'E');
-    }
-
-    public function imprimirPdf($materias)
-    {
-        $this->generarPdf($materias);
-        $this->Output('reporte.pdf', 'I');
-    }
 
     public function __construct()
     {
