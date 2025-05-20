1b33f3d11974e5e869b89167f39ddebad0ffbe8c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-20 12:00:18 -0500

    Update pdfQuejas.php

--- classes/pdfQuejas.php
+++ classes/pdfQuejas.php
@@ -334,9 +334,9 @@ $this->SetXY($this->GetX() + 40, $this->GetY()); // Mueves el cursor a la derech
         $this->Ln(12);
 
         // --- Pie de página ---
-        $this->Cell(80, 8, "Recibe el reclamo:", 0, 0);
+        $this->Cell(40, 8, "Recibe el reclamo:", 0, 0);
         $this->Cell(30, 8, $this->queja->fecha_accion ?? "", 'B', 0);
-        $this->Cell(80, 8, "Fecha y hora:", 0, 1);
+        $this->Cell(40, 8, "Fecha y hora:", 0, 1);
         $this->Cell(30, 8, $this->queja->fecha_accion ?? "", 'B', 0);
     }
 }
