5fd047fe3b396c7a8a94d4193c66917f1a954465
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-20 22:08:09 -0500

    Update formulario.php

--- views/admin/comercial/formulario.php
+++ views/admin/comercial/formulario.php
@@ -1,9 +1,13 @@
 
-<!-- CSS de Select2 -->
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- Select2 CSS -->
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
-<!-- JS de Select2 -->
+<!-- Select2 JS -->
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 
 <fieldset class="formulario__fieldset">
     <legend class="formulario__legend">RELCAMOS Y QUEJAS </legend>
