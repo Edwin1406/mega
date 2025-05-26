dc0f01ca22950981ab90741260d474228582bb8e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-26 11:40:21 -0500

    Update tabla_guillotina_lamina.php

---
+++ views/admin/produccion/papel/tabla_guillotina_lamina.php
@@ -0,0 +1,104 @@
+<h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
+
+<div class="dashboard__contenedor-boton">
+    <a class="dashboard__boton" href="/admin/produccion/papel/crear">
+        <i class="fa-solid fa-circle-arrow-left"></i>
+        REGRESAR A INICIO
+    </a>
+</div>
+
+<div class="dashboard__contenedor">
+    <?php if (!empty($bobinas)): ?>
+
+        <?php 
+            function letranegrita($value) {
+                return $value > 0 ? '<strong>' . $value . '</strong>' : $value;
+            }
+        ?>
+
+        <table class="table">
+            <thead class="table__thead">
+                <tr>
+                    <th rowspan="2" class="table__th">Tipo de clasificación</th>
+                    <th colspan="5" class="table__th" style="background-color: #a564a8; color: white; text-align: center;">CONTROLABLE</th>
+                    <th colspan="10" class="table__th" style="background-color: #4a90e2; color: white; text-align: center;">NO CONTROLABLE</th>
+                    <th rowspan="2" class="table__th">Consumo</th>
+                    <th rowspan="2" class="table__th">Total</th>
+                    <th rowspan="2" class="table__th">Porcentaje</th>
+                    <th rowspan="2" class="table__th">Fecha</th>
+                    <th rowspan="2" class="table__th">Acciones</th>
+                </tr>
+                <tr>
+                    <th class="table__th">REFILES</th>
+                 
+                  
+                    
+
+                </tr>
+            </thead>
+
+            <tbody class="table__tbody">
+
+                <?php foreach ($bobinas as $bobina): ?>
+                    <tr class="table__tr">
+                        <td class="table__td"><?php echo $bobina->tipo_clasificacion ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->CUADRE) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->FALTA_TINTA) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->MALTRATO_TRASPORT) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->MALTRATO_MONTACARGAS) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->TONALIDAD_TINTAS) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->TROQUEL) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->MONTAJE_CLICHE) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->MECANICO) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->ELECTRICO) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->GALLET) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->COMBADO) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->HUMEDO) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->DESPE) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->ERROM) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->SUSTRATO) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->CONSUMO) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->TOTAL) ?></td>
+                        <td class="table__td"><?php echo letranegrita($bobina->PORCENTAJE) ?></td>
+                        <td class="table__td"><?php echo $bobina->created_at ?></td>
+                        <td class="table__td--acciones">
+                            <a class="table__accion table__accion--editar" href="/admin/produccion/papel/editar?id=<?php echo $bobina->id; ?>">
+                                <i class="fa-solid fa-user-pen"></i>Ver
+                            </a>
+                            <form method="POST" action="/admin/produccion/papel/eliminar" class="table__formulario">
+                                <input type="hidden" name="id" value="<?php echo $bobina->id; ?>">
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+
+            </tbody>
+
+            <tfoot>
+                <tr style="background-color:rgb(113, 178, 204); font-weight: bold;">
+                    <th>Totales:</th>
+                    <?php 
+                      $columnas = [
+                           'REFILES', 
+
+                            'CONSUMO', 'TOTAL', 'PORCENTAJE'
+                      ];
+
+                      foreach ($columnas as $col) {
+                          $valor = isset($totales[$col]) && $totales[$col] !== null ? $totales[$col] : 0;
+                          echo '<td>' . letranegrita(number_format($valor, 2)) . '</td>';
+                      }
+                    ?>
+                    <td></td> <!-- Acciones -->
+                </tr>
+            </tfoot>
+
+        </table>
+
+    <?php else: ?>
+        <a class="text-center"> No hay Papel Aún</a>
+    <?php endif; ?>
+</div>
+
+<?php echo $paginacion; ?>
+
