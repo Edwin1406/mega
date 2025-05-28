6559db6c8c910e0925e61b248d5d2f1790c09315
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-28 16:06:12 -0500

    Update tabla_troquel.php

--- views/admin/produccion/papel/tablas/tabla_troquel.php
+++ views/admin/produccion/papel/tablas/tabla_troquel.php
@@ -51,13 +51,42 @@
                         <td class="table__td"><?php echo letranegrita($bobina->TOTAL) ?></td>
                         <td class="table__td"><?php echo letranegrita($bobina->PORCENTAJE) ?></td>
                         <td class="table__td"><?php echo $bobina->created_at ?></td>
-                        <td class="table__td--acciones">
+                        <!-- <td class="table__td--acciones">
                             <a class="table__accion table__accion--editar" href="/admin/produccion/papel/editar?id=<?php echo $bobina->id; ?>">
                                 <i class="fa-solid fa-user-pen"></i>Ver
                             </a>
                             <form method="POST" action="/admin/produccion/papel/eliminar" class="table__formulario">
                                 <input type="hidden" name="id" value="<?php echo $bobina->id; ?>">
                             </form>
+                        </td> -->
+                        
+                        <td class="table__td--acciones">
+                            <select class="opciones table__accion table__accion--editar" onchange="enviarIdOrden(this)"
+                                data-id-orden="<?php echo trim($bobina->id_orden); ?>"
+                                data-id-bobina="<?php echo $bobina->id; ?>">
+                                <option value="">JALAR ID ORDEN</option>
+                                <option value="TROQUEL">CORRUGADOR</option>
+                                <!-- <option value="troquel">Troquel</option> -->
+                                <option value="EDITAR">EDITAR</option>
+                            </select>
+
+                            <script>
+                                function enviarIdOrden(selectElement) {
+                                    const opcion = selectElement.value;
+                                    const idOrdenRaw  = selectElement.getAttribute('data-id-orden');
+                                    const idOrden = idOrdenRaw ? idOrdenRaw.trim() : '';
+                                    const idBobina = selectElement.getAttribute('data-id-bobina');
+
+                                    if (opcion === 'TROQUEL') {
+                                        // window.location.href = `/admin/produccion/papel/crear?id_orden=${idOrden}&id_bobina=${idBobina}`; 
+                                        window.location.href = `/admin/produccion/papel/crear?id_orden=${idOrden}&tipo=${opcion}`;
+
+                                    }  else if (opcion === 'EDITAR') {
+                                        // window.location.href = `/admin/produccion/papel/editar?id=${idBobina}`;
+                                    }
+                                    
+                                }
+                            </script>
                         </td>
                     </tr>
                 <?php endforeach; ?>
