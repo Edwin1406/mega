08fd6de2f6e38f8a934b1360053f6a355e5ab2a5
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-28 16:14:04 -0500

    Update IngresoConsumo.php

--- models/IngresoConsumo.php
+++ models/IngresoConsumo.php
@@ -4,16 +4,20 @@ namespace Model;
 
 class IngresoConsumo extends ActiveRecord {
     protected static $tabla = 'ingresos_consumo';
-    protected static $columnasDB = ['id', 'id_orden', 'consumo'];
+    protected static $columnasDB = ['id', 'id_orden', 'consumo','total', 'porcentaje'];
 
     public $id;
     public $id_orden;
     public $consumo;
+    public $total;
+    public $porcentaje;
 
     public function __construct($args = []) {
         $this->id = $args['id'] ?? null;
         $this->id_orden = $args['id_orden'] ?? null;
         $this->consumo = $args['consumo'] ?? null;
+        $this->total = $args['total'] ?? null;
+        $this->porcentaje = $args['porcentaje'] ?? null;
     }
 }
 
