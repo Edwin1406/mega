1e8025d2cbb597c122a9160dbda81f833307ea01
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-28 17:02:40 -0500

    Update tabla_corte_ceja.php

--- views/admin/produccion/papel/tablas/tabla_corte_ceja.php
+++ views/admin/produccion/papel/tablas/tabla_corte_ceja.php
@@ -10,10 +10,11 @@
 <div class="dashboard__contenedor">
     <?php if (!empty($bobinas)): ?>
 
-        <?php 
-            function letranegrita($value) {
-                return $value > 0 ? '<strong>' . $value . '</strong>' : $value;
-            }
+        <?php
+        function letranegrita($value)
+        {
+            return $value > 0 ? '<strong>' . $value . '</strong>' : $value;
+        }
         ?>
 
         <table class="table">
@@ -29,7 +30,7 @@
                 </tr>
                 <tr>
                     <th class="table__th">Cuadre SIERRA</th>
-                
+
                 </tr>
             </thead>
 
@@ -43,13 +44,41 @@
                         <td class="table__td"><?php echo letranegrita($bobina->TOTAL) ?></td>
                         <td class="table__td"><?php echo letranegrita($bobina->PORCENTAJE) ?></td>
                         <td class="table__td"><?php echo $bobina->created_at ?></td>
-                        <td class="table__td--acciones">
+                        <!-- <td class="table__td--acciones">
                             <a class="table__accion table__accion--editar" href="/admin/produccion/papel/editar?id=<?php echo $bobina->id; ?>">
                                 <i class="fa-solid fa-user-pen"></i>Ver
                             </a>
                             <form method="POST" action="/admin/produccion/papel/eliminar" class="table__formulario">
                                 <input type="hidden" name="id" value="<?php echo $bobina->id; ?>">
                             </form>
+                        </td> -->
+                        <td class="table__td--acciones">
+                            <select class="opciones table__accion table__accion--editar" onchange="enviarIdOrden(this)"
+                                data-id-orden="<?php echo trim($bobina->id_orden); ?>"
+                                data-id-bobina="<?php echo $bobina->id; ?>">
+                                <option value="">JALAR ID ORDEN</option>
+                                <option value="CORTE CEJA">CORTE CEJA</option>
+                                <!-- <option value="troquel">Troquel</option> -->
+                                <option value="EDITAR">EDITAR</option>
+                            </select>
+
+                            <script>
+                                function enviarIdOrden(selectElement) {
+                                    const opcion = selectElement.value;
+                                    const idOrdenRaw = selectElement.getAttribute('data-id-orden');
+                                    const idOrden = idOrdenRaw ? idOrdenRaw.trim() : '';
+                                    const idBobina = selectElement.getAttribute('data-id-bobina');
+
+                                    if (opcion === 'CORTE CEJA') {
+                                        // window.location.href = `/admin/produccion/papel/crear?id_orden=${idOrden}&id_bobina=${idBobina}`; 
+                                        window.location.href = `/admin/produccion/papel/crear?id_orden=${idOrden}&tipo=${opcion}`;
+
+                                    } else if (opcion === 'EDITAR') {
+                                        // window.location.href = `/admin/produccion/papel/editar?id=${idBobina}`;
+                                    }
+
+                                }
+                            </script>
                         </td>
                     </tr>
                 <?php endforeach; ?>
@@ -59,16 +88,18 @@
             <tfoot>
                 <tr style="background-color:rgb(113, 178, 204); font-weight: bold;">
                     <th>Totales:</th>
-                    <?php 
-                      $columnas = [
-                          'CUADRE_SIERRA',
-                          'CONSUMO', 'TOTAL', 'PORCENTAJE'
-                      ];
+                    <?php
+                    $columnas = [
+                        'CUADRE_SIERRA',
+                        'CONSUMO',
+                        'TOTAL',
+                        'PORCENTAJE'
+                    ];
 
-                      foreach ($columnas as $col) {
-                          $valor = isset($totales[$col]) && $totales[$col] !== null ? $totales[$col] : 0;
-                          echo '<td>' . letranegrita(number_format($valor, 2)) . '</td>';
-                      }
+                    foreach ($columnas as $col) {
+                        $valor = isset($totales[$col]) && $totales[$col] !== null ? $totales[$col] : 0;
+                        echo '<td>' . letranegrita(number_format($valor, 2)) . '</td>';
+                    }
                     ?>
                     <td></td> <!-- Acciones -->
                 </tr>
@@ -82,4 +113,3 @@
 </div>
 
 <?php echo $paginacion; ?>
-
