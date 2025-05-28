4641d564098b9ded0b69d574236ff653eb2fa5f8
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-28 16:32:29 -0500

    Update tabla_preprinter.php

--- views/admin/produccion/papel/tablas/tabla_preprinter.php
+++ views/admin/produccion/papel/tablas/tabla_preprinter.php
@@ -10,15 +10,17 @@
 <div class="dashboard__contenedor">
     <?php if (!empty($bobinas)): ?>
 
-        <?php 
-            function letranegrita($value) {
-                return $value > 0 ? '<strong>' . $value . '</strong>' : $value;
-            }
+        <?php
+        function letranegrita($value)
+        {
+            return $value > 0 ? '<strong>' . $value . '</strong>' : $value;
+        }
         ?>
 
         <table class="table">
             <thead class="table__thead">
                 <tr>
+                    <th rowspan="2" class="table__th">Id_orden</th>
                     <th rowspan="2" class="table__th">Tipo de clasificación</th>
                     <th colspan="7" class="table__th" style="background-color: #a564a8; color: white; text-align: center;">CONTROLABLE</th>
                     <th colspan="5" class="table__th" style="background-color: #4a90e2; color: white; text-align: center;">NO CONTROLABLE</th>
@@ -42,8 +44,8 @@
                     <th class="table__th">MECANICO</th>
                     <th class="table__th">SUSTRATO</th>
 
-                  
-                    
+
+
 
                 </tr>
             </thead>
@@ -52,6 +54,7 @@
 
                 <?php foreach ($bobinas as $bobina): ?>
                     <tr class="table__tr">
+                        <td class="table__td"><?php echo $bobina->id_orden ?></td>
                         <td class="table__td"><?php echo $bobina->tipo_clasificacion ?></td>
                         <td class="table__td"><?php echo letranegrita($bobina->FALTA_TINTA) ?></td>
                         <td class="table__td"><?php echo letranegrita($bobina->DERRAME_TINTA) ?></td>
@@ -69,13 +72,41 @@
                         <td class="table__td"><?php echo letranegrita($bobina->TOTAL) ?></td>
                         <td class="table__td"><?php echo letranegrita($bobina->PORCENTAJE) ?></td>
                         <td class="table__td"><?php echo $bobina->created_at ?></td>
-                        <td class="table__td--acciones">
+                        <!-- <td class="table__td--acciones">
                             <a class="table__accion table__accion--editar" href="/admin/produccion/papel/editar?id=<?php echo $bobina->id; ?>">
                                 <i class="fa-solid fa-user-pen"></i>Ver
                             </a>
                             <form method="POST" action="/admin/produccion/papel/eliminar" class="table__formulario">
                                 <input type="hidden" name="id" value="<?php echo $bobina->id; ?>">
                             </form>
+                        </td> -->
+                        <td class="table__td--acciones">
+                            <select class="opciones table__accion table__accion--editar" onchange="enviarIdOrden(this)"
+                                data-id-orden="<?php echo trim($bobina->id_orden); ?>"
+                                data-id-bobina="<?php echo $bobina->id; ?>">
+                                <option value="">JALAR ID ORDEN</option>
+                                <option value="TROQUEL">TROQUEL</option>
+                                <!-- <option value="troquel">Troquel</option> -->
+                                <option value="EDITAR">EDITAR</option>
+                            </select>
+
+                            <script>
+                                function enviarIdOrden(selectElement) {
+                                    const opcion = selectElement.value;
+                                    const idOrdenRaw = selectElement.getAttribute('data-id-orden');
+                                    const idOrden = idOrdenRaw ? idOrdenRaw.trim() : '';
+                                    const idBobina = selectElement.getAttribute('data-id-bobina');
+
+                                    if (opcion === 'TROQUEL') {
+                                        // window.location.href = `/admin/produccion/papel/crear?id_orden=${idOrden}&id_bobina=${idBobina}`; 
+                                        window.location.href = `/admin/produccion/papel/crear?id_orden=${idOrden}&tipo=${opcion}`;
+
+                                    } else if (opcion === 'EDITAR') {
+                                        // window.location.href = `/admin/produccion/papel/editar?id=${idBobina}`;
+                                    }
+
+                                }
+                            </script>
                         </td>
                     </tr>
                 <?php endforeach; ?>
@@ -85,18 +116,29 @@
             <tfoot>
                 <tr style="background-color:rgb(113, 178, 204); font-weight: bold;">
                     <th>Totales:</th>
-                    <?php 
-                      $columnas = [
-                          'FALTA_TINTA', 'DERRAME_TINTA', 'VISCOSIDAD', 'PH',
-                            'CUADRE', 'EMPALME', 'APROBACION_COLOR', 'FILOS_ROTOS',
-                            'CIREL_CORTADO', 'ELECTRICO', 'MECANICO', 'SUSTRATO',
-                            'CONSUMO', 'TOTAL', 'PORCENTAJE'
-                      ];
+                    <?php
+                    $columnas = [
+                        'FALTA_TINTA',
+                        'DERRAME_TINTA',
+                        'VISCOSIDAD',
+                        'PH',
+                        'CUADRE',
+                        'EMPALME',
+                        'APROBACION_COLOR',
+                        'FILOS_ROTOS',
+                        'CIREL_CORTADO',
+                        'ELECTRICO',
+                        'MECANICO',
+                        'SUSTRATO',
+                        'CONSUMO',
+                        'TOTAL',
+                        'PORCENTAJE'
+                    ];
 
-                      foreach ($columnas as $col) {
-                          $valor = isset($totales[$col]) && $totales[$col] !== null ? $totales[$col] : 0;
-                          echo '<td>' . letranegrita(number_format($valor, 2)) . '</td>';
-                      }
+                    foreach ($columnas as $col) {
+                        $valor = isset($totales[$col]) && $totales[$col] !== null ? $totales[$col] : 0;
+                        echo '<td>' . letranegrita(number_format($valor, 2)) . '</td>';
+                    }
                     ?>
                     <td></td> <!-- Acciones -->
                 </tr>
@@ -110,4 +152,3 @@
 </div>
 
 <?php echo $paginacion; ?>
-
