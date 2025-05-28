b5cf955e83456fe40ad0f3a90190f5d9b1e9b08e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-28 17:12:14 -0500

    Update IngresoConsumo.php

--- models/IngresoConsumo.php
+++ models/IngresoConsumo.php
@@ -4,20 +4,24 @@ namespace Model;
 
 class IngresoConsumo extends ActiveRecord {
     protected static $tabla = 'ingresos_consumo';
-    protected static $columnasDB = ['id', 'id_orden', 'consumo','total', 'porcentaje'];
+    protected static $columnasDB = ['id', 'id_orden', 'consumo','total', 'porcentaje', 'created_at'];
 
     public $id;
     public $id_orden;
     public $consumo;
     public $total;
     public $porcentaje;
+    public $created_at;
+
 
     public function __construct($args = []) {
+        date_default_timezone_set('America/Guayaquil');
         $this->id = $args['id'] ?? null;
         $this->id_orden = $args['id_orden'] ?? null;
         $this->consumo = $args['consumo'] ?? null;
         $this->total = $args['total'] ?? null;
         $this->porcentaje = $args['porcentaje'] ?? null;
+        $this->created_at = date('Y-m-d'); // Establecer la fecha y hora actual
     }
 }
 
