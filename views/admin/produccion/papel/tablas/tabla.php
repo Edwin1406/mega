3d8a80444c6507b41452c6bd6a9b5c7d608f9e08
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-28 12:28:38 -0500

    Update tabla.php

--- views/admin/produccion/papel/tablas/tabla.php
+++ views/admin/produccion/papel/tablas/tabla.php
@@ -122,7 +122,7 @@
             </tbody>
 
             <tfoot>
-                <tr style="background-color:rgb(113, 178, 204); font-weight: bold; padding-left: 10px;">
+                <tr style="background-color:rgb(113, 178, 204); font-weight: bold; padding-left: 20px;">
                             <td></td> <!-- Id_orden -->
 
                     <th>Totales:</th>
