0e7ba13f57d0659c93acfadf1771b4d5f4ce9e14
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-28 12:20:29 -0500

    Update tabla.php

--- views/admin/produccion/papel/tablas/tabla.php
+++ views/admin/produccion/papel/tablas/tabla.php
@@ -21,7 +21,7 @@
             <thead class="table__thead">
                 <tr>
                     <th rowspan="1" class="table__th">Id_orden</th>
-                    <th rowspan="2" class="table__th">Tipo de clasificación</th>
+                    <th rowspan="3" class="table__th">Tipo de clasificación</th>
                     <th colspan="9" class="table__th" style="background-color: #a564a8; color: white; text-align: center;">CONTROLABLE</th>
                     <th colspan="10" class="table__th" style="background-color: #4a90e2; color: white; text-align: center;">NO CONTROLABLE</th>
                     <th rowspan="2" class="table__th">Consumo</th>
