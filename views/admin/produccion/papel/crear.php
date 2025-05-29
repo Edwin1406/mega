bca85338928e835b9b6d022608f59fa00ad891b8
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-29 08:42:09 -0500

    Update crear.php

--- views/admin/produccion/papel/crear.php
+++ views/admin/produccion/papel/crear.php
@@ -1,11 +1,78 @@
 <h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
-<div class="dashboard__contenedor-boton">
-    <a class="dashboard__boton" href="/admin/produccion/papel/tabla">
-    <i class="fa-regular fa-eye"></i>
-        VER PAPEL
-    </a>
+<style>
+    .item {
+      background-color: #24292d;
+      color: #f8f2f2;
+      padding: 10px 15px;
+      transition: all 0.5s;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    
+    .container {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+    }
+    
+    .item:nth-child(1), .item:nth-child(2), .item:nth-child(3), .item:nth-child(4), .item:nth-child(5) {
+      width: 10%;
+    }
 
-</div>
+    .item:hover {
+      background-color: #d5f5e3;
+      scale: 1.1;
+      color: #24292d;
+      border-radius: 1rem;
+    }
+
+    .item a {
+      color: inherit;
+      text-decoration: none;
+      display: block;
+      text-align: center;
+      font-size: 1.5rem;
+    }
+    @media (min-width: 1024px) {
+    .item:nth-child(1) {
+      width: 20%;
+    }
+    
+    .item:nth-child(2) {
+      width: 20%;
+    }
+    
+    .item:nth-child(3) {
+      width: 20%;
+    }
+    
+    .item:nth-child(4) {
+      width: 20%;
+    }
+    
+    .item:nth-child(5) {
+      width: 20%;
+    }
+
+
+
+
+
+  }
+
+</style>
+
+
+    <div class="container">
+        <div class="item"><a href="/admin/produccion/estimar/index?id=20255"> <i class="fas fa-home"></i> INICIO</a></div>
+        <div class="item"><a href="/admin/produccion/estimar/micro"> <i class="fas fa-industry"></i> COTIZADOR MICRO</a></div>
+        <div class="item"><a href="/admin/produccion/estimar/cajas"> <i class="fas fa-scroll"></i> COTIZADOR CAJAS</a></div>
+        <div class="item"><a href="/admin/produccion/estimar/separadores">  <i class="fas fa-newspaper"></i> COTIZADOR SEPARADORES</a></div>
+        <div class="item"><a href="/admin/produccion/estimar/costos_generales/index">  <i class="fa-solid fa-calculator"></i> COSTOS GENERALES</a></div>
+        <div class="item"><a href="/admin/produccion/estimar/costos_papel">  <i class="fa-solid fa-money-bill-1"></i> COSTOS PAPEL</a></div>
+    </div>
+
 
 
 
@@ -28,4 +95,3 @@
 
 </div>
 
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
