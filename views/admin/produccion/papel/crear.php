a2e38d443384753fab2b36c4d375ad3d25b6ce85
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-05-29 08:51:52 -0500

    Update crear.php

--- views/admin/produccion/papel/crear.php
+++ views/admin/produccion/papel/crear.php
@@ -17,7 +17,7 @@
       justify-content: center;
     }
     
-    .item:nth-child(1), .item:nth-child(2), .item:nth-child(3), .item:nth-child(4), .item:nth-child(5) {
+    .item:nth-child(1), .item:nth-child(2), .item:nth-child(3), .item:nth-child(4), .item:nth-child(5),.item:nth-child(6),.item:nth-child(7),.item:nth-child(8),.item:nth-child(9),.item:nth-child(10) {
         width: 10%;
     }
     
@@ -67,19 +67,20 @@
 
 
 <div class="container">
-    <div class="item"><a href="/admin/produccion/estimar/index?id=20255"> <i class="fas fa-home"></i></a></div>
-    <div class="item"><a href="/admin/produccion/estimar/micro"> <i class="fas fa-industry"></i> COTIZADOR MICRO</a></div>
-    <div class="item"><a href="/admin/produccion/estimar/cajas"> <i class="fas fa-scroll"></i> COTIZADOR CAJAS</a></div>
-    <div class="item"><a href="/admin/produccion/estimar/separadores">  <i class="fas fa-newspaper"></i> COTIZADOR SEPARADORES</a></div>
-    <div class="item"><a href="/admin/produccion/estimar/costos_generales/index">  <i class="fa-solid fa-calculator"></i> COSTOS GENERALES</a></div>
-    <div class="item"><a href="/admin/produccion/estimar/costos_papel">  <i class="fa-solid fa-money-bill-1"></i> COSTOS PAPEL</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/index?id=20255"> <i class="fas fa-home"></i>CORRUGADOR</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/micro"> <i class="fas fa-industry"></i>MICRO</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/cajas"> <i class="fas fa-scroll"></i> FLEXOGRAFICA</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/separadores">  <i class="fas fa-newspaper"></i>PREPRINTER</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/costos_generales/index">  <i class="fa-solid fa-calculator"></i>BODLADO</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/costos_papel">  <i class="fa-solid fa-money-bill-1"></i> CORTE CEJA</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/costos_papel">  <i class="fa-solid fa-money-bill-1"></i> TROQUEL</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/costos_papel">  <i class="fa-solid fa-money-bill-1"></i> GUILLOTINA LAMINA</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/costos_papel">  <i class="fa-solid fa-money-bill-1"></i> GUILLOTINA PAPEL</a></div>
+    <div class="item"><a href="/admin/produccion/estimar/costos_papel">  <i class="fa-solid fa-money-bill-1"></i> EMPAQUE</a></div>
 </div>
 
 
-
 <h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
-
-
 
 <div class="dashboard__formulario">
 
