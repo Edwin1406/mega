a6db9a6f4f1bdf871892e098d1b03e0327b2ae99
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-06-18 09:25:01 -0500

    Update Pdf2.php

--- classes/Pdf2.php
+++ classes/Pdf2.php
@@ -12,7 +12,7 @@ class Pdf2 extends TCPDF
     {
         $this->Ln(20);
         $this->SetFont('helvetica', 'B', 16);
-        $this->Cell(0, 10, 'SOLICITUD DE COMPRA', 0, 1, 'C');
+        $this->Cell(0, 10, 'SOLICITUD DE APROBACIÓN DE COMPRA ', 0, 1, 'C');
         $this->Ln(10);
     }
     public function generarPdf($datos)
