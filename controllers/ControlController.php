f1169feb25c721ae2f3382da1c46665024422020
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-03 09:53:19 -0500

    Update ControlController.php

--- controllers/ControlController.php
+++ controllers/ControlController.php
@@ -8,6 +8,9 @@ class ControlController {
 
     public static function crear(Router $router)
     {
+
+        session_start();
+        isAuth();
         $router->render('admin/control/crear' , [
             'titulo' => 'CONTROL DE PRODUCCION',
         ]);
