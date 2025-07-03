650b3c71ce6642dc812dc41a5cb521a28ec7038f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-03 10:19:41 -0500

    .

--- views/admin/control/formulario.php
+++ views/admin/control/formulario.php
@@ -47,28 +47,102 @@
             <option value="PAUSADO" <?php echo (isset($cliente->estado) && $cliente->estado == 'PAUSADO') ? 'selected' : ''; ?>>PAUSADO</option>
             <option value="TERMINADO" <?php echo (isset($cliente->estado) && $cliente->estado == 'TERMINADO') ? 'selected' : ''; ?>>TERMINADO</option>
         </select>
-        
-    </div>
-    <div class="formulario__campo">
-    <label class="formulario__label" for="pdf">Subir PDF</label>
-    <input
-        type="file"
-        name="pdf"
-        id="pdf"
-        class="formulario__input"
-        placeholder="Subir PDF del cliente">
-</div>
 
-<?php if(isset($cliente->pdf)) :?>
-    <div class="formulario__campo">
-        <a class="formulario__texto">Archivo Actual:</a>
-        <div class="formulario__archivo">
-            <a href="<?php echo $_ENV['HOST'] . '/src/visor/' . $cliente->pdf; ?>" target="_blank" class="formulario__enlace">
-                Descargar/Ver PDF
-            </a>
-        </div>
     </div>
-<?php endif;?>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="horas_programadas">Horas Programadas</label>
+        <input
+            type="time"
+            name="horas_programadas"
+            id="horas_programadas"
+            class="formulario__input"
+            value="<?php echo $cliente->horas_programadas ?? '' ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="golpes_maquina">Golpes Máquina</label>
+        <input
+            type="number"
+            name="golpes_maquina"
+            id="golpes_maquina"
+            class="formulario__input"
+            placeholder="Golpes Máquina"
+            value="<?php echo $cliente->golpes_maquina ?? '' ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="golpes_maquina_hora">Golpes Máquina/Hora</label>
+        <input
+            type="number"
+            name="golpes_maquina_hora"
+            id="golpes_maquina_hora"
+            class="formulario__input"
+            placeholder="Golpes Máquina/Hora"
+            value="<?php echo $cliente->golpes_maquina_hora ?? '' ?>">
+
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="cambios_medida">Nº Cambios de Medida</label>
+        <input
+            type="number"
+            name="cambios_medida"
+            id="cambios_medida"
+            class="formulario__input"
+            placeholder="Nº Cambios de Medida"
+            value="<?php echo $cliente->cambios_medida ?? '' ?>">
+
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="cantidad_separadores">Separadores</label>
+        <input
+            type="number"
+            name="cantidad_separadores"
+            id="cantidad_separadores"
+            class="formulario__input"
+            placeholder="Cantidad de Separadores"
+            value="<?php echo $cliente->cantidad_separadores ?? '' ?>">
+
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="cantidad_cajas">Cajas</label>
+        <input
+            type="number"
+            name="cantidad_cajas"
+            id="cantidad_cajas"
+            class="formulario__input"
+            placeholder="Cantidad de Cajas"
+            value="<?php echo $cliente->cantidad_cajas ?? '' ?>">
+    </div>
+    <div class="formulario__campo">
+        <label class="formulario__label" for="cantidad_papel">Papel</label>
+        <input
+            type="number"
+            name="cantidad_papel"
+            id="cantidad_papel"
+            class="formulario__input"
+            placeholder="Cantidad de Papel"
+            value="<?php echo $cliente->cantidad_papel ?? '' ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="desperdicio_kg">Desperdicio (Kg)</label>
+        <input
+            type="number"
+            name="desperdicio_kg"
+            id="desperdicio_kg"
+            class="formulario__input"
+            placeholder="Desperdicio en Kg"
+            value="<?php echo $cliente->desperdicio_kg ?? '' ?>">
+    </div>
+
+    
+    
+
+
 
 </fieldset>
 
