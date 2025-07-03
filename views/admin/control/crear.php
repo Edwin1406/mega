c2083f0a4df82fee4c238475808bc4899a8a98df
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-03 10:02:27 -0500

    Update crear.php

--- views/admin/control/crear.php
+++ views/admin/control/crear.php
@@ -16,3 +16,30 @@
   Desperdicio (Kg): <input type="number" name="desperdicio_kg"><br>
   <button type="submit">Guardar</button>
 </form>
+
+
+
+<h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
+<div class="dashboard__contenedor-boton">
+    <a class="dashboard__boton" href="/admin/vendedor/cliente/tabla">
+    <i class="fa-regular fa-eye"></i>
+        VER ARCHIVO 
+    </a>
+
+</div>
+
+<div class="dashboard__formulario">
+
+<?php include_once __DIR__.'/../../../templates/alertas.php'  ?>
+
+    <form method="POST" action="/admin/vendedor/cliente/crear"  class="formulario" enctype="multipart/form-data">
+
+     
+    <?php include_once __DIR__.'/formulario.php'  ?>
+
+        <input class="formulario__submit formulario__submit--registrar" type="submit" value="Registrar Archivo">
+
+        
+    </form>
+
+</div>
