d4d36d39f3e2a82f1ca9f2bb9467b730018411ee
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-03 15:45:52 -0500

    Update ControlController.php

--- controllers/ControlController.php
+++ controllers/ControlController.php
@@ -16,42 +16,42 @@ class ControlController {
         isAuth();
 
         $control = new Control;
+        $alertas = [];
 
         // post 
       if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Primero sincroniza con los datos del formulario
+            $control->sincronizar($_POST);
 
-    // Primero sincroniza con los datos del formulario
-    $control->sincronizar($_POST);
+            // Luego realiza el cálculo
+            $golpes_maquina_hora = 0;
+            
+            if (!empty($control->horas_programadas) && !empty($control->golpes_maquina)) {
+                $horas_decimal = convertirHoraADecimal($control->horas_programadas);
+                
+                if ($horas_decimal > 0) {
+                    $golpes_maquina_hora = $control->golpes_maquina / $horas_decimal;
+                }
+            }
 
-    // Luego realiza el cálculo
-    $golpes_maquina_hora = 0;
-    
-    if (!empty($control->horas_programadas) && !empty($control->golpes_maquina)) {
-        $horas_decimal = convertirHoraADecimal($control->horas_programadas);
-        
-        if ($horas_decimal > 0) {
-            $golpes_maquina_hora = $control->golpes_maquina / $horas_decimal;
+            $control->golpes_maquina_hora = $golpes_maquina_hora;
+
+            // debuguear($control);
+
+            // Validar
+            $alertas = $control->validar();
+
+            if (empty($alertas)) {
+                // Guardar en la base de datos
+                $resultado = $control->guardar();
+                if ($resultado) {
+                    header('Location: /admin/control/crear');
+                }
+            } else {
+                // Mostrar alertas
+                $alertas = Control::getAlertas();
+            }
         }
-    }
-
-    $control->golpes_maquina_hora = $golpes_maquina_hora;
-
-    // debuguear($control);
-
-    // Validar
-    $alertas = $control->validar();
-
-    if (empty($alertas)) {
-        // Guardar en la base de datos
-        $resultado = $control->guardar();
-        if ($resultado) {
-            header('Location: /admin/control/crear');
-        }
-    } else {
-        // Mostrar alertas
-        $alertas = Control::getAlertas();
-    }
-}
 
 
         $router->render('admin/control/crear' , [
