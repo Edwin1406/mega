417844ded49975015778d74ff8be0ed7212e9aa6
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-03 10:09:38 -0500

    Update crear.php

--- views/admin/control/crear.php
+++ views/admin/control/crear.php
@@ -21,7 +21,7 @@
 
 <h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
 <div class="dashboard__contenedor-boton">
-    <a class="dashboard__boton" href="/admin/vendedor/cliente/tabla">
+    <a class="dashboard__boton" href="/">
     <i class="fa-regular fa-eye"></i>
         VER ARCHIVO 
     </a>
@@ -30,7 +30,7 @@
 
 <div class="dashboard__formulario">
 
-<?php include_once __DIR__.'/../../../templates/alertas.php'  ?>
+<?php include_once __DIR__.'/../../templates/alertas.php'  ?>
 
     <form method="POST" action="/admin/vendedor/cliente/crear"  class="formulario" enctype="multipart/form-data">
 
