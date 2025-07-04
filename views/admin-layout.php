e9ef5b99b67a0076a670bce5962a7804b47b6c6c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-04 14:02:19 -0500

    .

--- views/admin-layout.php
+++ views/admin-layout.php
@@ -39,11 +39,11 @@
          
 
 </body>
-  <!-- <script src="/public/build/js/bundle.min.js" defer></script> -->
+  <script src="/public/build/js/bundle.min.js" defer></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js"></script> -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js"></script>
 
-  <!-- <script src="https://cdn.jsdelivr.net/npm/quagga/dist/quagga.min.js"></script> -->
+  <script src="https://cdn.jsdelivr.net/npm/quagga/dist/quagga.min.js"></script>
 
   <script>
   AOS.init();
