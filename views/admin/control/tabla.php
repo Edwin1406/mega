b5e402ef923f6cddd9851219e8a6a342103ca9e9
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-04 12:31:00 -0500

    Update tabla.php

--- views/admin/control/tabla.php
+++ views/admin/control/tabla.php
@@ -219,9 +219,10 @@ foreach ($resumen as $operador => $valores) {
 <style>
 
 .grafico_control_produccion{
+    max-width: 800px;
     width: 100%;
     height: 400px;
-    /* margin: 20px auto; */
+    margin: 20px auto;
     padding: 20px;
     border-radius: 10px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
