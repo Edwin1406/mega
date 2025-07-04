37e518751d60ed5aa91731b4295b9234eae30467
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-04 14:59:52 -0500

    Update tabla.php

--- views/admin/control/tabla.php
+++ views/admin/control/tabla.php
@@ -229,8 +229,17 @@ $data = json_decode($response, true);
         }
 
         function randomColor() {
-            const colors = ['#4e79a7', '#f28e2b', '#e15759', '#76b7b2', '#59a14f'];
+            // const colors = ['#4e79a7', '#f28e2b', '#e15759', '#76b7b2', '#59a14f'];
+            const colors = [
+                '#4e79a7', '#f28e2b', '#e15759', '#76b7b2', '#59a14f', // colores originales
+                '#edc948', '#b07aa1', '#ff9da7', '#9c755f', '#bab0ab',
+                '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+                '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',
+                '#aec7e8', '#ffbb78', '#98df8a', '#ff9896', '#c5b0d5'
+                ];
+
             return colors[Math.floor(Math.random() * colors.length)];
+
         }
 
         function resetear() {
