3014b3bf33ba627e986a95d2ed38489efc67fc8f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-11 16:55:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,27 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
+
+// Detectar la IP del visitante (considerando proxies)
+$ip = $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'];
+
+// Consultar la API de geolocalización
+$response = file_get_contents("http://ip-api.com/json/$ip");
+$data = json_decode($response);
+
+// Verificar si es Ecuador
+if ($data && $data->countryCode !== "EC") {
+    // Redirigir si NO es Ecuador
+    header("Location: oops_ip_no_segura.html");
+    exit();
+}
+
+
+
+
+
+
 require_once __DIR__ . '/includes/app.php';
 
 
