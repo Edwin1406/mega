e632af7babf6d078f0e232b364f8a51fffa8471b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-14 16:57:15 -0500

    Update Consumo_general.php

--- models/Consumo_general.php
+++ models/Consumo_general.php
@@ -9,18 +9,21 @@ protected static $tabla = 'consumo_general';
 protected static $columnasDB = [
     'id',
     'tipo_maquina',
+    'total_general',
     'created_at'
 ];
 
 
     public $id;
     public $tipo_maquina;
+    public $total_general;
     public $created_at;
 
     public function __construct($args = [])
     {
         $this->id = $args['id'] ?? null;
         $this->tipo_maquina = $args['tipo_maquina'] ?? '';
+        $this->total_general = $args['total_general'] ?? '';
         $this->created_at = date('Y-m-d H:i:s');
     }
 
@@ -28,6 +31,11 @@ protected static $columnasDB = [
     {
         if (!$this->tipo_maquina) {
             self::$alertas['error'][] = 'El tipo de máquina es obligatorio';
+        }
+        if (!$this->total_general) {
+            self::$alertas['error'][] = 'El total general es obligatorio';
+        } elseif (!is_numeric($this->total_general)) {
+            self::$alertas['error'][] = 'El total general debe ser un número';
         }
         return self::$alertas;
     }
