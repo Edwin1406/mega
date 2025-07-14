31d849831ca6a64be845d6f5ffe284d8e387cd54
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-14 16:49:32 -0500

    Update consumo_general.php

--- views/admin/produccion/papel/consumo_general.php
+++ views/admin/produccion/papel/consumo_general.php
@@ -7,12 +7,32 @@
 
     <form method="POST" action="/admin/produccion/papel/crear"  class="formulario" enctype="multipart/form-data">
 
-     
-    <?php include_once __DIR__.'/formulario.php'  ?>
+        <div class="formulario-troquelado__campo">
+                <label class="formulario-troquelado__label" for="turnos">Nº Turnos:</label>
+                <input
+                    type="number"
+                    name="turnos"
+                    id="turnos"
+                    class="formulario-troquelado__input"
+                    placeholder="Nº Turnos"
+                    value="<?php echo $control->n_turno ?? '' ?>">
+            </div>
+
+
+            <div class="formulario-troquelado__campo">
+                <label class="formulario-troquelado__label" for="operador">Operadores</label>
+                <select
+                    name="operador"
+                    id="operador"
+                    class="formulario-troquelado__input">
+                    <option value="">Selecciona una opción</option>
+                    <option value="Luis Govea" <?php echo (isset($control->operadores) && $control->operadores == 'Luis Govea') ? 'selected' : ''; ?>>Luis Govea</option>
+                    <option value="Guillermo Bonilla" <?php echo (isset($control->operadores) && $control->operadores == 'Guillermo Bonilla') ? 'selected' : ''; ?>>Guillermo Bonilla</option>
+                    <!-- <option value="Carlos Govea" <?php echo (isset($control->operadores) && $control->operadores == 'Carlos Govea') ? 'selected' : ''; ?>>Carlos Govea</option> -->
+                </select>
+            </div>
 
         <input class="formulario__submit formulario__submit--registrar" type="submit" value="Registrar Papel">
-
-        
     </form>
 
 </div>
