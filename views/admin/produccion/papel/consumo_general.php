91121ee94e2ae45f5c3db59786fd6c3e57fed2cb
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-14 16:48:22 -0500

    Update consumo_general.php

--- views/admin/produccion/papel/consumo_general.php
+++ views/admin/produccion/papel/consumo_general.php
@@ -1,2 +1,18 @@
-<h2> <?php echo $titulo; ?></h2>
+<h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
 
+<div class="dashboard__formulario">
+
+<?php include_once __DIR__.'/../../../templates/alertas.php'  ?>
+
+
+    <form method="POST" action="/admin/produccion/papel/crear"  class="formulario" enctype="multipart/form-data">
+
+     
+    <?php include_once __DIR__.'/formulario.php'  ?>
+
+        <input class="formulario__submit formulario__submit--registrar" type="submit" value="Registrar Papel">
+
+        
+    </form>
+
+</div>
