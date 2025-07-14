382cec352000071cb3fb13b3ef6b37f17ef9bc1e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-14 16:50:19 -0500

    Update consumo_general.php

--- views/admin/produccion/papel/consumo_general.php
+++ views/admin/produccion/papel/consumo_general.php
@@ -2,34 +2,35 @@
 
 <div class="dashboard__formulario">
 
-<?php include_once __DIR__.'/../../../templates/alertas.php'  ?>
+    <?php include_once __DIR__ . '/../../../templates/alertas.php'  ?>
 
 
-    <form method="POST" action="/admin/produccion/papel/crear"  class="formulario" enctype="multipart/form-data">
+    <form method="POST" action="/admin/produccion/papel/crear" class="formulario" enctype="multipart/form-data">
 
         <div class="formulario-troquelado__campo">
-                <label class="formulario-troquelado__label" for="turnos">Nº Turnos:</label>
-                <input
-                    type="number"
-                    name="turnos"
-                    id="turnos"
-                    class="formulario-troquelado__input"
-                    placeholder="Nº Turnos"
-                    value="<?php echo $control->n_turno ?? '' ?>">
-            </div>
+            <label class="formulario-troquelado__label" for="turnos">Nº Turnos:</label>
+            <input
+                type="number"
+                name="turnos"
+                id="turnos"
+                class="formulario-troquelado__input"
+                placeholder="Nº Turnos"
+                value="<?php echo $control->n_turno ?? '' ?>">
+        </div>
 
 
-            <div class="formulario-troquelado__campo">
-                <label class="formulario-troquelado__label" for="operador">Operadores</label>
-                <select
-                    name="operador"
-                    id="operador"
-                    class="formulario-troquelado__input">
-                    <option value="">Selecciona una opción</option>
-                    <option value="Luis Govea" <?php echo (isset($control->operadores) && $control->operadores == 'Luis Govea') ? 'selected' : ''; ?>>Luis Govea</option>
-                    <option value="Guillermo Bonilla" <?php echo (isset($control->operadores) && $control->operadores == 'Guillermo Bonilla') ? 'selected' : ''; ?>>Guillermo Bonilla</option>
-                </select>
-            </div>
+        <div class="formulario-troquelado__campo">
+            <label class="formulario-troquelado__label" for="operador">Operadores</label>
+            <select
+                name="operador"
+                id="operador"
+                class="formulario-troquelado__input">
+                <option value="">Selecciona una opción</option>
+                <option value="Luis Govea" <?php echo (isset($control->operadores) && $control->operadores == 'Luis Govea') ? 'selected' : ''; ?>>Luis Govea</option>
+                <option value="Guillermo Bonilla" <?php echo (isset($control->operadores) && $control->operadores == 'Guillermo Bonilla') ? 'selected' : ''; ?>>Guillermo Bonilla</option>
+            </select>
+        </div>
+        <br>
 
         <input class="formulario__submit formulario__submit--registrar" type="submit" value="Registrar Papel">
     </form>
