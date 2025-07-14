3d5ad797f991bc366c8f14ef438faa5cd5eef530
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-14 16:53:41 -0500

    Update consumo_general.php

--- views/admin/produccion/papel/consumo_general.php
+++ views/admin/produccion/papel/consumo_general.php
@@ -7,30 +7,30 @@
 
     <form method="POST" action="/admin/produccion/papel/crear" class="formulario" enctype="multipart/form-data">
 
+        
         <div class="formulario__campo">
-            <label class="formulario__label" for="turnos">Nº Turnos:</label>
+            <label class="formulario__label" for="tipo_maquina">Tipo Maquina</label>
+            <select
+            name="tipo_maquina"
+            id="tipo_maquina"
+            class="formulario__input">
+            <option value="">Selecciona una opción</option>
+            <option value="CORRUGADOR" <?php echo (isset($control->tipo_maquina) && $control->tipo_maquina == 'CORRUGADOR') ? 'selected' : ''; ?>>CORRUGADOR</option>
+            <option value="MICRO" <?php echo (isset($control->tipo_maquina) && $control->tipo_maquina == 'MICRO') ? 'selected' : ''; ?>>MICRO</option>
+        </select>
+        
+        <div class="formulario__campo">
+            <label class="formulario__label" for="total_general">Total General:</label>
             <input
                 type="number"
-                name="turnos"
-                id="turnos"
+                name="total_general"
+                id="total_general"
                 class="formulario__input"
-                placeholder="Nº Turnos"
-                value="<?php echo $control->n_turno ?? '' ?>">
+                placeholder="total_general"
+                value="<?php echo $control->total_general ?? '' ?>">
         </div>
 
-
-        <div class="formulario__campo">
-            <label class="formulario__label" for="operador">Operadores</label>
-            <select
-                name="operador"
-                id="operador"
-                class="formulario__input">
-                <option value="">Selecciona una opción</option>
-                <option value="Luis Govea" <?php echo (isset($control->operadores) && $control->operadores == 'Luis Govea') ? 'selected' : ''; ?>>Luis Govea</option>
-                <option value="Guillermo Bonilla" <?php echo (isset($control->operadores) && $control->operadores == 'Guillermo Bonilla') ? 'selected' : ''; ?>>Guillermo Bonilla</option>
-            </select>
         </div>
-        <br>
 
         <input class="formulario__submit formulario__submit--registrar" type="submit" value="Registrar Papel">
     </form>
