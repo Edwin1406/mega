f10d17899ae62342eb36f59f4aee74e5302d800d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-15 16:09:53 -0500

    Update consumo_general.php

--- views/admin/produccion/papel/consumo_general.php
+++ views/admin/produccion/papel/consumo_general.php
@@ -17,7 +17,7 @@
             <option value="">Selecciona una opción</option>
             <option value="CORRUGADOR" <?php echo (isset($control->tipo_maquina) && $control->tipo_maquina == 'CORRUGADOR') ? 'selected' : ''; ?>>CORRUGADOR</option>
             <option value="MICRO" <?php echo (isset($control->tipo_maquina) && $control->tipo_maquina == 'MICRO') ? 'selected' : ''; ?>>MICRO</option>
-            <option value="TROQUELADOR" <?php echo (isset($control->tipo_maquina) && $control->tipo_maquina == 'TROQUELADOR') ? 'selected' : ''; ?>>TROQUELADOR</option>
+            <option value="TROQUEL" <?php echo (isset($control->tipo_maquina) && $control->tipo_maquina == 'TROQUEL') ? 'selected' : ''; ?>>TROQUEL</option>
             <option value="FLEXOGRAFICA" <?php echo (isset($control->tipo_maquina) && $control->tipo_maquina == 'FLEXOGRAFICA') ? 'selected' : ''; ?>>FLEXOGRAFICA</option>
             <option value="PRE PRINTER " <?php echo (isset($control->tipo_maquina) && $control->tipo_maquina == 'PRE PRINTER ') ? 'selected' : ''; ?>>PRE PRINTER </option>
             <option value="DOBLADO" <?php echo (isset($control->tipo_maquina) && $control->tipo_maquina == 'DOBLADO') ? 'selected' : ''; ?>>DOBLADO</option>
