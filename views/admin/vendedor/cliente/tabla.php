da7f86a129530399e8fafdf69a4200f6875621d0
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-17 17:03:22 -0500

    Update tabla.php

--- views/admin/vendedor/cliente/tabla.php
+++ views/admin/vendedor/cliente/tabla.php
@@ -113,20 +113,14 @@
                                     <!-- debe ser artes@megaecuador.com par que salga el boton de editar  -->
                                     <!-- session -->
 
-                             <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-?>
+                                    <?php echo '<pre>';
+print_r($_SESSION['usuario']);
+echo '</pre>';
+ ?>
 
-<?php if (isset($_SESSION['usuario']) && $_SESSION['usuario']->email == 'artes@megaecuador.com'): ?>
-    <td class="table__td--acciones">
-        <a class="table__accion table__accion--editar" href="/admin/vendedor/cliente/editar?id=<?php echo $visores->id; ?>">
-            <i class="fa-solid fa-user-pen"></i>Editar
-        </a>
-    </td>
-<?php endif; ?>
-
+                                    <?php if (isset($_SESSION['usuario']) && $_SESSION['usuario']->email === 'artes@megaecuador.com'): ?>
+                            <td  class="table__td--acciones"><a class="table__accion table__accion--editar" href="/admin/vendedor/cliente/editar?id=<?php echo $visores->id; ?>"><i class="fa-solid fa-user-pen"></i>Editar</a>
+                        <?php endif; ?>
 
 
                     </tr>
