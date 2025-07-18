a282621582b82b14bf7cb83ca3180bfce7958d44
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-18 14:06:36 -0500

    Update editar_consumo.php

--- views/admin/produccion/papel/editar_consumo.php
+++ views/admin/produccion/papel/editar_consumo.php
@@ -20,7 +20,7 @@
     <?php include_once __DIR__ . '/../../../templates/alertas.php'  ?>
 
 
-    <form method="POST" action="/admin/produccion/papel/consumo_general" class="formulario" enctype="multipart/form-data">
+    <form method="POST" action="/admin/produccion/papel/editar_consumo" class="formulario" enctype="multipart/form-data">
 
 
         <div class="formulario__campo">
