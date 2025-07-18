03291b25333d0bf0dd6c3f7f01cb5ffd58c4748f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-18 13:03:13 -0500

    Update PapelController.php

--- controllers/PapelController.php
+++ controllers/PapelController.php
@@ -1210,11 +1210,11 @@ public static function apiConsumoTablaPaginador()
         }
 
         $consumo = Consumo_general::find($id);
-        debuguear($consumo);
-
+        
         // tipo_maquina
         $consumo->tipo_maquina = trim($consumo->tipo_maquina);
         // debuguear($consumo);
+        debuguear($consumo);
 
         if (!$consumo) {
             header('Location: /admin/produccion/papel/tablaconsumoadmin');
