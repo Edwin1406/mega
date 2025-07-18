4e57ea3a35d7c0e1bdb7bfe15d9099978c50a923
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-18 12:17:42 -0500

    Update tablaconsumo.php

--- views/admin/produccion/papel/tablaconsumo.php
+++ views/admin/produccion/papel/tablaconsumo.php
@@ -1,6 +1,8 @@
 
 
 <h2 class="dashboard__heading"> <?php echo $titulo ?> </h2>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
