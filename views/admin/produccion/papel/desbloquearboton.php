871b3363eff9739bd1b1d02c6e996f2d39b160c6
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-07-18 12:48:20 -0500

    Update desbloquearboton.php

--- views/admin/produccion/papel/desbloquearboton.php
+++ views/admin/produccion/papel/desbloquearboton.php
@@ -24,7 +24,30 @@
 
 
         <div class="formulario__campo">
-                     <div class="formulario__campo">
+            <label class="formulario__label" for="tipo_maquina">Tipo Maquina</label>
+            <select
+                name="tipo_maquina"
+                id="tipo_maquina"
+                class="formulario__input">
+                <option value="">Selecciona una opción</option>
+                <option value="CORRUGADOR" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'CORRUGADOR') ? 'selected' : ''; ?>>CORRUGADOR</option>
+                <option value="MICRO" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'MICRO') ? 'selected' : ''; ?>>MICRO</option>
+                <option value="TROQUEL" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'TROQUEL') ? 'selected' : ''; ?>>TROQUEL</option>
+                <option value="FLEXOGRAFICA" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'FLEXOGRAFICA') ? 'selected' : ''; ?>>FLEXOGRAFICA</option>
+                <option value="PRE-PRINTER" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'PRE-PRINTER') ? 'selected' : ''; ?>>PRE-PRINTER</option>
+                <option value="DOBLADO" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'DOBLADO') ? 'selected' : ''; ?>>DOBLADO</option>
+                <option value="CORTE CEJA" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'CORTE CEJA') ? 'selected' : ''; ?>>CORTE CEJA</option>
+                <option value="TROQUEL" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'TROQUEL') ? 'selected' : ''; ?>>TROQUEL</option>
+                <option value="CONVERTIDOR" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'CONVERTIDOR') ? 'selected' : ''; ?>>CONVERTIDOR</option>
+                <option value="GUILLOTINA LAMINA" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'GUILLOTINA LAMINA') ? 'selected' : ''; ?>>GUILLOTINA LAMINA</option>
+                <option value="GUILLOTINA PAPEL" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'GUILLOTINA PAPEL') ? 'selected' : ''; ?>>GUILLOTINA PAPEL</option>
+                <option value="EMPAQUE" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'EMPAQUE') ? 'selected' : ''; ?>>EMPAQUE</option>
+                <option value="BODEGA" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'BODEGA') ? 'selected' : ''; ?>>BODEGA</option>
+                <option value="OTRO" <?php echo (isset($consumo->tipo_maquina) && $consumo->tipo_maquina == 'OTRO') ? 'selected' : ''; ?>>OTRO</option>
+
+            </select>
+
+            <div class="formulario__campo">
                 <label class="formulario__label" for="accion">Accion:</label>
                 <input
                     type="number"
